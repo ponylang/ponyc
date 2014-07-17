567d0487e1904fb7377677311e3d36809a7ca7fc
Sebastian Blessing <sebastian@causality.io>
2014-07-17 20:26:03 +0200

    platform independent llvm-config

--- premake4.lua
+++ premake4.lua
@@ -1,25 +1,45 @@
+-- os.outputof is broken in premake4, hence this workaround
+local function llvm_config(opt)
+    local stream = assert(io.popen("llvm-config-3.4 " .. opt))
+    local output = ""
+
+    --llvm-config contains '\n'
+    while true do
+      local curr = stream:read("*l")
+
+      if curr == nil then
+        break
+      end
+
+      output = output .. curr
+    end
+
+    stream:close()
+    return output
+end
+
 solution "ponyc"
   configurations {
     "Debug",
     "Release",
     "Profile"
-    }
+  }
 
   buildoptions {
     "-march=native",
     "-pthread",
-    }
+  }
 
   linkoptions {
     "-pthread",
-    "`llvm-config-3.4 --ldflags --libs all`",
+    llvm_config("--ldflags --libs all")
   }
 
   flags {
     "ExtraWarnings",
     "FatalWarnings",
     "Symbols",
-    }
+  }
 
   configuration "macosx"
     buildoptions "-Qunused-arguments"
@@ -41,27 +61,30 @@ solution "ponyc"
     buildoptions {
       "-O3",
       "-flto",
-      }
+    }
     linkoptions {
       "-flto",
       "-fuse-ld=gold",
-      }
+    }
 
   project "libponyc"
     targetname "ponyc"
     kind "StaticLib"
     language "C"
+    includedirs {
+      llvm_config("--includedir")
+    }
     buildoptions {
-      "-std=gnu11",
-      "-I`llvm-config-3.4 --includedir`",
-      }
+      "-std=gnu11"
+    }
+
     defines {
       "_DEBUG",
       "_GNU_SOURCE",
       "__STDC_CONSTANT_MACROS",
       "__STDC_FORMAT_MACROS",
       "__STDC_LIMIT_MACROS",
-      }
+    }
     files "src/libponyc/**.c"
 
   project "ponyc"
