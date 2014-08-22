878c25ee84d798179fcb35ce787876b738ea1bac
Sebastian Blessing <sebastian@causality.io>
2014-08-22 16:57:02 +0200

    intermediate commit, switching platforms

--- src/libponyc/platform/bigint.hpp
+++ src/libponyc/platform/bigint.hpp
@@ -1,43 +1,87 @@
 #if !defined(PLATFORM_INT128_H) && defined(PLATFORM_IS_VISUAL_STUDIO)
 #define PLATFORM_INT128_H
 
+/** Issues to be discussed:
+ *
+ * 
+ */
+
 template<size_t words>
 class BigUnsignedInteger
 {
-public:
-  size_t len; //actual length in blocks of uint64_ts.
-  uint64_t data[words];
+private:
+  template<typename T>
+  void assign_primitive(const T& value)
+  {
+    memset(&data, 0, sizeof(data));
 
+    data[0] = value;
+    len = 1;
+  }
+
+public:
+  uint64_t data[words];
+  size_t len; //actual length in blocks of uint64_ts.
+
+  /** General constructor to initialize a BigUnsignedInteger
+   *
+   */
   BigUnsignedInteger()
   {
-    len = 0;
     // We have to clear data, because we want to simulate
     // primitive behaviour of BigIntegers.
     memset(&data, 0, sizeof(data));
+    len = 0;
   }
 
+  /** Copy constructor for BigUnsignedIntegers of the same width.
+   *   
+   */
   BigUnsignedInteger(const BigUnsignedInteger &x)
   {
     len = x.len;
     memcpy(&data, &x.data, sizeof(data));
   }
 
+  /** Copy constructor for primitive built-in types of any width.
+   *
+   *  The problem is that this copy constructor is ambiguous and
+   *  would be accepted for any T, i.e. also BigSignedInteger of
+   *  any width. BigUnsignedIntegers are not a problem, as this
+   *  is resolved by the copy constructor above.
+   */
   template<typename T>
   BigUnsignedInteger(const T &x) : BigUnsignedInteger()
   {
-    len = 0;
+    len = 1;
     data[0] = x;
   }
 
-  BigUnsignedInteger& operator=(const BigUnsignedInteger &rvalue)
+  /** Assignment operators to assign primitive builtin types of any width.
+   * 
+   */
+  BigUnsignedInteger& operator=(const uint8_t value)
   {
+    assign_primitive<uint8_t>(value);
     return *this;
   }
-  
-  template<typename T>
-  BigUnsignedInteger& operator=(const T &rvalue)
+
+  BigUnsignedInteger& operator=(const uint16_t value)
   {
-    data[0] = rvalue;
+    assign_primitive<uint16_t>(value);
+    return *this;
+  }
+
+  /** Resolves unwanted implicit conversions of the operator above.
+   *
+   *  This means that we either support assignment of BigIntegers of the 
+   *  same type (and therefore sidedness) or to assign any integer that 
+   *  is natively supported on the platform this code is compiled.
+   */
+  BigUnsignedInteger& operator=(const BigUnsignedInteger &rvalue)
+  {
+    memcpy(&data, &rvalue.data, sizeof(data));
+    len = rvalue.len;
     return *this;
   }
 
@@ -103,6 +147,7 @@ public:
  
   BigUnsignedInteger& operator+=(const BigUnsignedInteger &rvalue)
   {
+    add(this, &rvalue, this);
     return *this;
   }
 
@@ -124,12 +169,6 @@ public:
   BigUnsignedInteger& operator%=(const BigUnsignedInteger &rvalue)
   {
     return *this;
-  }
-
-  template<typename T>
-  BigUnsignedInteger operator+(const T &rvalue) const
-  {
-    return this;
   }
 
   template<typename T>
@@ -173,7 +212,7 @@ public:
 };
 
 typedef enum cmp_t { less = -1, equal = 0, greater = 1 } cmp_t;
-typedef enum sign_t { negative = -1, zero = 0, positive = 1 } sign_t;
+typedef enum sign_t { invalid = -2, negative = -1, zero = 0, positive = 1 } sign_t;
 
 template<size_t words>
 class BigSignedInteger
@@ -377,13 +416,97 @@ static cmp_t compare(const BigUnsignedInteger<words>* a, const BigUnsignedIntege
 template<size_t words>
 static cmp_t compare(const BigUnsignedInteger<words>* a, const BigSignedInteger<words>* b)
 {
-  return equal;
+  if (b->sign == negative)
+    return greater;
+
+  return compare(a, b->magnitude);
 }
 
 template<size_t words>
 static cmp_t compare(const BigSignedInteger<words>* a, const BigSignedInteger<words>* b)
 {
-  return equal;
+  if (a->sign < b->sign)
+    return less;
+  else if (a->sign > b->sign)
+    return greater;
+  else
+  {
+    //The signs of a and b are equal.
+    switch (a->sign)
+    {
+    case zero:
+      return equal;
+    case negative:
+      //both a and b are negative integers, hence we can compare
+      //the two magnitudes and return the opposite result:
+      return cmp_t(-compare(a->magnitude, b->magnitude));
+    case positive:
+      //behavior is equivalent to comparing two unsigned ints
+      return compare(a->magnitude, b->magnitude);
+    }
+  }
+
+  //shouldn't occur
+  return invalid;
+}
+
+template <size_t words>
+static void add(BigUnsignedInteger<words>* a, BigUnsignedInteger<words>* b, BigUnsignedInteger<words>* res)
+{
+  // If either of the two operands is zero,
+  // addition is equivalent to assignment
+  if (a->len == 0)
+  {
+    res->operator=(b);
+    return;
+  }
+  else if (b->len == 0)
+  {
+    res->operator=(a);
+    return;
+  }
+
+  bool carry = false;
+  uint64_t tmp = 0;
+  size_t i = 0;
+
+  BigUnsignedInteger<words> *longer, *shorter;
+  a->len > b->len ? longer = a, shorter = b : longer = b; shorter = a;
+
+  for (; i < longer->len; ++i)
+  {
+    tmp = shorter->data[i] + longer->data[i];
+
+    // Tmp will be smaller than either of the two
+    // inputs only if the addition above overflowed.
+    carry = (tmp < longer->data[i]);
+
+    if (carry)
+    {
+      tmp++;
+      carry |= (tmp == 0);
+    }
+
+    res->data[i] = tmp;
+  }
+
+  for (; i < longer->len && carry; ++i)
+  {
+    tmp = longer->data[i] + 1;
+    carry = (tmp == 0);
+    res->data[i] = tmp;
+  }
+
+  //copy the remaining blocks of the larger number
+  for (; i < longer->len; ++i)
+  {
+    res->data[i] = longer->data[i];
+    
+    if (carry)
+      res->data[i] = 1;
+    else
+      res->len--;
+  }
 }
 
 #pragma endregion
@@ -620,7 +743,9 @@ template<size_t words>
 BigUnsignedInteger<words> operator+(const BigUnsignedInteger<words> &lvalue, 
   const BigUnsignedInteger<words> &rvalue)
 {
-  return lvalue;
+  BigUnsignedInteger<words> res;
+  add(&lvalue, &rvalue, &res);
+  return res;
 }
 
 template<size_t words>
