cd122c5daa1d1eb1c2891a6a561134d13bb78226
Sebastian Blessing <sebastian@causality.io>
2014-09-06 17:24:14 +0200

    trying to fix build problems on OSX Mavericks (libstdc++ vs. libc++)

--- scripts/helper.lua
+++ scripts/helper.lua
@@ -1,15 +1,16 @@
 function link_libponyc()
-  libdirs {
-    llvm_config("--libdir")
+  linkoptions {
+    llvm_config("--ldflags")
   }
+
+  links "libponyc"
 
   if os.is("windows") then
     links { "Shlwapi.lib" }
   end
 
-  links { "libponyc" }
   local output = llvm_config("--libs")
-  
+
   for lib in string.gmatch(output, "-l(%S+)") do
     links { lib }
   end
