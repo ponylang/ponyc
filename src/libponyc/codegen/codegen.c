e4f093435d9747592474adade75ec8b471058f12
sylvanc <sylvan@mk9sys.com>
2014-09-18 12:29:53 +0100

    rename libpony to libponyrt

--- src/libponyc/codegen/codegen.c
+++ src/libponyc/codegen/codegen.c
@@ -562,7 +562,7 @@ static bool codegen_finalise(compile_t* c, pass_opt_t* opt, pass_id pass_limit)
 
   // User specified libraries go here, in any order.
   append_link_paths(ld_cmd);
-  strcat(ld_cmd, " -lpony -lSystem");
+  strcat(ld_cmd, " -lponyrt -lSystem");
   free(arch);
 
   if(system(ld_cmd) != 0)
@@ -591,7 +591,7 @@ static bool codegen_finalise(compile_t* c, pass_opt_t* opt, pass_id pass_limit)
   // User specified libraries go here, surrounded with --start-group and
   // --end-group so that we don't have to determine an ordering.
   strcat(ld_cmd,
-    " -lpony -lpthread -lc "
+    " -lponyrt -lpthread -lc "
     "/lib/x86_64-linux-gnu/libgcc_s.so.1 "
     "/usr/lib/x86_64-linux-gnu/crtn.o"
     );
