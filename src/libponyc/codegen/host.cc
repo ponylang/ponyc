f8d301c16faeb70ae6f2e7634b56930a2b409c9e
Sebastian Blessing <sebastian@causality.io>
2014-10-13 11:56:34 +0200

    fixed windows build

--- src/libponyc/codegen/host.cc
+++ src/libponyc/codegen/host.cc
@@ -3,7 +3,12 @@
 #endif
 
 #include <llvm/Support/Host.h>
+#pragma warning(push) 
+//because LLVM IR Builder code is broken: e.g. Instructions.h:521-527
+#pragma warning(disable:4244)
+#pragma warning(disable:4800) 
 #include <llvm/IR/IRBuilder.h>
+#pragma warning(pop)
 #include <platform.h>
 #include "codegen.h"
 
