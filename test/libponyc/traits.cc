e68e82a1bb2d67aa1127a5440aa6cab89ebda982
andymcn <andrew@causality.io>
2015-05-29 15:23:17 +0100

    Simplify contra & co-variance and default body inheriting rules

--- test/libponyc/traits.cc
+++ test/libponyc/traits.cc
@@ -14,42 +14,51 @@ class TraitsTest : public PassTest
 {};
 
 
-TEST_F(TraitsTest, ClassGetsTraitBody)
+// Basics
+
+TEST_F(TraitsTest, InheritMethod)
 {
   const char* src =
-    "trait T\n"
-    "  fun ref bar(): U32 => 1\n"
-    "class C is T\n"
-    "  new create() => true";
+    "trait T1\n"
+    "  fun f(): U32\n"
+
+    "trait T2 is T1";
 
   const char* expect =
-    "trait T\n"
-    "  fun ref bar(): U32 => 1\n"
-    "class C is T\n"
-    "  new create() => true\n"
-    "  fun ref bar(): U32 => 1";
+    "trait T1\n"
+    "  fun f(): U32\n"
+
+    "trait T2 is T1\n"
+    "  fun f(): U32";
 
   TEST_EQUIV(src, expect);
 }
 
 
-TEST_F(TraitsTest, ClassBodyNotOverriddenByTrait)
+TEST_F(TraitsTest, TransitiveTraits)
 {
   const char* src =
-    "trait T\n"
-    "  fun ref bar(): U32 => 1\n"
-    "class C is T\n"
-    "  fun ref bar(): U32 => 2";
+    "trait T1\n"
+    "  fun f1(): U32\n"
 
-  TEST_COMPILE(src);
+    "trait T2 is T1\n"
+    "  fun f2(): Bool\n"
 
-  WALK_TREE(package);
-  LOOKUP("C", TK_CLASS);
-  LOOKUP("bar", TK_FUN);
-  METHOD_BODY;
-  CHILD(0);
+    "trait T3 is T2";
 
-  ASSERT_EQ(2, ast_int(walk_ast));
+  const char* expect =
+    "trait T1\n"
+    "  fun f1(): U32\n"
+
+    "trait T2 is T1\n"
+    "  fun f2(): Bool\n"
+    "  fun f1(): U32\n"
+
+    "trait T3 is T2\n"
+    "  fun f2(): Bool\n"
+    "  fun f1(): U32";
+
+  TEST_EQUIV(src, expect);
 }
 
 
@@ -60,59 +69,6 @@ TEST_F(TraitsTest, CannotProvideClass)
     "class C2 is C1";
 
   TEST_ERROR(src);
-}
-
-
-TEST_F(TraitsTest, NoBody)
-{
-  const char* src =
-    "trait T\n"
-    "  fun ref bar(): U32\n"
-    "class C is T";
-
-  TEST_ERROR(src);
-}
-
-
-TEST_F(TraitsTest, AmbiguousBody)
-{
-  const char* src =
-    "trait T1\n"
-    "  fun ref bar(): U32 => 1\n"
-    "trait T2\n"
-    "  fun ref bar(): U32 => 2\n"
-    "class C is T1, T2";
-
-  TEST_ERROR(src);
-}
-
-
-TEST_F(TraitsTest, TransitiveTraits)
-{
-  const char* src =
-    "trait T1\n"
-    "  fun ref f2(): U32 => 2\n"
-
-    "trait T2 is T1\n"
-    "  fun ref f1(): Bool => 1\n"
-
-    "class C is T2\n"
-    "  new create() => true";
-
-  const char* expect =
-    "trait T1\n"
-    "  fun ref f2(): U32 => 2\n"
-
-    "trait T2 is T1\n"
-    "  fun ref f1(): Bool => 1\n"
-    "  fun ref f2(): U32 => 2\n"
-
-    "class C is T2\n"
-    "  new create() => true\n"
-    "  fun ref f1(): Bool => 1\n"
-    "  fun ref f2(): U32 => 2\n";
-
-  TEST_EQUIV(src, expect);
 }
 
 
@@ -130,7 +86,7 @@ TEST_F(TraitsTest, MultiTraitLoop)
   const char* src =
     "trait T1 is T2\n"
     "trait T2 is T3\n"
-    "trait T3 is T1\n";
+    "trait T3 is T1";
 
   TEST_ERROR(src);
 }
@@ -140,7 +96,8 @@ TEST_F(TraitsTest, TraitAndClassNamesDontClash)
 {
   const char* src =
     "trait T\n"
-    "  fun ref bar(y: U32): U32 => y\n"
+    "  fun f(y: U32): U32 => y\n"
+
     "class C is T\n"
     "  var y: Bool";
 
@@ -148,11 +105,312 @@ TEST_F(TraitsTest, TraitAndClassNamesDontClash)
 }
 
 
+// Contra- and co-variance
+
+TEST_F(TraitsTest, Contravariance)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2 is T1\n"
+    "  fun f(x: AB)";
+
+  TEST_COMPILE(src);
+}
+
+
+TEST_F(TraitsTest, ReverseContravarianceFail)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: AB)\n"
+
+    "trait T2 is T1\n"
+    "  fun f(x: A)";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, MultiContravarianceSelection)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: AB)\n"
+
+    "trait T3 is T1, T2\n"
+    "  fun f(x: AB)";
+
+  TEST_COMPILE(src);
+}
+
+
+TEST_F(TraitsTest, ReverseMultiContravarianceSelectionFail)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: AB)\n"
+
+    "trait T3 is T1, T2\n"
+    "  fun f(x: A)";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, MultiContravarianceNoSelectionFail)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: AB)\n"
+
+    "trait T3 is T1, T2";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, MultiNovarianceNoSelection)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: A)\n"
+
+    "trait T3 is T1, T2";
+
+  TEST_COMPILE(src);
+}
+
+
+TEST_F(TraitsTest, IndepenedentContravarianceSelection)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: B)\n"
+
+    "trait T3 is T1, T2\n"
+    "  fun f(x: AB)";
+
+  TEST_COMPILE(src);
+}
+
+
+TEST_F(TraitsTest, IndepenedentContravarianceNoSelectionFail)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: B)\n"
+
+    "trait T3 is T1, T2";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, Covariance)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(): AB\n"
+
+    "trait T2 is T1\n"
+    "  fun f(): A";
+
+  TEST_COMPILE(src);
+}
+
+
+TEST_F(TraitsTest, ReverseCovarianceFail)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(): A\n"
+
+    "trait T2 is T1\n"
+    "  fun f(): AB";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, IndepenedentCovarianceNoSelectionFail)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(): A\n"
+
+    "trait T2\n"
+    "  fun f(): B\n"
+
+    "trait T3 is T1, T2";
+
+  TEST_ERROR(src);
+}
+
+
+// Default bodies
+
+TEST_F(TraitsTest, ClassGetsTraitBody)
+{
+  // Need to explicitly state C.create() as otherwise it's added AFTER the
+  // functions from T and our ASTs won't match
+
+  const char* src =
+    "trait T\n"
+    "  fun f(): U32 => 1\n"
+    "class C is T\n"
+    "  new create() => true";
+
+  const char* expect =
+    "trait T\n"
+    "  fun f(): U32 => 1\n"
+    "class C is T\n"
+    "  new create() => true\n"
+    "  fun f(): U32 => 1";
+
+  TEST_EQUIV(src, expect);
+}
+
+
+TEST_F(TraitsTest, ClassBodyNotOverriddenByTrait)
+{
+  const char* src =
+    "trait T\n"
+    "  fun f(): U32 => 1\n"
+    "class C is T\n"
+    "  fun f(): U32 => 2";
+
+  TEST_COMPILE(src);
+
+  WALK_TREE(package);
+  LOOKUP("C", TK_CLASS);
+  LOOKUP("f", TK_FUN);
+  METHOD_BODY;
+  CHILD(0);
+
+  ASSERT_EQ(2, ast_int(walk_ast));
+}
+
+
+TEST_F(TraitsTest, NoBodyFail)
+{
+  const char* src =
+    "trait T\n"
+    "  fun f(): U32\n"
+    "class C is T";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, AmbiguousBodyFail)
+{
+  const char* src =
+    "trait T1\n"
+    "  fun f(): U32 => 1\n"
+    "trait T2\n"
+    "  fun f(): U32 => 2\n"
+    "class C is T1, T2";
+
+  TEST_ERROR(src);
+}
+
+
+TEST_F(TraitsTest, ParentOverridesGrandparentBody)
+{
+  const char* src =
+    "trait T1\n"
+    "  fun f(): U32 => 1\n"
+
+    "trait T2 is T1\n"
+    "  fun f(): U32 => 2\n"
+
+    "class C is T2\n"
+    "  new create() => true";
+
+  const char* expect =
+    "trait T1\n"
+    "  fun f(): U32 => 1\n"
+
+    "trait T2 is T1\n"
+    "  fun f(): U32 => 2\n"
+
+    "class C is T2\n"
+    "  new create() => true\n"
+    "  fun f(): U32 => 2";
+
+  TEST_EQUIV(src, expect);
+}
+
+
 TEST_F(TraitsTest, DiamondInheritance)
 {
   const char* src =
     "trait T1\n"
-    "  fun ref bar(): U32 => 1\n"
+    "  fun f(): U32 => 1\n"
 
     "trait T2 is T1\n"
     "trait T3 is T1\n"
@@ -162,20 +420,23 @@ TEST_F(TraitsTest, DiamondInheritance)
 
   const char* expect =
     "trait T1\n"
-    "  fun ref bar(): U32 => 1\n"
+    "  fun f(): U32 => 1\n"
 
     "trait T2 is T1\n"
+    "  fun f(): U32 => 1\n"
+
     "trait T3 is T1\n"
+    "  fun f(): U32 => 1\n"
 
     "class C is T2, T3\n"
     "  new create() => true\n"
-    "  fun ref bar(): U32 => 1\n";
+    "  fun f(): U32 => 1";
 
   TEST_EQUIV(src, expect);
 }
 
 
-TEST_F(TraitsTest, MethodContravariance)
+TEST_F(TraitsTest, ContravariantBodyInheritted)
 {
   const char* src =
     "trait A\n"
@@ -183,237 +444,51 @@ TEST_F(TraitsTest, MethodContravariance)
     "type AB is (A | B)\n"
 
     "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: AB) => 1\n"
+
+    "trait T3 is T1, T2\n"
+    "  fun f(x: AB)";
+
+  const char* expect =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A)\n"
+
+    "trait T2\n"
+    "  fun f(x: AB) => 1\n"
+
+    "trait T3 is T1, T2\n"
+    "  fun f(x: AB) => 1";
+
+  TEST_EQUIV(src, expect);
+}
+
+
+TEST_F(TraitsTest, ReverseContravariantBodyNotInheritted)
+{
+  const char* src =
+    "trait A\n"
+    "trait B\n"
+    "type AB is (A | B)\n"
+
+    "trait T1\n"
+    "  fun f(x: A) => 1\n"
 
     "trait T2 is T1\n"
-    "  fun ref bar(x: AB): U32 => 2\n"
-    
-    "class C is T2\n"
-    "  new create() => true";
+    "  fun f(x: AB)";
 
-  const char* expect =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
+  TEST_COMPILE(src);
 
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
+  WALK_TREE(package);
+  LOOKUP("T2", TK_TRAIT);
+  LOOKUP("f", TK_FUN);
+  METHOD_BODY;
 
-    "trait T2 is T1\n"
-    "  fun ref bar(x: AB): U32 => 2\n"
-
-    "class C is T2\n"
-    "  new create() => true\n"
-    "  fun ref bar(x: AB): U32 => 2\n";
-
-  TEST_EQUIV(src, expect);
-}
-
-
-TEST_F(TraitsTest, MethodContravarianceMultiSource)
-{
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: AB): U32 => 2\n"
-
-    "class C is T1, T2\n"
-    "  new create() => true";
-
-  const char* expect =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: AB): U32 => 2\n"
-
-    "class C is T1, T2\n"
-    "  new create() => true\n"
-    "  fun ref bar(x: AB): U32 => 2\n";
-
-  TEST_EQUIV(src, expect);
-}
-
-
-TEST_F(TraitsTest, MethodContravarianceMultiSourceClash)
-{
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: B): U32 => 2\n"
-
-    "class C is T1, T2\n"
-    "  new create() => true";
-
-  TEST_ERROR(src);
-}
-
-
-TEST_F(TraitsTest, MethodContravariance3Source)
-{
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: B): U32 => 2\n"
-
-    "trait T3\n"
-    "  fun ref bar(x: AB): U32 => 3\n"
-
-    "class C is T1, T2, T3\n"
-    "  new create() => true";
-
-  const char* expect =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: B): U32 => 2\n"
-
-    "trait T3\n"
-    "  fun ref bar(x: AB): U32 => 3\n"
-
-    "class C is T1, T2, T3\n"
-    "  new create() => true\n"
-    "  fun ref bar(x: AB): U32 => 3\n";
-
-  TEST_EQUIV(src, expect);
-}
-
-
-TEST_F(TraitsTest, MethodContravariance3SourceReverseOrder)
-{
-
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: B): U32 => 2\n"
-
-    "trait T3\n"
-    "  fun ref bar(x: AB): U32 => 3\n"
-
-    "class C is T3, T2, T1\n"
-    "  new create() => true";
-
-  const char* expect =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(x: A): U32 => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(x: B): U32 => 2\n"
-
-    "trait T3\n"
-    "  fun ref bar(x: AB): U32 => 3\n"
-
-    "class C is T3, T2, T1\n"
-    "  new create() => true\n"
-    "  fun ref bar(x: AB): U32 => 3\n";
-
-  TEST_EQUIV(src, expect);
-}
-
-
-TEST_F(TraitsTest, MethodReverseContravariance)
-{
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T\n"
-    "  fun ref bar(x: AB): U32\n"
-
-    "class C is T\n"
-    "  new create() => true\n"
-    "  fun ref bar(x: A): U32 => 2\n";
-
-  TEST_ERROR(src);
-}
-
-
-TEST_F(TraitsTest, MethodCovariance)
-{
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(): A => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(): AB => 2\n"
-
-    "class C is T1, T2\n"
-    "  new create() => true";
-
-  const char* expect =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T1\n"
-    "  fun ref bar(): A => 1\n"
-
-    "trait T2\n"
-    "  fun ref bar(): AB => 2\n"
-
-    "class C is T1, T2\n"
-    "  new create() => true\n"
-    "  fun ref bar(): A => 1\n";
-
-  TEST_EQUIV(src, expect);
-}
-
-
-TEST_F(TraitsTest, MethodReverseCovariance)
-{
-  const char* src =
-    "trait A\n"
-    "trait B\n"
-    "type AB is (A | B)\n"
-
-    "trait T\n"
-    "  fun ref bar(): A\n"
-
-    "class C is T\n"
-    "  new create() => true\n"
-    "  fun ref bar(): AB => 1\n";
-
-  TEST_ERROR(src);
+  ASSERT_EQ(TK_NONE, ast_id(walk_ast));
 }
