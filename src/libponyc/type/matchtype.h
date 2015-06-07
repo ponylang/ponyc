f0b01a5499b0948f5bfd0c830fc5b1b6ea398682
sylvanc <sylvan@mk9sys.com>
2015-06-07 11:10:42 +0100

    add explanation of ACCEPT, REJECT, DENY

--- src/libponyc/type/matchtype.h
+++ src/libponyc/type/matchtype.h
@@ -15,6 +15,13 @@ typedef enum
 
 /**
  * Determine if we can pattern match on sub and extract a super.
+ *
+ * If a subtype of sub exists that is a subtype of super, return ACCEPT. If no
+ * such subtype can exist, return REJECT. If a subtype can't exist, but one
+ * could if capabilities were ignored, return DENY. This is to prevent a match
+ * that could be detected with runtime information but would actually violate
+ * the capability guarantees. When DENY is returned, no matching is allowed,
+ * even if some other could_subtype() relationship exists.
  */
 matchtype_t could_subtype(ast_t* sub, ast_t* super);
 
