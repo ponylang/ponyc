ba8675c669643b8ff3171a33fef0c3ecdac177b0
sylvanc <sylvan@mk9sys.com>
2015-06-15 14:06:07 +0100

    ssl multithreading support

---
+++ src/libponyrt/lang/ssl.c
@@ -0,0 +1,45 @@
+#include <platform.h>
+
+#if defined(PLATFORM_IS_WINDOWS)
+static HANDLE* locks;
+#else
+static pthread_mutex_t* locks;
+#endif
+
+static void locking_callback(int mode, int type, const char* file, int line)
+{
+  (void)file;
+  (void)line;
+
+  if(mode & 1)
+  {
+#if defined(PLATFORM_IS_WINDOWS)
+    WaitForSingleObject(locks[type], INFINITE);
+#else
+    pthread_mutex_lock(&locks[type]);
+#endif
+  } else {
+#if defined(PLATFORM_IS_WINDOWS)
+    ReleaseMutex(locks[type]);
+#else
+    pthread_mutex_unlock(&locks[type]);
+#endif
+  }
+}
+
+void* os_ssl_multithreading(uint32_t count)
+{
+#if defined(PLATFORM_IS_WINDOWS)
+  locks = malloc(count * sizeof(HANDLE*));
+
+  for(uint32_t i = 0; i < count; i++)
+    locks[i] = CreateMutex(NULL, FALSE, NULL);
+#else
+  locks = malloc(count * sizeof(pthread_mutex_t));
+
+  for(uint32_t i = 0; i < count; i++)
+    pthread_mutex_init(&locks[i], NULL);
+#endif
+
+  return locking_callback;
+}
