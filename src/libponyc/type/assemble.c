c7a50db09510303163dfe0bd37c6611ee69c1163
sylvanc <sylvan@mk9sys.com>
2015-07-20 09:17:42 +0100

    partial impl of type_isect_fun for andy

--- src/libponyc/type/assemble.c
+++ src/libponyc/type/assemble.c
@@ -1,6 +1,7 @@
 #include "assemble.h"
 #include "subtype.h"
 #include "lookup.h"
+#include "cap.h"
 #include "../ast/token.h"
 #include "../ast/astbuild.h"
 #include "../expr/literal.h"
@@ -303,6 +304,63 @@ ast_t* type_for_fun(ast_t* ast)
   BUILD(fun, ast,
     NODE(TK_FUNTYPE, NODE(fcap) TREE(typeparams) TREE(params) TREE(result)));
 
+  return fun;
+}
+
+ast_t* type_isect_fun(ast_t* a, ast_t* b)
+{
+  token_id ta = ast_id(a);
+  token_id tb = ast_id(b);
+
+  if(((ta == TK_NEW) || (tb == TK_NEW)) && (ta != tb))
+    return NULL;
+
+  AST_GET_CHILDREN(a, a_cap, a_id, a_typeparams, a_params, a_result, a_throw);
+  AST_GET_CHILDREN(b, b_cap, b_id, b_typeparams, b_params, b_result, b_throw);
+
+  // Must have the same name.
+  if(ast_name(a_id) != ast_name(b_id))
+    return NULL;
+
+  // Must have the same number of type parameters and parameters.
+  if((ast_childcount(a_typeparams) != ast_childcount(b_typeparams)) ||
+    (ast_childcount(a_params) != ast_childcount(b_params)))
+    return NULL;
+
+  // Contravariant receiver cap.
+  token_id tcap;
+  token_id a_tcap = ast_id(a_cap);
+  token_id b_tcap = ast_id(b_cap);
+
+  if(is_cap_sub_cap(b_tcap, a_tcap))
+    tcap = a_tcap;
+  else if(is_cap_sub_cap(a_tcap, b_tcap))
+    tcap = b_tcap;
+  else
+    tcap = TK_BOX;
+
+  // Result is the intersection of the results.
+  ast_t* result = type_isect(a_result, b_result);
+
+  // Covariant throws.
+  token_id throws;
+
+  if((ast_id(a_throw) == TK_NONE) || (ast_id(b_throw) == TK_NONE))
+    throws = TK_NONE;
+  else
+    throws = TK_QUESTION;
+
+  BUILD(fun, a,
+    NODE(tcap)
+    TREE(a_id)
+    NODE(TK_TYPEPARAMS)
+    NODE(TK_PARAMS)
+    TREE(result)
+    NODE(throws)
+    );
+
+  // TODO: union typeparams and params
+  // handling typeparam names is tricky
   return fun;
 }
 
