7f8cb889a389c13d1ccf3dc0ee33a89320d25205
andymcn <andrew@causality.io>
2015-11-11 14:21:41 +0000

    Improve package merge error messages

--- src/libponyc/pass/import.c
+++ src/libponyc/pass/import.c
@@ -1,6 +1,8 @@
 #include "import.h"
 #include "../ast/ast.h"
+#include "../ast/symtab.h"
 #include <assert.h>
+#include <string.h>
 
 
 static bool import_use(ast_t* ast)
@@ -9,27 +11,76 @@ static bool import_use(ast_t* ast)
 
   ast_t* import = (ast_t*)ast_data(ast);
 
-  if(import == NULL) // Nothing to import
+  if(import == NULL) // Nothing to import.
     return true;
 
   ast_t* module = ast_parent(ast);
   assert(ast_id(module) == TK_MODULE);
+  symtab_t* mod_symtab = ast_get_symtab(module);
 
   ast_t* package = ast_parent(module);
   assert(ast_id(package) == TK_PACKAGE);
+  symtab_t* pkg_symtab = ast_get_symtab(package);
 
-  if(!ast_canmerge(package, import) || !ast_merge(module, import))
+  ast_t* uri_child = ast_childidx(ast, 1);
+  assert(uri_child != NULL);
+  const char* uri = ast_name(uri_child);
+  assert(uri != NULL);
+
+  bool is_builtin = (strcmp(uri, "builtin") == 0);
+  bool ok = true;
+
+  size_t i = HASHMAP_BEGIN;
+  symbol_t* sym;
+  symtab_t* src_symtab = ast_get_symtab(import);
+  assert(src_symtab != NULL);
+
+  while((sym = symtab_next(src_symtab, &i)) != NULL)
   {
-    ast_t* uri_child = ast_childidx(ast, 1);
-    assert(uri_child != NULL);
-    const char* uri = ast_name(uri_child);
-    assert(uri != NULL);
+    if((sym->name[0] == '_') || // Public symbols only.
+      (sym->name[0] == '@') ||  // Don't merge FFI declarations.
+      (sym->status == SYM_NOCASE) ||
+      (strcmp(sym->name, "Main") == 0))
+      continue;
 
-    ast_error(ast, "can't merge symbols from '%s'", uri);
-    return false;
+    ast_t* existing = symtab_find_case(pkg_symtab, sym->name, NULL);
+
+    if(existing == NULL)
+      existing = symtab_find_case(mod_symtab, sym->name, NULL);
+
+    if(existing != NULL)
+    {
+      // Symbol clash.
+      if(is_builtin)
+      {
+        ast_error(existing, "type name clashes with builtin type");
+        ast_error(sym->def, "builtin type here");
+      }
+      else
+      {
+        if(ok)
+        {
+          // Only print the "use" as an error once.
+          ast_error(ast, "can't use '%s' without alias, clashing symbols",
+            uri);
+        }
+
+        ast_error(existing, "existing type name clashes with type from '%s'",
+          uri);
+        ast_error(sym->def, "clash trying to use this type");
+      }
+
+      ok = false;
+    }
+    else
+    {
+      // OK to add symbol.
+      symtab_add(mod_symtab, sym->name, sym->def, sym->status);
+    }
+
   }
 
-  return true;
+  return ok;
 }
 
 
