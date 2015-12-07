e5642ff17850cd3a602b75e8284f88ef16495dbe
Sylvan Clebsch <sylvan@mk9sys.com>
2015-12-07 15:39:10 +0000

    fix 32-bit pad size

--- src/libponyrt/pony.h
+++ src/libponyrt/pony.h
@@ -101,9 +101,9 @@ typedef const struct _pony_type_t
  * 44/80 bytes: gc
  */
 #if INTPTR_MAX == INT64_MAX
-#define PONY_ACTOR_PAD_SIZE 240
+#  define PONY_ACTOR_PAD_SIZE 240
 #elif INTPTR_MAX == INT32_MAX
-#define PONY_ACTOR_PAD_SIZE 152
+#  define PONY_ACTOR_PAD_SIZE 156
 #endif
 
 typedef struct pony_actor_pad_t
