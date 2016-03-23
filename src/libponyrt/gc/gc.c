95b6009184b0149084f1677e45b34e43074543dc
sylvanc <sylvan.clebsch@gmail.com>
2016-03-23 07:57:37 +0000

    use actormap_getorput in gc_markactor

--- src/libponyrt/gc/gc.c
+++ src/libponyrt/gc/gc.c
@@ -265,7 +265,8 @@ static void mark_remote_object(pony_ctx_t* ctx, pony_actor_t* actor,
   obj->mark = gc->mark;
 
   // Add to heap used size.
-  ponyint_heap_used(ponyint_actor_heap(ctx->current), ponyint_heap_size(chunk));
+  ponyint_heap_used(ponyint_actor_heap(ctx->current),
+    ponyint_heap_size(chunk));
 
   if(immutable && !obj->immutable && (obj->rc > 0))
   {
@@ -364,7 +365,8 @@ void ponyint_gc_sendactor(pony_ctx_t* ctx, pony_actor_t* actor)
   {
     send_local_actor(gc);
   } else {
-    actorref_t* aref = ponyint_actormap_getorput(&gc->foreign, actor, gc->mark);
+    actorref_t* aref = ponyint_actormap_getorput(&gc->foreign, actor,
+      gc->mark);
     send_remote_actor(ctx, gc, aref);
   }
 }
@@ -377,7 +379,8 @@ void ponyint_gc_recvactor(pony_ctx_t* ctx, pony_actor_t* actor)
   {
     recv_local_actor(gc);
   } else {
-    actorref_t* aref = ponyint_actormap_getorput(&gc->foreign, actor, gc->mark);
+    actorref_t* aref = ponyint_actormap_getorput(&gc->foreign, actor,
+      gc->mark);
     recv_remote_actor(ctx, gc, aref);
   }
 }
@@ -388,7 +391,7 @@ void ponyint_gc_markactor(pony_ctx_t* ctx, pony_actor_t* actor)
     return;
 
   gc_t* gc = ponyint_actor_gc(ctx->current);
-  actorref_t* aref = ponyint_actormap_getactor(&gc->foreign, actor);
+  actorref_t* aref = ponyint_actormap_getorput(&gc->foreign, actor, gc->mark);
   mark_remote_actor(ctx, gc, aref);
 }
 
