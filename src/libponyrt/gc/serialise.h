ef424feda26013d52330367a881af20a91963002
Gordon Tisher <gordon@balafon.net>
2016-06-08 14:19:31 -0700

    Force vs2015 to not mangle serialise function names (#920)
    VS2015 is mangling the function names in serialise.c, which causes link errors when ponyc is linking on Windows. This change declares the functions as extern "C" so the exported names are not mangled.

--- src/libponyrt/gc/serialise.h
+++ src/libponyrt/gc/serialise.h
@@ -14,6 +14,15 @@ void ponyint_serialise_object(pony_ctx_t* ctx, void* p, pony_type_t* t,
 
 void ponyint_serialise_actor(pony_ctx_t* ctx, pony_actor_t* actor);
 
+void pony_serialise(pony_ctx_t* ctx, void* p, void* out);
+size_t pony_serialise_offset(pony_ctx_t* ctx, void* p);
+void pony_serialise_reserve(pony_ctx_t* ctx, void* p, size_t size);
+
+void* pony_deserialise(pony_ctx_t* ctx, void* in);
+void* pony_deserialise_block(pony_ctx_t* ctx, uintptr_t offset, size_t size);
+void* pony_deserialise_offset(pony_ctx_t* ctx, pony_type_t* t,
+  uintptr_t offset);
+
 PONY_EXTERN_C_END
 
 #endif
