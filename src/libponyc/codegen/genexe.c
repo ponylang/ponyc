f0fa3fc0c7c128c05b2493e5231eed94289d8f01
Reini Urban <rurban@cpan.org>
2016-08-24 21:48:37 +0200

    Add support for LLVM 3.9
    LoadCombine bug is still present but this commit doesn't do anything to impact
    the current handling of that. Shouldn't have any impact compared to 3.8 et al
    wrt LoadCombine.
    
    Includes:
    
    * Fix for llvm-3.7/3.8 ranlib on macports
    
    macports fails now with the system ar -rcs, its
    ranlib it cannot sort the symbols in the libponyc.a.
    Hence check for llvm-ar-mp-3.7 and homebrew llvm-ar-3.7 with
    a different mandatory AR_FLAGS=rcs
    This is only needed for config=release, the -flto linker.
    
    WIP: ld fails to find libLTO also. maybe need to add the macports
    llvm-config --libpath.
    See [ponyc #456]
    
    * darwin: honor --linker
    
    which is esp. needed since llvm 3.7 to use a working ld with LTO support.
    macports ld has it.
    
    * llvm-3.9 support
    
    LLVMGetTargetMachineData is deprecated. Rather set the
    DataLayout from the triple.
    
    Passes all tests, but the darwin-specific LoadCombiner bug
    is still present.

--- src/libponyc/codegen/genexe.c
+++ src/libponyc/codegen/genexe.c
@@ -220,12 +220,14 @@ static bool link_exe(compile_t* c, ast_t* program,
   size_t ld_len = 128 + arch_len + strlen(file_exe) + strlen(file_o) +
     strlen(lib_args);
   char* ld_cmd = (char*)ponyint_pool_alloc_size(ld_len);
+  const char* linker = c->opt->linker != NULL ? c->opt->linker
+                                              : "ld";
 
-  // Avoid incorrect ld, eg from macports.
   snprintf(ld_cmd, ld_len,
-    "/usr/bin/ld -execute -no_pie -dead_strip -arch %.*s "
+    "%s -execute -no_pie -dead_strip -arch %.*s "
     "-macosx_version_min 10.8 -o %s %s %s %s -lSystem",
-    (int)arch_len, c->opt->triple, file_exe, file_o, lib_args, ponyrt
+           linker, (int)arch_len, c->opt->triple, file_exe, file_o,
+           lib_args, ponyrt
     );
 
   if(c->opt->verbosity >= VERBOSITY_TOOL_INFO)
