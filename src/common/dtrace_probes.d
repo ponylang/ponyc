b82be3e219d630696fea37c388007ce3ab8c7d03
Jip J. Dekker <jip@dekker.li>
2016-09-27 11:13:55 +0200

    Adds descriptions to the initial DTrace probe definitions

--- src/common/dtrace_probes.d
+++ src/common/dtrace_probes.d
@@ -1,4 +1,11 @@
 provider pony {
+  /**
+   * Fired when the garbage collection function has started
+   */
   probe gc__start();
+
+  /**
+   * Fired when the garbage collection function is ending
+   */
   probe gc__end();
 };
