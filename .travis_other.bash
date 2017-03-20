6a83c4c8b73c1682486c1190404e9393ae7171c2
Kevin Cantú <kevincantu@gmail.com>
2017-03-19 21:33:03 -0700

    Create shell scripts for Travis 'release' and other builds

--- .travis_other.bash
+++ .travis_other.bash
@@ -4,50 +4,30 @@ set +o errexit
 set +o nounset
 set +o
 
-if [[ "$TRAVIS_BRANCH" == "release" ]]
-then
-  if [[ "$config" != "release" || "${TRAVIS_OS_NAME}" != "linux" || "${LLVM_CONFIG}" != "llvm-config-3.9" ]]
-  then
-    echo "This is a release branch and there's nothing this matrix element must do."
-    exit 0
-  fi
-fi
-
-download-llvm(){
-  # Given the Travis LLVM version envvar,
-  # download an LLVM binary and install it.
-
-  # note 3.6.2 was different:
-  #wget "http://llvm.org/releases/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
-
-  wget "http://llvm.org/releases/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz"
-  tar -xvf clang+llvm*
-  cd clang+llvm* && sudo mkdir /tmp/llvm && sudo cp -r ./* /tmp/llvm/
-  sudo ln -s "/tmp/llvm/bin/llvm-config" "/usr/local/bin/${LLVM_CONFIG}"
-}
-
-download-pcre(){
-  # Download, build, and install PCRE 2.
-  wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.21.tar.bz2
-  tar -xjvf pcre2-10.21.tar.bz2
-  cd pcre2-10.21 && ./configure --prefix=/usr && make && sudo make install
-}
+here="$(cd "$(dirname "${0}")" && pwd)"
+source "${here}/.travis_common.bash"
 
 case "${TRAVIS_OS_NAME}:${LLVM_CONFIG}" in
 
   "linux:llvm-config-3.7")
     download-llvm
     download-pcre
+
+    make CC="$CC1" CXX="$CXX1" test-ci
   ;;
 
   "linux:llvm-config-3.8")
     download-llvm
     download-pcre
+
+    make CC="$CC1" CXX="$CXX1" test-ci
   ;;
 
   "linux:llvm-config-3.9")
     download-llvm
     download-pcre
+
+    make CC="$CC1" CXX="$CXX1" test-ci
   ;;
 
   "osx:llvm-config-3.7")
@@ -58,6 +38,8 @@ case "${TRAVIS_OS_NAME}:${LLVM_CONFIG}" in
     brew install libressl
 
     brew install llvm37
+
+    make CC="$CC1" CXX="$CXX1" test-ci
   ;;
 
   "osx:llvm-config-3.8")
@@ -68,6 +50,8 @@ case "${TRAVIS_OS_NAME}:${LLVM_CONFIG}" in
     brew install libressl
 
     brew install llvm38
+
+    make CC="$CC1" CXX="$CXX1" test-ci
   ;;
 
   "osx:llvm-config-3.9")
@@ -83,8 +67,8 @@ case "${TRAVIS_OS_NAME}:${LLVM_CONFIG}" in
     ln -s "$(which llvm-config)" llvmsym/llvm-config-3.9
     ln -s "$(which clang++)" llvmsym/clang++-3.9
 
-    # do this elsewhere:
-    #export PATH=llvmsym/:$PATH
+    export PATH=llvmsym/:$PATH
+    make CC="$CC1" CXX="$CXX1" test-ci
   ;;
 
   *)
