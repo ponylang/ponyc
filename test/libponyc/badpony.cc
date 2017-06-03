ponylang/ponyc

commit 3a36c0d50e8eb94d83da312c0ed92b6dd98dd352

Add bare methods and bare lambdas (RFC 34) (#1858)
These new language constructs can be used in FFI interoperation with C
libraries that use function pointers as callbacks.

Closes #1690.

src/libponyc/expr/reference.c
@@ -229,6 +229,8 @@ bool expr_typeref(pass_opt_t* opt, ast_t** astp)
   {
     case TK_QUALIFY:
     case TK_DOT:
+    case TK_TILDE:
+    case TK_CHAIN:
       break;
 
     case TK_CALL:
@@ -517,27 +519,77 @@ bool expr_addressof(pass_opt_t* opt, ast_t* ast)
       return false;
   }
 
-  // Set the type to Pointer[ast_type(expr)]. Set to Pointer[None] for function
-  // pointers.
   ast_t* expr_type = ast_type(expr);
 
   if(is_typecheck_error(expr_type))
     return false;
 
+  ast_t* type = NULL;
+
   switch(ast_id(expr))
   {
     case TK_FUNREF:
     case TK_BEREF:
+    {
       if(!method_check_type_params(opt, &expr))
         return false;
 
-      expr_type = type_builtin(opt, ast, "None");
-      break;
+      AST_GET_CHILDREN(expr, receiver, method);
+      if(ast_id(receiver) == ast_id(expr))
+        AST_GET_CHILDREN_NO_DECL(receiver, receiver, method);
 
-    default: {}
+      ast_t* def = lookup(opt, expr, ast_type(receiver), ast_name(method));
+      pony_assert((ast_id(def) == TK_FUN) || (ast_id(def) == TK_BE));
+
+      // Set the type to a bare lambda type equivalent to the function type.
+      bool bare = ast_id(ast_child(def)) == TK_AT;
+      ast_t* params = ast_childidx(def, 3);
+      ast_t* result = ast_sibling(params);
+      ast_t* partial = ast_sibling(result);
+
+      ast_t* lambdatype_params = ast_from(params, TK_NONE);
+      if(ast_id(params) != TK_NONE)
+      {
+        ast_setid(lambdatype_params, TK_PARAMS);
+        ast_t* param = ast_child(params);
+        while(param != NULL)
+        {
+          ast_t* param_type = ast_childidx(param, 1);
+          ast_append(lambdatype_params, param_type);
+          param = ast_sibling(param);
+        }
+      }
+
+      if(!bare)
+      {
+        ast_setid(lambdatype_params, TK_PARAMS);
+        ast_t* receiver_type = ast_type(receiver);
+        ast_add(lambdatype_params, receiver_type);
+      }
+
+      BUILD_NO_DECL(type, expr_type,
+        NODE(TK_BARELAMBDATYPE,
+          NONE // receiver cap
+          NONE // id
+          NONE // type parameters
+          TREE(lambdatype_params)
+          TREE(result)
+          TREE(partial)
+          NODE(TK_VAL) // object cap
+          NONE)); // object cap mod
+
+      if(!ast_passes_subtree(&type, opt, PASS_EXPR))
+        return false;
+
+      break;
+    }
+
+    default:
+      // Set the type to Pointer[ast_type(expr)].
+      type = type_pointer_to(opt, expr_type);
+      break;
   }
 
-  ast_t* type = type_pointer_to(opt, expr_type);
   ast_settype(ast, type);
   return true;
 }
@@ -572,6 +624,13 @@ bool expr_digestof(pass_opt_t* opt, ast_t* ast)
 
 bool expr_this(pass_opt_t* opt, ast_t* ast)
 {
+  if(ast_id(ast_child(opt->check.frame->method)) == TK_AT)
+  {
+    ast_error(opt->check.errors, ast,
+      "can't reference 'this' in a bare method");
+    return false;
+  }
+
   if(opt->check.frame->def_arg != NULL)
   {
     ast_error(opt->check.errors, ast,

examples/ffi-callbacks/callbacks.c
@@ -1,0 +1,6 @@
+typedef void(*CB)(void*, int);
+
+void do_callback(CB callback, void* env)
+{
+  callback(env, 10);
+}

examples/ffi-callbacks/callbacks.pony
@@ -1,0 +1,42 @@
+"""
+Example of the syntax used to pass Pony functions as callbacks to C functions.
+First, build the C file as a library, then build the Pony program.
+"""
+
+use "path:./"
+use "lib:ffi-callbacks"
+
+use @do_callback[None](fptr: CB, env: Env)
+
+// This is a bare lambda type, used to interface with C function pointer types.
+// The equivalent C type for this lambda type is `void(*)(Env*, int)` (bare
+// lambdas returning `None` are used for C functions returning `void`).
+type CB is @{(Env, I32)}
+
+// Bare lambda types can be used as field types to represent structures
+// containing function pointers.
+struct ContainsFunctionPointer
+  var ptr: CB = @{(env: Env, i: I32) => None}
+
+primitive Callback
+  // This is a bare function, i.e. a function with no receiver. Bare functions
+  // can be used as callbacks. Bare functions can be part of any type.
+  fun @callback(env: Env, i: I32) =>
+    env.out.print(i.string()) // Prints 10
+
+actor Main
+  new create(env: Env) =>
+    // Use `addressof` to get a function pointer to pass to C.
+    @do_callback(addressof Callback.callback, env)
+
+    // This is a bare lambda.
+    let callback = @{(env: Env, i: I32) =>
+      env.out.print(i.string()) // Prints 10
+    }
+    // The underlying value of a bare lambda is equivalent to a function pointer,
+    // which means that it can directly be passed as a callback.
+    @do_callback(callback, env)
+
+    let cfp = ContainsFunctionPointer
+    // The partial application of a bare method yields a bare lambda.
+    cfp.ptr = Callback~callback()

pony.g
@@ -34,7 +34,7 @@ field
   ;
 
 method
-  : ('fun' | 'be' | 'new') ('\\' ID (',' ID)* '\\')? cap? ID typeparams? ('(' | LPAREN_NEW) params? ')' (':' type)? '?'? STRING? ('if' rawseq)? ('=>' rawseq)?
+  : ('fun' | 'be' | 'new') ('\\' ID (',' ID)* '\\')? (cap | '@')? ID typeparams? ('(' | LPAREN_NEW) params? ')' (':' type)? '?'? STRING? ('if' rawseq)? ('=>' rawseq)?
   ;
 
 annotatedrawseq
@@ -202,6 +202,7 @@ nextatom
   | 'object' ('\\' ID (',' ID)* '\\')? cap? ('is' type)? members 'end'
   | '{' ('\\' ID (',' ID)* '\\')? cap? ID? typeparams? ('(' | LPAREN_NEW) params? ')' lambdacaptures? (':' type)? '?'? '=>' rawseq '}' cap?
   | 'lambda' ('\\' ID (',' ID)* '\\')? cap? ID? typeparams? ('(' | LPAREN_NEW) params? ')' lambdacaptures? (':' type)? '?'? '=>' rawseq 'end'
+  | '@{' ('\\' ID (',' ID)* '\\')? cap? ID? typeparams? ('(' | LPAREN_NEW) params? ')' lambdacaptures? (':' type)? '?'? '=>' rawseq '}' cap?
   | '@' (ID | STRING) typeargs? ('(' | LPAREN_NEW) positional? named? ')' '?'?
   | '__loc'
   ;
@@ -215,6 +216,7 @@ atom
   | 'object' ('\\' ID (',' ID)* '\\')? cap? ('is' type)? members 'end'
   | '{' ('\\' ID (',' ID)* '\\')? cap? ID? typeparams? ('(' | LPAREN_NEW) params? ')' lambdacaptures? (':' type)? '?'? '=>' rawseq '}' cap?
   | 'lambda' ('\\' ID (',' ID)* '\\')? cap? ID? typeparams? ('(' | LPAREN_NEW) params? ')' lambdacaptures? (':' type)? '?'? '=>' rawseq 'end'
+  | '@{' ('\\' ID (',' ID)* '\\')? cap? ID? typeparams? ('(' | LPAREN_NEW) params? ')' lambdacaptures? (':' type)? '?'? '=>' rawseq '}' cap?
   | '@' (ID | STRING) typeargs? ('(' | LPAREN_NEW) positional? named? ')' '?'?
   | '__loc'
   ;
@@ -253,6 +255,11 @@ atomtype
   | ('(' | LPAREN_NEW) infixtype tupletype? ')'
   | nominal
   | lambdatype
+  | barelambdatype
+  ;
+
+barelambdatype
+  : '@{' cap? ID? typeparams? ('(' | LPAREN_NEW) (type (',' type)*)? ')' (':' type)? '?'? '}' (cap | gencap)? ('^' | '!')?
   ;
 
 lambdatype

src/libponyc/ast/ast.c
@@ -40,7 +40,7 @@
  * no annotation, the type will be stored in the annotation_type field.
  *
  * These situations can be distinguished because an annotation must always be a
- * TK_BACKSLASH, and the type may never be a TK_BACKSLASH.
+ * TK_ANNOTATION, and the type may never be a TK_ANNOTATION.
  *
  * It is STRONGLY recommended to only access them through provided functions:
  *   ast_type()
@@ -55,7 +55,7 @@ enum
   AST_ORPHAN = 0x10,
   AST_INHERIT_FLAGS = (AST_FLAG_CAN_ERROR | AST_FLAG_CAN_SEND |
     AST_FLAG_MIGHT_SEND | AST_FLAG_RECURSE_1 | AST_FLAG_RECURSE_2),
-  AST_ALL_FLAGS = 0x1FFFFF
+  AST_ALL_FLAGS = 0x3FFFFF
 };
 
 
@@ -653,13 +653,13 @@ ast_t* ast_type(ast_t* ast)
   pony_assert(ast != NULL);
 
   // An annotation may never have a type.
-  if(ast_id(ast) == TK_BACKSLASH)
+  if(ast_id(ast) == TK_ANNOTATION)
     return NULL;
 
   // If the annotation_type is an annotation, the type node (if any) is in the
   // annotation_type field of the annotation node.
   ast_t* type = ast->annotation_type;
-  if((type != NULL) && (ast_id(type) == TK_BACKSLASH))
+  if((type != NULL) && (ast_id(type) == TK_ANNOTATION))
     type = type->annotation_type;
 
   return type;
@@ -670,12 +670,12 @@ static void settype(ast_t* ast, ast_t* type, bool allow_free)
   pony_assert(ast != NULL);
 
   // An annotation may never have a type.
-  if(ast_id(ast) == TK_BACKSLASH)
+  if(ast_id(ast) == TK_ANNOTATION)
     pony_assert(type == NULL);
 
-  // A type can never be a TK_BACKSLASH (the annotation token).
+  // A type can never be a TK_ANNOTATION.
   if(type != NULL)
-    pony_assert(ast_id(type) != TK_BACKSLASH);
+    pony_assert(ast_id(type) != TK_ANNOTATION);
 
   ast_t* prev_type = ast_type(ast);
   if(prev_type == type)
@@ -690,7 +690,7 @@ static void settype(ast_t* ast, ast_t* type, bool allow_free)
   }
 
   if((ast->annotation_type != NULL) &&
-    (ast_id(ast->annotation_type) == TK_BACKSLASH))
+    (ast_id(ast->annotation_type) == TK_ANNOTATION))
     ast->annotation_type->annotation_type = type;
   else
     ast->annotation_type = type;
@@ -709,12 +709,12 @@ ast_t* ast_annotation(ast_t* ast)
   pony_assert(ast != NULL);
 
   // An annotation may never be annotated.
-  if(ast_id(ast) == TK_BACKSLASH)
+  if(ast_id(ast) == TK_ANNOTATION)
     return NULL;
 
   // If annotation_type is an annotation, we return it.
   ast_t* annotation = ast->annotation_type;
-  if((annotation != NULL) && (ast_id(annotation) == TK_BACKSLASH))
+  if((annotation != NULL) && (ast_id(annotation) == TK_ANNOTATION))
     return annotation;
 
   return NULL;
@@ -725,12 +725,12 @@ void setannotation(ast_t* ast, ast_t* annotation, bool allow_free)
   pony_assert(ast != NULL);
 
   // An annotation may never be annotated.
-  if(ast_id(ast) == TK_BACKSLASH)
+  if(ast_id(ast) == TK_ANNOTATION)
     pony_assert(annotation == NULL);
 
-  // An annotation must always be a TK_BACKSLASH (or NULL).
+  // An annotation must always be a TK_ANNOTATION (or NULL).
   if(annotation != NULL)
-    pony_assert(ast_id(annotation) == TK_BACKSLASH);
+    pony_assert(ast_id(annotation) == TK_ANNOTATION);
 
   ast_t* prev_annotation = ast_annotation(ast);
   if(prev_annotation == annotation)
@@ -781,7 +781,7 @@ bool ast_has_annotation(ast_t* ast, const char* name)
 
   ast_t* annotation = ast_annotation(ast);
 
-  if((annotation != NULL) && (ast_id(annotation) == TK_BACKSLASH))
+  if((annotation != NULL) && (ast_id(annotation) == TK_ANNOTATION))
   {
     const char* strtab_name = stringtab(name);
     ast_t* elem = ast_child(annotation);

src/libponyc/ast/ast.h
@@ -45,6 +45,7 @@ enum
   AST_FLAG_ERROR_2      = 0x40000,
   AST_FLAG_JUMPS_AWAY   = 0x80000, // Jumps away (control flow) without a value.
   AST_FLAG_INCOMPLETE   = 0x100000, // Not all fields of `this` are defined yet.
+  AST_FLAG_IMPORT       = 0x200000, // Import the referenced package.
 };
 
 DECLARE_LIST(astlist, astlist_t, ast_t);

src/libponyc/ast/astbuild.h
@@ -29,6 +29,10 @@
  */
 #define BUILD(var, existing, ...) \
   ast_t* var; \
+  BUILD_NO_DECL(var, existing, __VA_ARGS__)
+
+/// Builds an AST but without declaring a new variable.
+#define BUILD_NO_DECL(var, existing, ...) \
   { \
     ast_t* basis_ast = existing; \
     ast_t* parent = NULL; \

src/libponyc/ast/lexer.c
@@ -84,6 +84,8 @@ static const lextoken_t symbols[] =
 
   { "\\", TK_BACKSLASH },
 
+  { "@{", TK_AT_LBRACE },
+
   { "{", TK_LBRACE },
   { "}", TK_RBRACE },
   { "(", TK_LPAREN },
@@ -243,6 +245,7 @@ static const lextoken_t abstract[] =
   { "thistype", TK_THISTYPE },
   { "funtype", TK_FUNTYPE },
   { "lambdatype", TK_LAMBDATYPE },
+  { "barelambdatype", TK_BARELAMBDATYPE },
   { "dontcaretype", TK_DONTCARETYPE },
   { "infer", TK_INFERTYPE },
   { "errortype", TK_ERRORTYPE },
@@ -264,6 +267,8 @@ static const lextoken_t abstract[] =
   { "updatearg", TK_UPDATEARG },
   { "lambdacaptures", TK_LAMBDACAPTURES },
   { "lambdacapture", TK_LAMBDACAPTURE },
+
+  { "barelambda", TK_BARELAMBDA },
 
   { "seq", TK_SEQ },
   { "qualify", TK_QUALIFY },
@@ -295,6 +300,8 @@ static const lextoken_t abstract[] =
   { "funapp", TK_FUNAPP },
   { "bechain", TK_BECHAIN },
   { "funchain", TK_FUNCHAIN },
+
+  { "annotation", TK_ANNOTATION },
 
   { "\\n", TK_NEWLINE },
   {NULL, (token_id)0}

commit 59e2a5dbf062070d46efd86be20f7bb7f1c9fb20

Fix compiler crash when a field is used in a default argument.
This was broken as a side effect the placement of a check
introduced in #1858, and was fixed by just changing the order
of the checks so that we don't expect to be in a method body frame
if we are in a default argument frame.

src/libponyc/expr/reference.c
@@ -624,17 +624,17 @@ bool expr_digestof(pass_opt_t* opt, ast_t* ast)
 
 bool expr_this(pass_opt_t* opt, ast_t* ast)
 {
-  if(ast_id(ast_child(opt->check.frame->method)) == TK_AT)
-  {
-    ast_error(opt->check.errors, ast,
-      "can't reference 'this' in a bare method");
-    return false;
-  }
-
   if(opt->check.frame->def_arg != NULL)
   {
     ast_error(opt->check.errors, ast,
       "can't reference 'this' in a default argument");
+    return false;
+  }
+
+  if(ast_id(ast_child(opt->check.frame->method)) == TK_AT)
+  {
+    ast_error(opt->check.errors, ast,
+      "can't reference 'this' in a bare method");
     return false;
   }
 

test/libponyc/badpony.cc
@@ -585,3 +585,17 @@ TEST_F(BadPonyTest, TypeArgErrorInsideReturn)
 
   TEST_ERRORS_1(src, "not enough type arguments");
 }
+
+TEST_F(BadPonyTest, FieldReferenceInDefaultArgument)
+{
+  const char* src =
+    "actor Main\n"
+    "  let _env: Env\n"
+    "  new create(env: Env) =>\n"
+    "    _env = env\n"
+    "    foo()\n"
+    "  fun foo(env: Env = _env) =>\n"
+    "    None";
+
+  TEST_ERRORS_1(src, "can't reference 'this' in a default argument");
+}