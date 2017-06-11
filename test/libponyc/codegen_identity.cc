ponylang/ponyc

commit b99d5b68faaafd5fe548e5f6bbd0489ae331146c

Fix identity comparison for boxed values (#1726)
Previously, the identity comparison for boxed values worked by
comparing the addresses of the boxes regardless of their contents. This
was leading to confusing behaviour, for example

  let a: U32 = 0
  let b: U32 = 0
  let boxa: Any = a
  let boxb: Any = b
  a is b // True.
  boxa is boxb // False.

Now, the identity of a boxed value (either a numeric primitive or a
tuple) depends on its content rather than its address. This affects the
`is`, `isnt` and `digestof` operators. Several general changes were
made to the code generator in order to make the identity comparison of
a boxed value as efficient as possible.

When comparing a possibly boxed value to an unboxed value, the type of
the box is checked first by looking at its type descriptor. If the
types of the values are the same, the boxed value is unboxed and the
comparison is done on the raw objects.

When comparing two possibly boxed values, the check depends on the
underlying types.

- First, the addresses are compared. If they are the same, the objects
  have the same identity regardless of whether they are boxed values or
  not
- If the addresses are different, we check whether the objects are two
  boxed values of the same type. This is done by checking the type IDs
  with a method described afterwards
  - If the values are two boxed numbers, the identity check is done
    with `memcmp` without unboxing
  - If the values are two boxed tuples, the identity check is done by
    calling a type-specific `__is` function through the vtable. This
    function unboxes the tuples and does the identity check
  - If the values aren't boxed or if they don't have the same type,
    they don't have the same identity.

When computing the digest of a possibly boxed value, we first check
whether the value is boxed by checking the type ID with the same method
as for the identity comparison. If the value is boxed, a type-specific

src/libponyc/codegen/genident.c
@@ -1,10 +1,70 @@
 #include "genident.h"
+#include "genbox.h"
+#include "gencall.h"
 #include "gendesc.h"
 #include "genexpr.h"
+#include "genopt.h"
+#include "../type/subtype.h"
+#include "../../libponyrt/mem/pool.h"
 #include "ponyassert.h"
+#include <string.h>
 
 static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
   ast_t* right_type, LLVMValueRef l_value, LLVMValueRef r_value);
+
+enum boxed_subtypes_t
+{
+  BOXED_SUBTYPES_NONE,
+  BOXED_SUBTYPES_NUMERIC = 1 << 0,
+  BOXED_SUBTYPES_TUPLE = 1 << 1,
+  BOXED_SUBTYPES_UNBOXED = 1 << 2,
+
+  BOXED_SUBTYPES_BOXED = BOXED_SUBTYPES_NUMERIC | BOXED_SUBTYPES_TUPLE,
+  BOXED_SUBTYPES_ALL = BOXED_SUBTYPES_BOXED | BOXED_SUBTYPES_UNBOXED
+};
+
+static int boxed_subtypes_overlap(reach_t* reach, ast_t* left_type,
+  ast_t* right_type)
+{
+  reach_type_t* r_left = reach_type(reach, left_type);
+  reach_type_t* r_right = reach_type(reach, right_type);
+
+  int subtypes = BOXED_SUBTYPES_NONE;
+
+  size_t i = HASHMAP_BEGIN;
+  reach_type_t* sub_left;
+
+  while((sub_left = reach_type_cache_next(&r_left->subtypes, &i)) != NULL)
+  {
+    if(!sub_left->can_be_boxed)
+    {
+      subtypes |= BOXED_SUBTYPES_UNBOXED;
+      if(subtypes == BOXED_SUBTYPES_ALL)
+        return subtypes;
+
+      continue;
+    }
+
+    size_t j = HASHMAP_BEGIN;
+    reach_type_t* sub_right;
+
+    while((sub_right = reach_type_cache_next(&r_right->subtypes, &j)) != NULL)
+    {
+      if(sub_left == sub_right)
+      {
+        if(sub_left->underlying == TK_PRIMITIVE)
+          subtypes |= BOXED_SUBTYPES_NUMERIC;
+        else
+          subtypes |= BOXED_SUBTYPES_TUPLE;
+
+        if(subtypes == BOXED_SUBTYPES_ALL)
+          return subtypes;
+      }
+    }
+  }
+
+  return subtypes;
+}
 
 static LLVMValueRef tuple_is(compile_t* c, ast_t* left_type, ast_t* right_type,
   LLVMValueRef l_value, LLVMValueRef r_value)
@@ -39,7 +99,8 @@ static LLVMValueRef tuple_is(compile_t* c, ast_t* left_type, ast_t* right_type,
 
     // If false, go directly to the post block.
     LLVMBuildCondBr(c->builder, test, next_block, post_block);
-    LLVMAddIncoming(phi, &test, &this_block, 1);
+    LLVMBasicBlockRef current_block = LLVMGetInsertBlock(c->builder);
+    LLVMAddIncoming(phi, &test, &current_block, 1);
 
     // Point to the next block.
     this_block = next_block;
@@ -61,6 +122,158 @@ static LLVMValueRef tuple_is(compile_t* c, ast_t* left_type, ast_t* right_type,
   return phi;
 }
 
+static LLVMValueRef raw_is_box(compile_t* c, ast_t* left_type,
+  LLVMValueRef l_value, LLVMValueRef r_value)
+{
+  pony_assert(LLVMGetTypeKind(LLVMTypeOf(r_value)) == LLVMPointerTypeKind);
+
+  LLVMValueRef r_desc = gendesc_fetch(c, r_value);
+  LLVMValueRef same_type = gendesc_isentity(c, r_desc, left_type);
+  pony_assert(same_type != GEN_NOVALUE);
+
+  LLVMBasicBlockRef this_block = LLVMGetInsertBlock(c->builder);
+  LLVMBasicBlockRef value_block = codegen_block(c, "is_value");
+  LLVMBasicBlockRef post_block = codegen_block(c, "is_post");
+  LLVMBuildCondBr(c->builder, same_type, value_block, post_block);
+
+  LLVMPositionBuilderAtEnd(c->builder, value_block);
+  r_value = gen_unbox(c, left_type, r_value);
+  LLVMValueRef is_value = gen_is_value(c, left_type, left_type, l_value,
+    r_value);
+  LLVMBuildBr(c->builder, post_block);
+  value_block = LLVMGetInsertBlock(c->builder);
+
+  LLVMPositionBuilderAtEnd(c->builder, post_block);
+  LLVMValueRef phi = LLVMBuildPhi(c->builder, c->i1, "");
+  LLVMValueRef zero = LLVMConstInt(c->i1, 0, false);
+  LLVMAddIncoming(phi, &is_value, &value_block, 1);
+  LLVMAddIncoming(phi, &zero, &this_block, 1);
+  return phi;
+}
+
+static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
+  LLVMValueRef l_value, LLVMValueRef r_value, int possible_boxes)
+{
+  pony_assert(LLVMGetTypeKind(LLVMTypeOf(l_value)) == LLVMPointerTypeKind);
+  pony_assert(LLVMGetTypeKind(LLVMTypeOf(r_value)) == LLVMPointerTypeKind);
+
+  LLVMBasicBlockRef this_block = LLVMGetInsertBlock(c->builder);
+  LLVMBasicBlockRef checkbox_block = codegen_block(c, "is_checkbox");
+  LLVMBasicBlockRef box_block = codegen_block(c, "is_box");
+  LLVMBasicBlockRef num_block = NULL;
+  if((possible_boxes & BOXED_SUBTYPES_NUMERIC) != 0)
+    num_block = codegen_block(c, "is_num");
+  LLVMBasicBlockRef tuple_block = NULL;
+  if((possible_boxes & BOXED_SUBTYPES_TUPLE) != 0)
+    tuple_block = codegen_block(c, "is_tuple");
+  LLVMBasicBlockRef post_block = codegen_block(c, "is_post");
+
+  LLVMValueRef eq_addr = LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value,
+    "");
+  LLVMBuildCondBr(c->builder, eq_addr, post_block, checkbox_block);
+
+  // Check whether we have two boxed objects of the same type.
+  LLVMPositionBuilderAtEnd(c->builder, checkbox_block);
+  LLVMValueRef l_desc = gendesc_fetch(c, l_value);
+  LLVMValueRef r_desc = gendesc_fetch(c, r_value);
+  LLVMValueRef same_type = LLVMBuildICmp(c->builder, LLVMIntEQ, l_desc, r_desc,
+    "");
+  LLVMValueRef l_typeid = NULL;
+  if((possible_boxes & BOXED_SUBTYPES_UNBOXED) != 0)
+  {
+    l_typeid = gendesc_typeid(c, l_value);
+    LLVMValueRef boxed_mask = LLVMConstInt(c->i32, 1, false);
+    LLVMValueRef left_boxed = LLVMBuildAnd(c->builder, l_typeid, boxed_mask,
+      "");
+    LLVMValueRef zero = LLVMConstInt(c->i32, 0, false);
+    left_boxed = LLVMBuildICmp(c->builder, LLVMIntEQ, left_boxed, zero, "");
+    LLVMValueRef both_boxed = LLVMBuildAnd(c->builder, same_type, left_boxed,
+      "");
+    LLVMBuildCondBr(c->builder, both_boxed, box_block, post_block);
+  } else {
+    LLVMBuildCondBr(c->builder, same_type, box_block, post_block);
+  }
+
+  // Check whether it's a numeric primitive or a tuple.
+  LLVMPositionBuilderAtEnd(c->builder, box_block);
+  if((possible_boxes & BOXED_SUBTYPES_BOXED) == BOXED_SUBTYPES_BOXED)
+  {
+    if(l_typeid == NULL)
+      l_typeid = gendesc_typeid(c, l_value);
+    LLVMValueRef num_mask = LLVMConstInt(c->i32, 2, false);
+    LLVMValueRef boxed_num = LLVMBuildAnd(c->builder, l_typeid, num_mask, "");
+    LLVMValueRef zero = LLVMConstInt(c->i32, 0, false);
+    boxed_num = LLVMBuildICmp(c->builder, LLVMIntEQ, boxed_num, zero, "");
+    LLVMBuildCondBr(c->builder, boxed_num, num_block, tuple_block);
+  } else if((possible_boxes & BOXED_SUBTYPES_NUMERIC) != 0) {
+    LLVMBuildBr(c->builder, num_block);
+  } else {
+    pony_assert((possible_boxes & BOXED_SUBTYPES_TUPLE) != 0);
+    LLVMBuildBr(c->builder, tuple_block);
+  }
+
+  LLVMValueRef args[3];
+  LLVMValueRef is_num = NULL;
+  if(num_block != NULL)
+  {
+    // Get the machine word size and memcmp without unboxing.
+    LLVMPositionBuilderAtEnd(c->builder, num_block);
+    if(l_typeid == NULL)
+      l_typeid = gendesc_typeid(c, l_value);
+    LLVMValueRef num_sizes = LLVMBuildBitCast(c->builder, c->numeric_sizes,
+      c->void_ptr, "");
+    args[0] = LLVMBuildZExt(c->builder, l_typeid, c->intptr, "");
+    LLVMValueRef size = LLVMBuildInBoundsGEP(c->builder, num_sizes, args, 1,
+      "");
+    size = LLVMBuildBitCast(c->builder, size, LLVMPointerType(c->i32, 0), "");
+    size = LLVMBuildLoad(c->builder, size, "");
+    LLVMSetAlignment(size, 4);
+    LLVMValueRef one = LLVMConstInt(c->i32, 1, false);
+    args[0] = LLVMBuildInBoundsGEP(c->builder, l_value, &one, 1, "");
+    args[0] = LLVMBuildBitCast(c->builder, args[0], c->void_ptr, "");
+    args[1] = LLVMBuildInBoundsGEP(c->builder, r_value, &one, 1, "");
+    args[1] = LLVMBuildBitCast(c->builder, args[1], c->void_ptr, "");
+    args[2] = LLVMBuildZExt(c->builder, size, c->intptr, "");
+    is_num = gencall_runtime(c, "memcmp", args, 3, "");
+    is_num = LLVMBuildICmp(c->builder, LLVMIntEQ, is_num,
+      LLVMConstInt(c->i32, 0, false), "");
+    LLVMBuildBr(c->builder, post_block);
+  }
+
+  LLVMValueRef is_tuple = NULL;
+  if(tuple_block != NULL)
+  {
+    // Call the type-specific __is function, which will unbox the tuples.
+    LLVMPositionBuilderAtEnd(c->builder, tuple_block);
+    reach_type_t* r_left = reach_type(c->reach, left_type);
+    reach_method_t* is_fn = reach_method(r_left, TK_BOX, stringtab("__is"),
+      NULL);
+    pony_assert(is_fn != NULL);
+    LLVMValueRef func = gendesc_vtable(c, l_value, is_fn->vtable_index);
+    LLVMTypeRef params[2];
+    params[0] = c->object_ptr;
+    params[1] = c->object_ptr;
+    LLVMTypeRef type = LLVMFunctionType(c->i1, params, 2, false);
+    func = LLVMBuildBitCast(c->builder, func, LLVMPointerType(type, 0), "");
+    args[0] = l_value;
+    args[1] = r_value;
+    is_tuple = codegen_call(c, func, args, 2);
+    LLVMBuildBr(c->builder, post_block);
+  }
+
+  LLVMPositionBuilderAtEnd(c->builder, post_block);
+  LLVMValueRef phi = LLVMBuildPhi(c->builder, c->i1, "");
+  LLVMValueRef one = LLVMConstInt(c->i1, 1, false);
+  LLVMValueRef zero = LLVMConstInt(c->i1, 0, false);
+  LLVMAddIncoming(phi, &one, &this_block, 1);
+  if(is_num != NULL)
+    LLVMAddIncoming(phi, &is_num, &num_block, 1);
+  if(is_tuple != NULL)
+    LLVMAddIncoming(phi, &is_tuple, &tuple_block, 1);
+  LLVMAddIncoming(phi, &zero, &checkbox_block, 1);
+  return phi;
+}
+
 static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
   ast_t* right_type, LLVMValueRef l_value, LLVMValueRef r_value)
 {
@@ -75,7 +288,12 @@ static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
       if(l_type == r_type)
         return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
 
-      // It can't have the same identity, even if it's the same value.
+      // If left_type is a subtype of right_type, check if r_value is a boxed
+      // numeric primitive.
+      if(is_subtype(left_type, right_type, NULL, c->opt))
+        return raw_is_box(c, left_type, l_value, r_value);
+
+      // It can't have the same identity.
       return LLVMConstInt(c->i1, 0, false);
     }
 
@@ -86,7 +304,12 @@ static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
       if(l_type == r_type)
         return LLVMBuildFCmp(c->builder, LLVMRealOEQ, l_value, r_value, "");
 
-      // It can't have the same identity, even if it's the same value.
+      // If left_type is a subtype of right_type, check if r_value is a boxed
+      // numeric primitive.
+      if(is_subtype(left_type, right_type, NULL, c->opt))
+        return raw_is_box(c, left_type,l_value, r_value);
+
+      // It can't have the same identity.
       return LLVMConstInt(c->i1, 0, false);
     }
 
@@ -96,20 +319,38 @@ static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
       if(LLVMGetTypeKind(r_type) == LLVMStructTypeKind)
         return tuple_is(c, left_type, right_type, l_value, r_value);
 
-      // It can't have the same identity, even if it's the same value.
+      // If left_type is a subtype of right_type, check if r_value is a boxed
+      // tuple.
+      if(is_subtype(left_type, right_type, NULL, c->opt))
+        return raw_is_box(c, left_type, l_value, r_value);
+
+      // It can't have the same identity.
       return LLVMConstInt(c->i1, 0, false);
     }
 
     case LLVMPointerTypeKind:
     {
-      // It can't have the same identity, even if it's the same value.
       if(LLVMGetTypeKind(r_type) != LLVMPointerTypeKind)
-        return LLVMConstInt(c->i1, 0, false);
+        return gen_is_value(c, right_type, left_type, r_value, l_value);
 
-      // Pointers must be to the same address.
       l_value = LLVMBuildBitCast(c->builder, l_value, c->object_ptr, "");
       r_value = LLVMBuildBitCast(c->builder, r_value, c->object_ptr, "");
-      return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
+
+      if(!is_known(left_type) && !is_known(right_type))
+      {
+        int possible_boxes = boxed_subtypes_overlap(c->reach, left_type,
+          right_type);
+        if((possible_boxes & BOXED_SUBTYPES_BOXED) != 0)
+          return box_is_box(c, left_type, l_value, r_value, possible_boxes);
+      }
+
+      // If the types can be the same, check the address.
+      if(is_subtype(left_type, right_type, NULL, c->opt) ||
+        is_subtype(right_type, left_type, NULL, c->opt))
+        return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
+
+      // It can't have the same identity.
+      return LLVMConstInt(c->i1, 0, false);
     }
 
     default: {}
@@ -158,4 +399,68 @@ LLVMValueRef gen_isnt(compile_t* c, ast_t* ast)
   LLVMValueRef value = LLVMBuildZExt(c->builder, result, c->ibool, "");
   codegen_debugloc(c, NULL);
   return value;
+}
+
+void gen_is_tuple_fun(compile_t* c, reach_type_t* t)
+{
+  pony_assert(t->underlying == TK_TUPLETYPE);
+
+  reach_method_t* m = reach_method(t, TK_BOX, stringtab("__is"), NULL);
+  pony_assert(m != NULL);
+
+  LLVMTypeRef params[2];
+  params[0] = t->structure_ptr;
+  params[1] = t->structure_ptr;
+  m->func_type = LLVMFunctionType(c->i1, params, 2, false);
+  m->func = codegen_addfun(c, m->full_name, m->func_type);
+
+  codegen_startfun(c, m->func, NULL, NULL);
+  LLVMValueRef l_value = LLVMGetParam(codegen_fun(c), 0);
+  LLVMValueRef r_value = LLVMGetParam(codegen_fun(c), 1);
+
+  l_value = gen_unbox(c, t->ast_cap, l_value);
+  r_value = gen_unbox(c, t->ast_cap, r_value);
+  LLVMBuildRet(c->builder, tuple_is(c, t->ast_cap, t->ast_cap, l_value,
+    r_value));
+
+  codegen_finishfun(c);
+}
+
+LLVMValueRef gen_numeric_size_table(compile_t* c)
+{
+  uint32_t len = c->reach->numeric_type_count;
+  if(len == 0)
+    return NULL;
+
+  size_t size = len * sizeof(LLVMValueRef);
+  LLVMValueRef* args = (LLVMValueRef*)ponyint_pool_alloc_size(size);
+
+  uint32_t count = 0;
+  reach_type_t* t;
+  size_t i = HASHMAP_BEGIN;
+
+  while(count < len)
+  {
+    t = reach_types_next(&c->reach->types, &i);
+    pony_assert(t != NULL);
+    uint32_t type_id = t->type_id;
+    if((type_id % 4) == 0)
+    {
+      size_t type_size = LLVMABISizeOfType(c->target_data, t->use_type);
+      args[type_id >> 2] = LLVMConstInt(c->i32, type_size, false);
+      count++;
+    }
+  }
+
+  LLVMTypeRef type = LLVMArrayType(c->i32, len);
+  LLVMValueRef table = LLVMAddGlobal(c->module, type, "__NumSizeTable");
+  LLVMValueRef value = LLVMConstArray(c->i32, args, len);
+  LLVMSetInitializer(table, value);
+  LLVMSetGlobalConstant(table, true);
+  LLVMSetAlignment(table, 4);
+  LLVMSetLinkage(table, LLVMPrivateLinkage);
+
+  ponyint_pool_free_size(size, args);
+
+  return table;
 }

test/libponyc/codegen_identity.cc
@@ -1,0 +1,261 @@
+#include <gtest/gtest.h>
+#include <platform.h>
+
+#include "util.h"
+
+#define TEST_COMPILE(src) DO(test_compile(src, "ir"))
+
+
+class CodegenIdentityTest : public PassTest
+{};
+
+
+extern "C"
+{
+
+EXPORT_SYMBOL intptr_t ptr_to_int(void* p)
+{
+  return (intptr_t)p;
+}
+
+}
+
+
+TEST_F(CodegenIdentityTest, ObjectIsObject)
+{
+  const char* src =
+    "class C1\n"
+    "class C2\n"
+
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let c1: Any = C1\n"
+    "    let c2: Any = C2\n"
+    "    if (c1 is c1) and (c1 isnt c2) then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, NumericIsNumeric)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    if (U32(0) is U32(0)) and (U32(0) isnt U32(1)) and\n"
+    "      (U32(0) isnt U64(0))\n"
+    "    then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, TupleIsTuple)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let a: (Any, Any) = (U32(0), env)\n"
+    "    let b: (Any, Any) = (U32(1), this)\n"
+    "    if (a is a) and (a isnt b) then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, NumericIsBoxedNumeric)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let boxed: (Any | (U32, U32)) = U32(0)\n"
+    "    if (U32(0) is boxed) and (U32(1) isnt boxed) and\n"
+    "      (U64(0) isnt boxed)\n"
+    "    then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, BoxedNumericIsBoxedNumeric)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let u32_0_a: (Any | (U32, U32)) = U32(0)\n"
+    "    let u32_0_b: (Any | (U32, U32)) = U32(0)\n"
+    "    let u32_1: (Any | (U32, U32)) = U32(1)\n"
+    "    let u64_0: (Any | (U32, U32)) = U64(0)\n"
+    "    if (u32_0_a is u32_0_a) and (u32_0_a is u32_0_b) and\n"
+    "      (u32_0_a isnt u32_1) and (u32_0_a isnt u64_0)\n"
+    "    then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, TupleIsBoxedTuple)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let boxed: (Any | (U32, U32) | (U64, U64)) = (U32(0), U32(0))\n"
+    "    if ((U32(0), U32(0)) is boxed) and ((U32(1), U32(0)) isnt boxed) and\n"
+    "      ((U64(0), U64(0)) isnt boxed)\n"
+    "    then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, BoxedTupleIsBoxedTuple)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let t1_a: (Any | (U32, U32) | (U64, U64)) = (U32(0), U32(0))\n"
+    "    let t1_b: (Any | (U32, U32) | (U64, U64)) = (U32(0), U32(0))\n"
+    "    let t2: (Any | (U32, U32) | (U64, U64)) = (U32(1), U32(0))\n"
+    "    let t3: (Any | (U32, U32) | (U64, U64)) = (U64(0), U64(0))\n"
+    "    if (t1_a is t1_a) and (t1_a is t1_b) and (t1_a isnt t2) and\n"
+    "      (t1_a isnt t3)\n"
+    "    then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, DigestofObject)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let dg = digestof env\n"
+    "    if dg == @ptr_to_int[ISize](env).u64() then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, DigestofNumeric)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let value = U32(5)\n"
+    "    let dg = digestof value\n"
+    "    if dg == 5 then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, DigestofTuple)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let value = (this, env)\n"
+    "    let dg = digestof value\n"
+    "    if dg == ((digestof this) xor (digestof env)) then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, DigestofBoxedNumeric)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let boxed: (Any | (U32, U32)) = U32(5)\n"
+    "    let dg = digestof boxed\n"
+    "    if dg == 5 then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
+TEST_F(CodegenIdentityTest, DigestofBoxedTuple)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let boxed: (Any | (Main, Env)) = (this, env)\n"
+    "    let dg = digestof boxed\n"
+    "    if dg == ((digestof this) xor (digestof env)) then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}

src/libponyc/codegen/codegen.c
@@ -763,6 +763,26 @@ static void init_runtime(compile_t* c)
   // i32 pony_personality_v0(...)
   type = LLVMFunctionType(c->i32, NULL, 0, true);
   c->personality = LLVMAddFunction(c->module, "pony_personality_v0", type);
+
+  // i32 memcmp(i8*, i8*, intptr)
+  params[0] = c->void_ptr;
+  params[1] = c->void_ptr;
+  params[2] = c->intptr;
+  type = LLVMFunctionType(c->i32, params, 3, false);
+  value = LLVMAddFunction(c->module, "memcmp", type);
+#if PONY_LLVM >= 309
+  LLVMAddAttributeAtIndex(value, LLVMAttributeFunctionIndex, nounwind_attr);
+  LLVMAddAttributeAtIndex(value, LLVMAttributeFunctionIndex, readonly_attr);
+  LLVMAddAttributeAtIndex(value, 1, readonly_attr);
+  LLVMAddAttributeAtIndex(value, 2, readonly_attr);
+#else
+  LLVMAddFunctionAttr(value, LLVMNoUnwindAttribute);
+  LLVMAddFunctionAttr(value, LLVMReadOnlyAttribute);
+  LLVMValueRef param = LLVMGetParam(value, 0);
+  LLVMAddAttribute(param, LLVMReadOnlyAttribute);
+  param = LLVMGetParam(value, 1);
+  LLVMAddAttribute(param, LLVMReadOnlyAttribute);
+#endif
 }
 
 static bool init_module(compile_t* c, ast_t* program, pass_opt_t* opt)
@@ -1010,6 +1030,7 @@ bool codegen_gen_test(compile_t* c, ast_t* program, pass_opt_t* opt)
 
   reach(c->reach, main_ast, c->str_create, NULL, opt);
   reach(c->reach, env_ast, c->str__create, NULL, opt);
+  reach_done(c->reach, c->opt);
 
   if(opt->limit == PASS_REACH)
     return true;

src/libponyc/codegen/codegen.h
@@ -174,6 +174,7 @@ typedef struct compile_t
   LLVMValueRef none_instance;
   LLVMValueRef primitives_init;
   LLVMValueRef primitives_final;
+  LLVMValueRef numeric_sizes;
 
   LLVMTypeRef void_type;
   LLVMTypeRef ibool;

commit 49362f2e1a489c290407657bce39a8d20430f7bb

Fix identity comparison of abstract types with no subtyping relationship
This was accidentally broken in b99d5b6.

src/libponyc/codegen/genident.c
@@ -101,7 +101,7 @@ static LLVMValueRef raw_is_box(compile_t* c, ast_t* left_type,
 }
 
 static LLVMValueRef box_is_box(compile_t* c, reach_type_t* left_type,
-  LLVMValueRef l_value, LLVMValueRef r_value, int possible_boxes)
+  LLVMValueRef l_value, LLVMValueRef r_value, int sub_kind)
 {
   pony_assert(LLVMGetTypeKind(LLVMTypeOf(l_value)) == LLVMPointerTypeKind);
   pony_assert(LLVMGetTypeKind(LLVMTypeOf(r_value)) == LLVMPointerTypeKind);
@@ -110,10 +110,10 @@ static LLVMValueRef box_is_box(compile_t* c, reach_type_t* left_type,
   LLVMBasicBlockRef checkbox_block = codegen_block(c, "is_checkbox");
   LLVMBasicBlockRef box_block = codegen_block(c, "is_box");
   LLVMBasicBlockRef num_block = NULL;
-  if((possible_boxes & SUBTYPE_KIND_NUMERIC) != 0)
+  if((sub_kind & SUBTYPE_KIND_NUMERIC) != 0)
     num_block = codegen_block(c, "is_num");
   LLVMBasicBlockRef tuple_block = NULL;
-  if((possible_boxes & SUBTYPE_KIND_TUPLE) != 0)
+  if((sub_kind & SUBTYPE_KIND_TUPLE) != 0)
     tuple_block = codegen_block(c, "is_tuple");
   LLVMBasicBlockRef post_block = codegen_block(c, "is_post");
 
@@ -128,7 +128,7 @@ static LLVMValueRef box_is_box(compile_t* c, reach_type_t* left_type,
   LLVMValueRef same_type = LLVMBuildICmp(c->builder, LLVMIntEQ, l_desc, r_desc,
     "");
   LLVMValueRef l_typeid = NULL;
-  if((possible_boxes & SUBTYPE_KIND_UNBOXED) != 0)
+  if((sub_kind & SUBTYPE_KIND_UNBOXED) != 0)
   {
     l_typeid = gendesc_typeid(c, l_desc);
     LLVMValueRef boxed_mask = LLVMConstInt(c->i32, 1, false);
@@ -145,7 +145,7 @@ static LLVMValueRef box_is_box(compile_t* c, reach_type_t* left_type,
 
   // Check whether it's a numeric primitive or a tuple.
   LLVMPositionBuilderAtEnd(c->builder, box_block);
-  if((possible_boxes & SUBTYPE_KIND_BOXED) == SUBTYPE_KIND_BOXED)
+  if((sub_kind & SUBTYPE_KIND_BOXED) == SUBTYPE_KIND_BOXED)
   {
     if(l_typeid == NULL)
       l_typeid = gendesc_typeid(c, l_desc);
@@ -154,10 +154,10 @@ static LLVMValueRef box_is_box(compile_t* c, reach_type_t* left_type,
     LLVMValueRef zero = LLVMConstInt(c->i32, 0, false);
     boxed_num = LLVMBuildICmp(c->builder, LLVMIntEQ, boxed_num, zero, "");
     LLVMBuildCondBr(c->builder, boxed_num, num_block, tuple_block);
-  } else if((possible_boxes & SUBTYPE_KIND_NUMERIC) != 0) {
+  } else if((sub_kind & SUBTYPE_KIND_NUMERIC) != 0) {
     LLVMBuildBr(c->builder, num_block);
   } else {
-    pony_assert((possible_boxes & SUBTYPE_KIND_TUPLE) != 0);
+    pony_assert((sub_kind & SUBTYPE_KIND_TUPLE) != 0);
     LLVMBuildBr(c->builder, tuple_block);
   }
 
@@ -286,20 +286,42 @@ static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
       l_value = LLVMBuildBitCast(c->builder, l_value, c->object_ptr, "");
       r_value = LLVMBuildBitCast(c->builder, r_value, c->object_ptr, "");
 
-      if(!is_known(left_type) && !is_known(right_type))
+      bool left_known = is_known(left_type);
+      bool right_known = is_known(right_type);
+      reach_type_t* r_left = reach_type(c->reach, left_type);
+      reach_type_t* r_right = reach_type(c->reach, right_type);
+
+      if(!left_known && !right_known)
       {
-        reach_type_t* r_left = reach_type(c->reach, left_type);
-        reach_type_t* r_right = reach_type(c->reach, right_type);
-        int possible_boxes = subtype_kind_overlap(r_left, r_right);
+        int sub_kind = subtype_kind_overlap(r_left, r_right);
 
-        if((possible_boxes & SUBTYPE_KIND_BOXED) != 0)
-          return box_is_box(c, r_left, l_value, r_value, possible_boxes);
+        if((sub_kind & SUBTYPE_KIND_BOXED) != 0)
+          return box_is_box(c, r_left, l_value, r_value, sub_kind);
+
+        // If the types can be the same, check the address.
+        if(sub_kind != SUBTYPE_KIND_NONE)
+          return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
+      } else if(left_known && right_known) {
+        // If the types are the same, check the address.
+        if(r_left == r_right)
+          return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
+      } else {
+        ast_t* known;
+        ast_t* unknown;
+
+        if(left_known)
+        {
+          known = left_type;
+          unknown = right_type;
+        } else {
+          known = right_type;
+          unknown = left_type;
+        }
+
+        // If the types can be the same, check the address.
+        if(is_subtype(known, unknown, NULL, c->opt))
+          return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
       }
-
-      // If the types can be the same, check the address.
-      if(is_subtype(left_type, right_type, NULL, c->opt) ||
-        is_subtype(right_type, left_type, NULL, c->opt))
-        return LLVMBuildICmp(c->builder, LLVMIntEQ, l_value, r_value, "");
 
       // It can't have the same identity.
       return LLVMConstInt(c->i1, 0, false);

test/libponyc/codegen_identity.cc
@@ -184,6 +184,25 @@ TEST_F(CodegenIdentityTest, TupleCardinality)
 }
 
 
+TEST_F(CodegenIdentityTest, AbstractTypeNoSubtyping)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let a: (Env | None) = None\n"
+    "    let b: (Main | None) = None\n"
+    "    if a is b then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
 TEST_F(CodegenIdentityTest, DigestofObject)
 {
   const char* src =