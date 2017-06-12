34a38101652214470311e39a7a1a7b02ac2ded45
Paul Lietar <paul@lietar.net>
2017-06-12 05:21:12 +0100

    Fix support for recursive constraints in iftype conditions.
    By defining the shadowing TK_TYPEPARAM on the entire iftype clause, which
    contains both the constraint and the body, the appropriate definition
    of the type variable is used when it appears in the constraint.
    
    Fixes #1960

--- src/libponyc/pass/scope.c
+++ src/libponyc/pass/scope.c
@@ -203,7 +203,7 @@ static ast_result_t scope_iftype_clause(pass_opt_t* opt, ast_t* ast)
 {
   pony_assert(ast_id(ast) == TK_IFTYPE_CLAUSE);
 
-  AST_GET_CHILDREN(ast, subtype, supertype, body);
+  AST_GET_CHILDREN(ast, subtype, supertype);
 
   ast_t* typeparams = ast_from(ast, TK_TYPEPARAMS);
 
@@ -211,14 +211,14 @@ static ast_result_t scope_iftype_clause(pass_opt_t* opt, ast_t* ast)
   {
     case TK_NOMINAL:
     {
-      ast_t* typeparam = make_iftype_typeparam(opt, subtype, supertype, body);
+      ast_t* typeparam = make_iftype_typeparam(opt, subtype, supertype, ast);
       if(typeparam == NULL)
       {
         ast_free_unattached(typeparams);
         return AST_ERROR;
       }
 
-      if(!set_scope(opt, body, ast_child(typeparam), typeparam, true))
+      if(!set_scope(opt, ast, ast_child(typeparam), typeparam, true))
       {
         ast_free_unattached(typeparams);
         return AST_ERROR;
@@ -253,14 +253,14 @@ static ast_result_t scope_iftype_clause(pass_opt_t* opt, ast_t* ast)
       while(sub_child != NULL)
       {
         ast_t* typeparam = make_iftype_typeparam(opt, sub_child, super_child,
-          body);
+          ast);
         if(typeparam == NULL)
         {
           ast_free_unattached(typeparams);
           return AST_ERROR;
         }
 
-        if(!set_scope(opt, body, ast_child(typeparam), typeparam, true))
+        if(!set_scope(opt, ast, ast_child(typeparam), typeparam, true))
         {
           ast_free_unattached(typeparams);
           return AST_ERROR;
