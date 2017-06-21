ponylang/ponyc

commit 9b9c084ccdb69d62a51738a90c161351a3981f6c

Only generate internal methods when needed
This change is an optimisation allowing the compiler to generate the
`__is` and `__digestof` methods for a type only when needed. This saves
a vtable slot for each method which isn't generated.

src/libponyc/reach/reach.c
@@ -1,4 +1,5 @@
 #include "reach.h"
+#include "subtype.h"
 #include "../ast/astbuild.h"
 #include "../codegen/genname.h"
 #include "../pass/expr.h"
@@ -12,8 +13,9 @@
 #include <stdio.h>
 #include <string.h>
 
-DEFINE_STACK(reach_method_stack, reach_method_stack_t,
-  reach_method_t);
+DEFINE_STACK(reachable_expr_stack, reachable_expr_stack_t, ast_t);
+
+DEFINE_STACK(reach_method_stack, reach_method_stack_t, reach_method_t);
 
 static reach_method_t* add_rmethod(reach_t* r, reach_type_t* t,
   reach_method_name_t* n, token_id cap, ast_t* typeargs, pass_opt_t* opt,
@@ -219,12 +221,24 @@ static const char* make_full_name(reach_type_t* t, reach_method_t* m)
   return name;
 }
 
+bool valid_internal_method_for_type(reach_type_t* type, const char* method_name)
+{
+  if(method_name == stringtab("__is"))
+    return type->underlying == TK_TUPLETYPE;
+
+  if(method_name == stringtab("__digestof"))
+    return type->can_be_boxed;
+
+  pony_assert(0);
+  return false;
+}
+
 static void add_rmethod_to_subtype(reach_t* r, reach_type_t* t,
-  reach_method_name_t* n, reach_method_t* m, pass_opt_t* opt)
+  reach_method_name_t* n, reach_method_t* m, pass_opt_t* opt, bool internal)
 {
   // Add the method to the type if it isn't already there.
-  reach_method_name_t* n2 = add_method_name(t, n->name, false);
-  add_rmethod(r, t, n2, m->cap, m->typeargs, opt, false);
+  reach_method_name_t* n2 = add_method_name(t, n->name, internal);
+  add_rmethod(r, t, n2, m->cap, m->typeargs, opt, internal);
 
   // Add this mangling to the type if it isn't already there.
   size_t index = HASHMAP_UNKNOWN;
@@ -258,59 +272,43 @@ static void add_rmethod_to_subtype(reach_t* r, reach_type_t* t,
 }
 
 static void add_rmethod_to_subtypes(reach_t* r, reach_type_t* t,
-  reach_method_name_t* n, reach_method_t* m, pass_opt_t* opt)
+  reach_method_name_t* n, reach_method_t* m, pass_opt_t* opt, bool internal)
 {
-  switch(ast_id(t->ast))
+  bool typeexpr = false;
+  switch(t->underlying)
   {
-    case TK_NOMINAL:
-    {
-      ast_t* def = (ast_t*)ast_data(t->ast);
-
-      switch(ast_id(def))
-      {
-        case TK_INTERFACE:
-        case TK_TRAIT:
-        {
-          // Add to subtypes if we're an interface or trait.
-          size_t i = HASHMAP_BEGIN;
-          reach_type_t* t2;
-
-          while((t2 = reach_type_cache_next(&t->subtypes, &i)) != NULL)
-            add_rmethod_to_subtype(r, t2, n, m, opt);
-
-          break;
-        }
-
-        default: {}
-      }
-      return;
-    }
-
     case TK_UNIONTYPE:
     case TK_ISECTTYPE:
+      typeexpr = true;
+      // fallthrough
+
+    case TK_INTERFACE:
+    case TK_TRAIT:
     {
-      ast_t* child = ast_child(t->ast);
+      size_t i = HASHMAP_BEGIN;
+      reach_type_t* t2;
 
-      while(child != NULL)
+      while((t2 = reach_type_cache_next(&t->subtypes, &i)) != NULL)
       {
-        ast_t* find = lookup_try(NULL, NULL, child, n->name);
-
-        if(find != NULL)
+        if(!internal && typeexpr)
         {
-          reach_type_t* t2 = add_type(r, child, opt);
-          add_rmethod_to_subtype(r, t2, n, m, opt);
+          ast_t* find = lookup_try(NULL, NULL, t2->ast, n->name);
+
+          if(find == NULL)
+            continue;
+
           ast_free_unattached(find);
         }
 
-        child = ast_sibling(child);
+        if(!internal || valid_internal_method_for_type(t2, n->name))
+          add_rmethod_to_subtype(r, t2, n, m, opt, internal);
       }
-      return;
+
+      break;
     }
 
     default: {}
   }
-
-  pony_assert(0);
 }
 
 static reach_method_t* add_rmethod(reach_t* r, reach_type_t* t,
@@ -361,11 +359,11 @@ static reach_method_t* add_rmethod(reach_t* r, reach_type_t* t,
   if(!internal)
   {
     // Put on a stack of reachable methods to trace.
-    r->stack = reach_method_stack_push(r->stack, m);
-
-    // Add the method to any subtypes.
-    add_rmethod_to_subtypes(r, t, n, m, opt);
+    r->method_stack = reach_method_stack_push(r->method_stack, m);
   }
+
+  // Add the method to any subtypes.
+  add_rmethod_to_subtypes(r, t, n, m, opt, internal);
 
   return m;
 }
@@ -378,14 +376,11 @@ static void add_methods_to_type(reach_t* r, reach_type_t* from,
 
   while((n = reach_method_names_next(&from->methods, &i)) != NULL)
   {
-    if(n->internal)
-      continue;
-
     size_t j = HASHMAP_BEGIN;
     reach_method_t* m;
 
     while((m = reach_mangled_next(&n->r_mangled, &j)) != NULL)
-      add_rmethod_to_subtype(r, to, n, m, opt);
+      add_rmethod_to_subtype(r, to, n, m, opt, m->internal);
   }
 }
 
@@ -739,12 +734,6 @@ static reach_type_t* add_tuple(reach_t* r, ast_t* type, pass_opt_t* opt)
 
   add_traits_to_type(r, t, opt);
 
-  reach_method_name_t* n = add_method_name(t, stringtab("__is"), true);
-  add_rmethod(r, t, n, TK_BOX, NULL, opt, true);
-
-  n = add_method_name(t, stringtab("__digestof"), true);
-  add_rmethod(r, t, n, TK_BOX, NULL, opt, true);
-
   printbuf_t* mangle = printbuf_new();
   printbuf(mangle, "%d", t->field_count);
 
@@ -799,12 +788,7 @@ static reach_type_t* add_nominal(reach_t* r, ast_t* type, pass_opt_t* opt)
       add_special(r, t, type, "_init", opt);
       add_special(r, t, type, "_final", opt);
       if(is_machine_word(type))
-      {
-        reach_method_name_t* n = add_method_name(t, stringtab("__digestof"),
-          true);
-        add_rmethod(r, t, n, TK_BOX, NULL, opt, true);
         t->can_be_boxed = true;
-      }
       break;
 
     case TK_STRUCT:
@@ -1058,6 +1042,51 @@ static void reachable_ffi(reach_t* r, ast_t* ast, pass_opt_t* opt)
   }
 }
 
+static void reachable_identity(reach_t* r, ast_t* ast, pass_opt_t* opt)
+{
+  AST_GET_CHILDREN(ast, left, right);
+
+  ast_t* l_type = ast_type(left);
+  ast_t* r_type = ast_type(right);
+
+  if(!is_known(l_type) && !is_known(r_type))
+  {
+    reach_type_t* r_left = reach_type(r, l_type);
+    reach_type_t* r_right = reach_type(r, r_type);
+    int sub_kind = subtype_kind_overlap(r_left, r_right);
+
+    if((sub_kind & SUBTYPE_KIND_TUPLE) != 0)
+    {
+      const char* name = stringtab("__is");
+
+      reach_method_name_t* n = add_method_name(r_left, name, true);
+      add_rmethod(r, r_left, n, TK_BOX, NULL, opt, true);
+
+      n = add_method_name(r_right, name, true);
+      add_rmethod(r, r_right, n, TK_BOX, NULL, opt, true);
+    }
+  }
+}
+
+static void reachable_digestof(reach_t* r, ast_t* ast, pass_opt_t* opt)
+{
+  ast_t* expr = ast_child(ast);
+  ast_t* type = ast_type(expr);
+
+  if(!is_known(type))
+  {
+    reach_type_t* t = reach_type(r, type);
+    int sub_kind = subtype_kind(t);
+
+    if((sub_kind & SUBTYPE_KIND_BOXED) != 0)
+    {
+      const char* name = stringtab("__digestof");
+      reach_method_name_t* n = add_method_name(t, name, true);
+      add_rmethod(r, t, n, TK_BOX, NULL, opt, true);
+    }
+  }
+}
+
 static void reachable_expr(reach_t* r, ast_t* ast, pass_opt_t* opt)
 {
   // If this is a method call, mark the method as reachable.
@@ -1162,6 +1191,7 @@ static void reachable_expr(reach_t* r, ast_t* ast, pass_opt_t* opt)
     }
 
     case TK_IS:
+    case TK_ISNT:
     {
       AST_GET_CHILDREN(ast, left, right);
 
@@ -1170,6 +1200,11 @@ static void reachable_expr(reach_t* r, ast_t* ast, pass_opt_t* opt)
 
       add_type(r, l_type, opt);
       add_type(r, r_type, opt);
+
+      // We might need to reach the `__is` method but we can't determine that
+      // yet since we don't know every reachable type.
+      // Put it on the expr stack in order to trace it later.
+      r->expr_stack = reachable_expr_stack_push(r->expr_stack, ast);
       break;
     }
 
@@ -1179,6 +1214,9 @@ static void reachable_expr(reach_t* r, ast_t* ast, pass_opt_t* opt)
       ast_t* type = ast_type(expr);
 
       add_type(r, type, opt);
+
+      // Same as TK_IS but for `__digestof`
+      r->expr_stack = reachable_expr_stack_push(r->expr_stack, ast);
       break;
     }
 
@@ -1248,12 +1286,37 @@ static void reachable_method(reach_t* r, ast_t* type, const char* name,
   }
 }
 
-static void handle_stack(reach_t* r, pass_opt_t* opt)
+static void handle_expr_stack(reach_t* r, pass_opt_t* opt)
 {
-  while(r->stack != NULL)
+  // New types must not be reached by this function. New methods are fine.
+
+  while(r->expr_stack != NULL)
+  {
+    ast_t* ast;
+    r->expr_stack = reachable_expr_stack_pop(r->expr_stack, &ast);
+
+    switch(ast_id(ast))
+    {
+      case TK_IS:
+      case TK_ISNT:
+        reachable_identity(r, ast, opt);
+        break;
+
+      case TK_DIGESTOF:
+        reachable_digestof(r, ast, opt);
+        break;
+
+      default: {}
+    }
+  }
+}
+
+static void handle_method_stack(reach_t* r, pass_opt_t* opt)
+{
+  while(r->method_stack != NULL)
   {
     reach_method_t* m;
-    r->stack = reach_method_stack_pop(r->stack, &m);
+    r->method_stack = reach_method_stack_pop(r->method_stack, &m);
 
     ast_t* body = ast_childidx(m->r_fun, 6);
     reachable_expr(r, body, opt);
@@ -1263,7 +1326,8 @@ static void handle_stack(reach_t* r, pass_opt_t* opt)
 reach_t* reach_new()
 {
   reach_t* r = POOL_ALLOC(reach_t);
-  r->stack = NULL;
+  r->expr_stack = NULL;
+  r->method_stack = NULL;
   r->object_type_count = 0;
   r->numeric_type_count = 0;
   r->tuple_type_count = 0;
@@ -1286,27 +1350,13 @@ void reach(reach_t* r, ast_t* type, const char* name, ast_t* typeargs,
   pass_opt_t* opt)
 {
   reachable_method(r, type, name, typeargs, opt);
-  handle_stack(r, opt);
-}
-
-static void add_internal_rmethod_to_traits(reach_t* r, reach_type_t* t,
-  const char* name, pass_opt_t* opt)
-{
-  size_t i = HASHMAP_BEGIN;
-  reach_type_t* super;
-
-  // For concrete types, subtypes is a map of supertypes.
-  while((super = reach_type_cache_next(&t->subtypes, &i)) != NULL)
-  {
-    reach_method_name_t* n = add_method_name(super, name, true);
-    add_rmethod(r, super, n, TK_BOX, NULL, opt, true);
-  }
+  handle_method_stack(r, opt);
 }
 
 void reach_done(reach_t* r, pass_opt_t* opt)
 {
   // Type IDs are assigned as:
-  // - Object type IDs:   1, 3, 5, 7, 9, ...
+  // - Object type IDs:  1, 3, 5, 7, 9, ...
   // - Numeric type IDs: 0, 4, 8, 12, 16, ...
   // - Tuple type IDs:   2, 6, 10, 14, 18, ...
   // This allows to quickly check whether a type is unboxed or not.
@@ -1315,32 +1365,7 @@ void reach_done(reach_t* r, pass_opt_t* opt)
   r->total_type_count = r->object_type_count + r->numeric_type_count +
     r->tuple_type_count;
 
-  size_t i = HASHMAP_BEGIN;
-  reach_type_t* t;
-  while((t = reach_types_next(&r->types, &i)) != NULL)
-  {
-    switch(t->underlying)
-    {
-      case TK_UNIONTYPE:
-      case TK_ISECTTYPE:
-      case TK_INTERFACE:
-      case TK_TRAIT:
-        continue;
-
-      default: {}
-    }
-
-    // Add internal methods to supertypes.
-    size_t j = HASHMAP_BEGIN;
-    reach_method_name_t* n;
-    while((n = reach_method_names_next(&t->methods, &j)) != NULL)
-    {
-      if(!n->internal)
-        continue;
-
-      add_internal_rmethod_to_traits(r, t, n->name, opt);
-    }
-  }
+  handle_expr_stack(r, opt);
 }
 
 reach_type_t* reach_type(reach_t* r, ast_t* type)

src/libponyc/codegen/genident.c
@@ -4,6 +4,7 @@
 #include "gendesc.h"
 #include "genexpr.h"
 #include "genopt.h"
+#include "../reach/subtype.h"
 #include "../type/subtype.h"
 #include "../../libponyrt/mem/pool.h"
 #include "ponyassert.h"
@@ -11,60 +12,6 @@
 
 static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
   ast_t* right_type, LLVMValueRef l_value, LLVMValueRef r_value);
-
-enum boxed_subtypes_t
-{
-  BOXED_SUBTYPES_NONE,
-  BOXED_SUBTYPES_NUMERIC = 1 << 0,
-  BOXED_SUBTYPES_TUPLE = 1 << 1,
-  BOXED_SUBTYPES_UNBOXED = 1 << 2,
-
-  BOXED_SUBTYPES_BOXED = BOXED_SUBTYPES_NUMERIC | BOXED_SUBTYPES_TUPLE,
-  BOXED_SUBTYPES_ALL = BOXED_SUBTYPES_BOXED | BOXED_SUBTYPES_UNBOXED
-};
-
-static int boxed_subtypes_overlap(reach_t* reach, ast_t* left_type,
-  ast_t* right_type)
-{
-  reach_type_t* r_left = reach_type(reach, left_type);
-  reach_type_t* r_right = reach_type(reach, right_type);
-
-  int subtypes = BOXED_SUBTYPES_NONE;
-
-  size_t i = HASHMAP_BEGIN;
-  reach_type_t* sub_left;
-
-  while((sub_left = reach_type_cache_next(&r_left->subtypes, &i)) != NULL)
-  {
-    if(!sub_left->can_be_boxed)
-    {
-      subtypes |= BOXED_SUBTYPES_UNBOXED;
-      if(subtypes == BOXED_SUBTYPES_ALL)
-        return subtypes;
-
-      continue;
-    }
-
-    size_t j = HASHMAP_BEGIN;
-    reach_type_t* sub_right;
-
-    while((sub_right = reach_type_cache_next(&r_right->subtypes, &j)) != NULL)
-    {
-      if(sub_left == sub_right)
-      {
-        if(sub_left->underlying == TK_PRIMITIVE)
-          subtypes |= BOXED_SUBTYPES_NUMERIC;
-        else
-          subtypes |= BOXED_SUBTYPES_TUPLE;
-
-        if(subtypes == BOXED_SUBTYPES_ALL)
-          return subtypes;
-      }
-    }
-  }
-
-  return subtypes;
-}
 
 static LLVMValueRef tuple_is(compile_t* c, ast_t* left_type, ast_t* right_type,
   LLVMValueRef l_value, LLVMValueRef r_value)
@@ -153,7 +100,7 @@ static LLVMValueRef raw_is_box(compile_t* c, ast_t* left_type,
   return phi;
 }
 
-static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
+static LLVMValueRef box_is_box(compile_t* c, reach_type_t* left_type,
   LLVMValueRef l_value, LLVMValueRef r_value, int possible_boxes)
 {
   pony_assert(LLVMGetTypeKind(LLVMTypeOf(l_value)) == LLVMPointerTypeKind);
@@ -163,10 +110,10 @@ static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
   LLVMBasicBlockRef checkbox_block = codegen_block(c, "is_checkbox");
   LLVMBasicBlockRef box_block = codegen_block(c, "is_box");
   LLVMBasicBlockRef num_block = NULL;
-  if((possible_boxes & BOXED_SUBTYPES_NUMERIC) != 0)
+  if((possible_boxes & SUBTYPE_KIND_NUMERIC) != 0)
     num_block = codegen_block(c, "is_num");
   LLVMBasicBlockRef tuple_block = NULL;
-  if((possible_boxes & BOXED_SUBTYPES_TUPLE) != 0)
+  if((possible_boxes & SUBTYPE_KIND_TUPLE) != 0)
     tuple_block = codegen_block(c, "is_tuple");
   LLVMBasicBlockRef post_block = codegen_block(c, "is_post");
 
@@ -181,7 +128,7 @@ static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
   LLVMValueRef same_type = LLVMBuildICmp(c->builder, LLVMIntEQ, l_desc, r_desc,
     "");
   LLVMValueRef l_typeid = NULL;
-  if((possible_boxes & BOXED_SUBTYPES_UNBOXED) != 0)
+  if((possible_boxes & SUBTYPE_KIND_UNBOXED) != 0)
   {
     l_typeid = gendesc_typeid(c, l_desc);
     LLVMValueRef boxed_mask = LLVMConstInt(c->i32, 1, false);
@@ -198,7 +145,7 @@ static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
 
   // Check whether it's a numeric primitive or a tuple.
   LLVMPositionBuilderAtEnd(c->builder, box_block);
-  if((possible_boxes & BOXED_SUBTYPES_BOXED) == BOXED_SUBTYPES_BOXED)
+  if((possible_boxes & SUBTYPE_KIND_BOXED) == SUBTYPE_KIND_BOXED)
   {
     if(l_typeid == NULL)
       l_typeid = gendesc_typeid(c, l_desc);
@@ -207,10 +154,10 @@ static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
     LLVMValueRef zero = LLVMConstInt(c->i32, 0, false);
     boxed_num = LLVMBuildICmp(c->builder, LLVMIntEQ, boxed_num, zero, "");
     LLVMBuildCondBr(c->builder, boxed_num, num_block, tuple_block);
-  } else if((possible_boxes & BOXED_SUBTYPES_NUMERIC) != 0) {
+  } else if((possible_boxes & SUBTYPE_KIND_NUMERIC) != 0) {
     LLVMBuildBr(c->builder, num_block);
   } else {
-    pony_assert((possible_boxes & BOXED_SUBTYPES_TUPLE) != 0);
+    pony_assert((possible_boxes & SUBTYPE_KIND_TUPLE) != 0);
     LLVMBuildBr(c->builder, tuple_block);
   }
 
@@ -247,8 +194,7 @@ static LLVMValueRef box_is_box(compile_t* c, ast_t* left_type,
   {
     // Call the type-specific __is function, which will unbox the tuples.
     LLVMPositionBuilderAtEnd(c->builder, tuple_block);
-    reach_type_t* r_left = reach_type(c->reach, left_type);
-    reach_method_t* is_fn = reach_method(r_left, TK_BOX, stringtab("__is"),
+    reach_method_t* is_fn = reach_method(left_type, TK_BOX, stringtab("__is"),
       NULL);
     pony_assert(is_fn != NULL);
     LLVMValueRef func = gendesc_vtable(c, l_desc, is_fn->vtable_index);
@@ -340,10 +286,12 @@ static LLVMValueRef gen_is_value(compile_t* c, ast_t* left_type,
 
       if(!is_known(left_type) && !is_known(right_type))
       {
-        int possible_boxes = boxed_subtypes_overlap(c->reach, left_type,
-          right_type);
-        if((possible_boxes & BOXED_SUBTYPES_BOXED) != 0)
-          return box_is_box(c, left_type, l_value, r_value, possible_boxes);
+        reach_type_t* r_left = reach_type(c->reach, left_type);
+        reach_type_t* r_right = reach_type(c->reach, right_type);
+        int possible_boxes = subtype_kind_overlap(r_left, r_right);
+
+        if((possible_boxes & SUBTYPE_KIND_BOXED) != 0)
+          return box_is_box(c, r_left, l_value, r_value, possible_boxes);
       }
 
       // If the types can be the same, check the address.
@@ -408,7 +356,9 @@ void gen_is_tuple_fun(compile_t* c, reach_type_t* t)
   pony_assert(t->underlying == TK_TUPLETYPE);
 
   reach_method_t* m = reach_method(t, TK_BOX, stringtab("__is"), NULL);
-  pony_assert(m != NULL);
+
+  if(m == NULL)
+    return;
 
   LLVMTypeRef params[2];
   params[0] = t->structure_ptr;

src/libponyc/codegen/genreference.c
@@ -6,6 +6,7 @@
 #include "gencall.h"
 #include "gentype.h"
 #include "../expr/literal.h"
+#include "../reach/subtype.h"
 #include "../type/cap.h"
 #include "../type/subtype.h"
 #include "../type/viewpoint.h"
@@ -270,38 +271,7 @@ LLVMValueRef gen_addressof(compile_t* c, ast_t* ast)
   return NULL;
 }
 
-enum subtype_kind_t
-{
-  SUBTYPE_KIND_NONE,
-  SUBTYPE_KIND_BOXED = 1 << 0,
-  SUBTYPE_KIND_UNBOXED = 1 << 1,
-  SUBTYPE_KIND_BOTH = SUBTYPE_KIND_BOXED | SUBTYPE_KIND_UNBOXED
-};
-
-static int has_boxed_subtype(reach_t* reach, ast_t* type)
-{
-  reach_type_t* t = reach_type(reach, type);
-
-  int subtypes = SUBTYPE_KIND_NONE;
-
-  size_t i = HASHMAP_BEGIN;
-  reach_type_t* sub;
-
-  while((sub = reach_type_cache_next(&t->subtypes, &i)) != NULL)
-  {
-    if(sub->can_be_boxed)
-      subtypes |= SUBTYPE_KIND_BOXED;
-    else
-      subtypes |= SUBTYPE_KIND_UNBOXED;
-
-    if(subtypes == SUBTYPE_KIND_BOTH)
-      return subtypes;
-  }
-
-  return subtypes;
-}
-
-static LLVMValueRef gen_digestof_box(compile_t* c, ast_t* type,
+static LLVMValueRef gen_digestof_box(compile_t* c, reach_type_t* type,
   LLVMValueRef value, int boxed_subtype)
 {
   pony_assert(LLVMGetTypeKind(LLVMTypeOf(value)) == LLVMPointerTypeKind);
@@ -312,7 +282,7 @@ static LLVMValueRef gen_digestof_box(compile_t* c, ast_t* type,
 
   LLVMValueRef desc = gendesc_fetch(c, value);
 
-  if(boxed_subtype == SUBTYPE_KIND_BOTH)
+  if((boxed_subtype & SUBTYPE_KIND_UNBOXED) != 0)
   {
     box_block = codegen_block(c, "digestof_box");
     nonbox_block = codegen_block(c, "digestof_nonbox");
@@ -329,16 +299,15 @@ static LLVMValueRef gen_digestof_box(compile_t* c, ast_t* type,
   }
 
   // Call the type-specific __digestof function, which will unbox the value.
-  reach_type_t* t = reach_type(c->reach, type);
-  reach_method_t* digest_fn = reach_method(t, TK_BOX, stringtab("__digestof"),
-    NULL);
+  reach_method_t* digest_fn = reach_method(type, TK_BOX,
+    stringtab("__digestof"), NULL);
   pony_assert(digest_fn != NULL);
   LLVMValueRef func = gendesc_vtable(c, desc, digest_fn->vtable_index);
   LLVMTypeRef fn_type = LLVMFunctionType(c->i64, &c->object_ptr, 1, false);
   func = LLVMBuildBitCast(c->builder, func, LLVMPointerType(fn_type, 0), "");
   LLVMValueRef box_digest = codegen_call(c, func, &value, 1, true);
 
-  if(boxed_subtype == SUBTYPE_KIND_BOTH)
+  if((boxed_subtype & SUBTYPE_KIND_UNBOXED) != 0)
   {
     LLVMBuildBr(c->builder, post_block);
 
@@ -412,9 +381,11 @@ static LLVMValueRef gen_digestof_value(compile_t* c, ast_t* type,
     case LLVMPointerTypeKind:
       if(!is_known(type))
       {
-        int sub_kind = has_boxed_subtype(c->reach, type);
+        reach_type_t* t = reach_type(c->reach, type);
+        int sub_kind = subtype_kind(t);
+
         if((sub_kind & SUBTYPE_KIND_BOXED) != 0)
-          return gen_digestof_box(c, type, value, sub_kind);
+          return gen_digestof_box(c, t, value, sub_kind);
       }
 
       return LLVMBuildPtrToInt(c->builder, value, c->i64, "");
@@ -438,7 +409,9 @@ void gen_digestof_fun(compile_t* c, reach_type_t* t)
   pony_assert(t->can_be_boxed);
 
   reach_method_t* m = reach_method(t, TK_BOX, stringtab("__digestof"), NULL);
-  pony_assert(m != NULL);
+
+  if(m == NULL)
+    return;
 
   m->func_type = LLVMFunctionType(c->i64, &t->structure_ptr, 1, false);
   m->func = codegen_addfun(c, m->full_name, m->func_type);

src/libponyc/reach/reach.h
@@ -17,6 +17,7 @@ typedef struct reach_field_t reach_field_t;
 typedef struct reach_param_t reach_param_t;
 typedef struct reach_type_t reach_type_t;
 
+DECLARE_STACK(reachable_expr_stack, reachable_expr_stack_t, ast_t);
 DECLARE_STACK(reach_method_stack, reach_method_stack_t, reach_method_t);
 DECLARE_HASHMAP(reach_methods, reach_methods_t, reach_method_t);
 DECLARE_HASHMAP(reach_mangled, reach_mangled_t, reach_method_t);
@@ -128,7 +129,8 @@ struct reach_type_t
 typedef struct
 {
   reach_types_t types;
-  reach_method_stack_t* stack;
+  reachable_expr_stack_t* expr_stack;
+  reach_method_stack_t* method_stack;
   uint32_t object_type_count;
   uint32_t numeric_type_count;
   uint32_t tuple_type_count;

src/libponyc/reach/subtype.c
@@ -1,0 +1,68 @@
+#include "subtype.h"
+#include <ponyassert.h>
+
+int subtype_kind(reach_type_t* type)
+{
+  int subtypes = SUBTYPE_KIND_NONE;
+
+  size_t i = HASHMAP_BEGIN;
+  reach_type_t* sub;
+
+  while((sub = reach_type_cache_next(&type->subtypes, &i)) != NULL)
+  {
+    if(sub->can_be_boxed)
+    {
+      if(type->underlying == TK_PRIMITIVE)
+        subtypes |= SUBTYPE_KIND_NUMERIC;
+      else
+        subtypes |= SUBTYPE_KIND_TUPLE;
+    } else {
+      subtypes |= SUBTYPE_KIND_UNBOXED;
+    }
+
+    if(subtypes == SUBTYPE_KIND_ALL)
+      return subtypes;
+  }
+
+  return subtypes;
+}
+
+int subtype_kind_overlap(reach_type_t* left, reach_type_t* right)
+{
+  int subtypes = SUBTYPE_KIND_NONE;
+
+  size_t i = HASHMAP_BEGIN;
+  reach_type_t* sub_left;
+
+  while((sub_left = reach_type_cache_next(&left->subtypes, &i)) != NULL)
+  {
+    if(!sub_left->can_be_boxed)
+    {
+      subtypes |= SUBTYPE_KIND_UNBOXED;
+      if(subtypes == SUBTYPE_KIND_ALL)
+        return subtypes;
+
+      continue;
+    }
+
+    reach_type_t k;
+    k.name = sub_left->name;
+    size_t j = HASHMAP_UNKNOWN;
+    reach_type_t* sub_right = reach_type_cache_get(&right->subtypes, &k, &j);
+
+    if(sub_right != NULL)
+    {
+      pony_assert(sub_left == sub_right);
+
+      if(sub_left->underlying == TK_PRIMITIVE)
+        subtypes |= SUBTYPE_KIND_NUMERIC;
+      else
+        subtypes |= SUBTYPE_KIND_TUPLE;
+
+      if(subtypes == SUBTYPE_KIND_ALL)
+        return subtypes;
+    }
+  }
+
+  return subtypes;
+}

src/libponyc/reach/subtype.h
@@ -1,0 +1,27 @@
+#ifndef REACH_SUBTYPE_H
+#define REACH_SUBTYPE_H
+
+#include "reach.h"
+
+PONY_EXTERN_C_BEGIN
+
+enum subtype_kind_t
+{
+  SUBTYPE_KIND_NONE,
+  SUBTYPE_KIND_NUMERIC = 1 << 0,
+  SUBTYPE_KIND_TUPLE = 1 << 1,
+  SUBTYPE_KIND_UNBOXED = 1 << 2,
+
+  SUBTYPE_KIND_BOXED = SUBTYPE_KIND_NUMERIC | SUBTYPE_KIND_TUPLE,
+  SUBTYPE_KIND_ALL = SUBTYPE_KIND_BOXED | SUBTYPE_KIND_UNBOXED
+};
+
+// These functions shouldn't be called before `reach_done` has been called.
+
+int subtype_kind(reach_type_t* type);
+
+int subtype_kind_overlap(reach_type_t* left, reach_type_t* right);
+
+PONY_EXTERN_C_END
+
+#endif

commit 5c7dfaa76d067402f30cd934628d826b05180ee4

Fix assertion failure on nested tuple identity
This fixes a bug introduced in 9b9c084 caused by the `__is` method of a
tuple nested in another tuple not being reached.

src/libponyc/reach/reach.c
@@ -1055,6 +1055,63 @@ static void reachable_ffi(reach_t* r, ast_t* ast, pass_opt_t* opt)
   }
 }
 
+static void reachable_identity_type(reach_t* r, ast_t* l_type, ast_t* r_type,
+  pass_opt_t* opt)
+{
+  if((ast_id(l_type) == TK_TUPLETYPE) && (ast_id(r_type) == TK_TUPLETYPE))
+  {
+    if(ast_childcount(l_type) != ast_childcount(r_type))
+      return;
+
+    ast_t* l_child = ast_child(l_type);
+    ast_t* r_child = ast_child(r_type);
+
+    while(l_child != NULL)
+    {
+      reachable_identity_type(r, l_child, r_child, opt);
+      l_child = ast_sibling(l_child);
+      r_child = ast_sibling(r_child);
+    }
+  } else if(!is_known(l_type) && !is_known(r_type)) {
+    reach_type_t* r_left = reach_type(r, l_type);
+    reach_type_t* r_right = reach_type(r, r_type);
+
+    int sub_kind = subtype_kind_overlap(r_left, r_right);
+
+    if((sub_kind & SUBTYPE_KIND_TUPLE) != 0)
+    {
+      const char* name = stringtab("__is");
+
+      if((reach_method_name(r_left, name) != NULL) &&
+        (reach_method_name(r_right, name) != NULL))
+        return;
+
+      reach_method_name_t* n = add_method_name(r_left, name, true);
+      add_rmethod(r, r_left, n, TK_BOX, NULL, opt, true);
+
+      reach_type_t* l_sub;
+      size_t i = HASHMAP_BEGIN;
+
+      while((l_sub = reach_type_cache_next(&r_left->subtypes, &i)) != NULL)
+      {
+        if(l_sub->underlying != TK_TUPLETYPE)
+          continue;
+
+        reach_type_t k;
+        k.name = l_sub->name;
+        size_t j = HASHMAP_UNKNOWN;
+        reach_type_t* r_sub = reach_type_cache_get(&r_right->subtypes, &k, &j);
+
+        if(r_sub != NULL)
+        {
+          pony_assert(l_sub == r_sub);
+          reachable_identity_type(r, l_sub->ast_cap, r_sub->ast_cap, opt);
+        }
+      }
+    }
+  }
+}
+
 static void reachable_identity(reach_t* r, ast_t* ast, pass_opt_t* opt)
 {
   AST_GET_CHILDREN(ast, left, right);
@@ -1062,21 +1119,42 @@ static void reachable_identity(reach_t* r, ast_t* ast, pass_opt_t* opt)
   ast_t* l_type = ast_type(left);
   ast_t* r_type = ast_type(right);
 
-  if(!is_known(l_type) && !is_known(r_type))
+  reachable_identity_type(r, l_type, r_type, opt);
+}
+
+static void reachable_digestof_type(reach_t* r, ast_t* type, pass_opt_t* opt)
+{
+  if(ast_id(type) == TK_TUPLETYPE)
   {
-    reach_type_t* r_left = reach_type(r, l_type);
-    reach_type_t* r_right = reach_type(r, r_type);
-    int sub_kind = subtype_kind_overlap(r_left, r_right);
+    ast_t* child = ast_child(type);
 
-    if((sub_kind & SUBTYPE_KIND_TUPLE) != 0)
+    while(child != NULL)
     {
-      const char* name = stringtab("__is");
+      reachable_digestof_type(r, child, opt);
+      child = ast_sibling(child);
+    }
+  } else if(!is_known(type)) {
+    reach_type_t* t = reach_type(r, type);
+    int sub_kind = subtype_kind(t);
 
-      reach_method_name_t* n = add_method_name(r_left, name, true);
-      add_rmethod(r, r_left, n, TK_BOX, NULL, opt, true);
+    if((sub_kind & SUBTYPE_KIND_BOXED) != 0)
+    {
+      const char* name = stringtab("__digestof");
 
-      n = add_method_name(r_right, name, true);
-      add_rmethod(r, r_right, n, TK_BOX, NULL, opt, true);
+      if(reach_method_name(t, name) != NULL)
+        return;
+
+      reach_method_name_t* n = add_method_name(t, name, true);
+      add_rmethod(r, t, n, TK_BOX, NULL, opt, true);
+
+      reach_type_t* sub;
+      size_t i = HASHMAP_BEGIN;
+
+      while((sub = reach_type_cache_next(&t->subtypes, &i)) != NULL)
+      {
+        if(sub->can_be_boxed)
+          reachable_digestof_type(r, sub->ast_cap, opt);
+      }
     }
   }
 }
@@ -1086,18 +1164,7 @@ static void reachable_digestof(reach_t* r, ast_t* ast, pass_opt_t* opt)
   ast_t* expr = ast_child(ast);
   ast_t* type = ast_type(expr);
 
-  if(!is_known(type))
-  {
-    reach_type_t* t = reach_type(r, type);
-    int sub_kind = subtype_kind(t);
-
-    if((sub_kind & SUBTYPE_KIND_BOXED) != 0)
-    {
-      const char* name = stringtab("__digestof");
-      reach_method_name_t* n = add_method_name(t, name, true);
-      add_rmethod(r, t, n, TK_BOX, NULL, opt, true);
-    }
-  }
+  reachable_digestof_type(r, type, opt);
 }
 
 static void reachable_expr(reach_t* r, ast_t* ast, pass_opt_t* opt)

test/libponyc/codegen_identity.cc
@@ -203,6 +203,25 @@ TEST_F(CodegenIdentityTest, AbstractTypeNoSubtyping)
 }
 
 
+TEST_F(CodegenIdentityTest, NestedTuple)
+{
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let a: (((U8, U16) | None, U8) | None) = None\n"
+    "    let b: (((U8, U16) | None, U8) | None) = None\n"
+    "    if a is b then\n"
+    "      @pony_exitcode[None](I32(1))\n"
+    "    end";
+
+  TEST_COMPILE(src);
+
+  int exit_code = 0;
+  ASSERT_TRUE(run_program(&exit_code));
+  ASSERT_EQ(exit_code, 1);
+}
+
+
 TEST_F(CodegenIdentityTest, DigestofObject)
 {
   const char* src =