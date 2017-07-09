dde7133da62072040dd4f0da9b571aa551c04218
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2017-07-09 10:02:02 -0700

    Update parser to use last token as optional token pos.
    This improves error reporting for optional tokens, which usually
    make more sense to display at the location of the previous token,
    not the next token, espectially when the next token is on the next line.

--- test/libponyc/sugar_expr.cc
+++ test/libponyc/sugar_expr.cc
@@ -599,7 +599,7 @@ TEST_F(SugarExprTest, LocationDefaultArg)
     "      fun tag file(): String => \"\"\n"
     "      fun tag method(): String => \"bar\"\n"
     "      fun tag line(): USize => 9\n"
-    "      fun tag pos(): USize => 12\n"
+    "      fun tag pos(): USize => 11\n"
     "    end)\n"
     "  fun wombat(x: SourceLoc = __loc) =>\n"
     "    None";
