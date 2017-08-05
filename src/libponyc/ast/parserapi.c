ponylang/ponyc

commit 54225e41bd141b8b7a64f0e65d3dbe46bc5317d4

Feature: Explicit partial calls. (#2039)
Requires a question mark to be at the call site for every call to a partial function.

Resolves #1771.

See https://github.com/ponylang/rfcs/blob/master/text/0039-explicit-partial-calls.md

Migration scripts for user code, for convenience, are provided here:
Unix: https://gist.github.com/jemc/95969e3e2b58ddb0dede138c737907f5
Windows: https://gist.github.com/kulibali/cd5caf3a32d510bb86412f3fd4d52d0f

src/libponyc/ast/parserapi.c
@@ -10,9 +10,10 @@ struct parser_t
   source_t* source;
   lexer_t* lexer;
   token_t* token;
+  token_t* last_token;
   const char* last_matched;
-  size_t last_token_line;
   uint32_t next_flags;  // Data flags to set on the next token created
+  bool free_last_token;
   bool failed;
   errors_t* errors;
 };
@@ -27,13 +28,10 @@ static token_id current_token_id(parser_t* parser)
 }
 
 
-static void fetch_next_lexer_token(parser_t* parser, bool free_prev_token)
+static void fetch_next_lexer_token(parser_t* parser, bool free_last_token)
 {
   token_t* old_token = parser->token;
   token_t* new_token = lexer_next(parser->lexer);
-
-  if(old_token != NULL)
-    parser->last_token_line = token_line_number(old_token);
 
   if(old_token != NULL && token_get_id(new_token) == TK_EOF)
   {
@@ -42,8 +40,14 @@ static void fetch_next_lexer_token(parser_t* parser, bool free_prev_token)
       token_line_number(old_token), token_line_position(old_token));
   }
 
-  if(free_prev_token)
-    token_free(old_token);
+  if(old_token != NULL)
+  {
+    if(parser->free_last_token)
+      token_free(parser->last_token);
+
+    parser->free_last_token = free_last_token;
+    parser->last_token = old_token;
+  }
 
   parser->token = new_token;
 }
@@ -209,19 +213,19 @@ static void add_ast(parser_t* parser, rule_state_t* state, ast_t* new_ast,
 // Add an AST node for the specified token, which may be deferred
 void add_deferrable_ast(parser_t* parser, rule_state_t* state, token_id id)
 {
-  pony_assert(parser->token != NULL);
+  pony_assert(parser->last_token != NULL);
 
   if(!state->matched && state->ast == NULL && !state->deferred)
   {
     // This is the first AST node, defer creation
     state->deferred = true;
     state->deferred_id = id;
-    state->line = token_line_number(parser->token);
-    state->pos = token_line_position(parser->token);
+    state->line = token_line_number(parser->last_token);
+    state->pos = token_line_position(parser->last_token);
     return;
   }
 
-  add_ast(parser, state, ast_new(parser->token, id), default_builder);
+  add_ast(parser, state, ast_new(parser->last_token, id), default_builder);
 }
 
 
@@ -428,7 +432,7 @@ ast_t* parse_token_set(parser_t* parser, rule_state_t* state, const char* desc,
     if(*p == TK_NEWLINE)
     {
       pony_assert(parser->token != NULL);
-      size_t last_token_line = parser->last_token_line;
+      size_t last_token_line = token_line_number(parser->last_token);
       size_t next_token_line = token_line_number(parser->token);
       bool is_newline = (next_token_line != last_token_line);
 
@@ -617,9 +621,10 @@ bool parse(ast_t* package, source_t* source, rule_t start, const char* expected,
   parser->source = source;
   parser->lexer = lexer;
   parser->token = lexer_next(lexer);
+  parser->last_token = parser->token;
   parser->last_matched = NULL;
-  parser->last_token_line = 0;
   parser->next_flags = 0;
+  parser->free_last_token = false;
   parser->failed = false;
   parser->errors = errors;
 

commit 69e906cc1472002a12e7e909fe1d373ad1528be9

Fix cursor location for some reported errors and info.
In PR #2039 (explicit partial calls), one of the changes I made was
to make it so that the cursor, when reporting on an error for a token
that was missing (like the missing '?' for an explicit partial call,
would be placed under the token to the left of where the missing
token should be, instead of under the token to the right (which
was often on the next line).

However, in doing this, I accidentally made it so that many other
(non-missing) tokens were also being reported one token to the left
instead of under the actual token itself. For example:

    main.pony:5:5: can't find declaration of 'present'
        until present end
        ^

Which should be displayed as:

    main.pony:5:5: can't find declaration of 'present'
        until present end
              ^

This PR fixes the accidental breakage, while retaining the desired
behaviour introduced in PR #2039. Now all tokens should be reported
in the correct position when displaying errors.

src/libponyc/ast/parserapi.c
@@ -211,21 +211,25 @@ static void add_ast(parser_t* parser, rule_state_t* state, ast_t* new_ast,
 
 
 // Add an AST node for the specified token, which may be deferred
-void add_deferrable_ast(parser_t* parser, rule_state_t* state, token_id id)
+void add_deferrable_ast(parser_t* parser, rule_state_t* state, token_id id,
+  token_t* token_for_pos)
 {
-  pony_assert(parser->last_token != NULL);
+  if(token_for_pos == NULL)
+    token_for_pos = parser->token;
+
+  pony_assert(token_for_pos != NULL);
 
   if(!state->matched && state->ast == NULL && !state->deferred)
   {
     // This is the first AST node, defer creation
     state->deferred = true;
     state->deferred_id = id;
-    state->line = token_line_number(parser->last_token);
-    state->pos = token_line_position(parser->last_token);
+    state->line = token_line_number(token_for_pos);
+    state->pos = token_line_position(token_for_pos);
     return;
   }
 
-  add_ast(parser, state, ast_new(parser->last_token, id), default_builder);
+  add_ast(parser, state, ast_new(token_for_pos, id), default_builder);
 }
 
 
@@ -348,7 +352,7 @@ static ast_t* handle_not_found(parser_t* parser, rule_state_t* state,
   {
     // Optional token / sub rule not found
     if(state->deflt_id != TK_EOF) // Default node is specified
-      add_deferrable_ast(parser, state, state->deflt_id);
+      add_deferrable_ast(parser, state, state->deflt_id, parser->last_token);
 
     state->deflt_id = TK_LEX_ERROR;
     return PARSE_OK;

src/libponyc/ast/parserapi.h
@@ -112,7 +112,8 @@ void infix_builder(rule_state_t* state, ast_t* new_ast);
 
 void infix_reverse_builder(rule_state_t* state, ast_t* new_ast);
 
-void add_deferrable_ast(parser_t* parser, rule_state_t* state, token_id id);
+void add_deferrable_ast(parser_t* parser, rule_state_t* state, token_id id,
+  token_t* token_for_pos);
 
 ast_t* parse_token_set(parser_t* parser, rule_state_t* state, const char* desc,
   const char* terminating, const token_id* id_set, bool make_ast,
@@ -198,7 +199,7 @@ bool parse(ast_t* package, source_t* source, rule_t start, const char* expected,
  * Example:
  *    AST_NODE(TK_CASE);
  */
-#define AST_NODE(ID)  add_deferrable_ast(parser, &state, ID)
+#define AST_NODE(ID)  add_deferrable_ast(parser, &state, ID, NULL)
 
 
 /** Map our AST node ID.