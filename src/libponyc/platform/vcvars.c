65fb1b95b190e6d481d1b08e135b7c16584c7a70
Gordon Tisher <gordon@balafon.net>
2017-09-20 17:13:39 -0700

    compilation updates for debug packages

--- src/libponyc/platform/vcvars.c
+++ src/libponyc/platform/vcvars.c
@@ -224,7 +224,11 @@ static bool find_kernel32(vcvars_t* vcvars, errors_t* errors)
 
   vcvars->ucrt[0] = '\0';
   strcpy(vcvars->default_libs,
-    "kernel32.lib msvcrt.lib Ws2_32.lib advapi32.lib vcruntime.lib "
+    "kernel32.lib "
+#if !defined(DEBUG)
+    "msvcrt.lib "
+#endif
+    "Ws2_32.lib advapi32.lib vcruntime.lib "
     "legacy_stdio_definitions.lib dbghelp.lib");
 
   strcpy(vcvars->kernel32, sdk.path);
