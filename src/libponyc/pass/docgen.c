46b692b95b6d16d1ac4a9530d5c2145ce5202314
codec-abc <noemail>
2017-11-18 15:31:09 +0100

    remove unsued method.

--- src/libponyc/pass/docgen.c
+++ src/libponyc/pass/docgen.c
@@ -717,7 +717,7 @@ static void doc_method(docgen_t* docgen, docgen_opt_t* docgen_opt,
   const char* name = ast_name(id);
   pony_assert(name != NULL);
 
-
+  // Method
   fprintf(docgen->type_file, "### %s", name);
   doc_type_params(docgen, docgen_opt, t_params, true, false);
 
@@ -803,54 +803,6 @@ static char* concat(const char *s1, const char *s2)
     strcpy(result, s1);
     strcat(result, s2);
     return result;
-}
-
-//https://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
-static char* str_replace(char *orig, char *rep, char *with) 
-{
-  char *result; // the return string
-  char *ins;    // the next insert point
-  char *tmp;    // varies
-  size_t len_rep;  // length of rep (the string to remove)
-  size_t len_with; // length of with (the string to replace rep with)
-  size_t len_front; // distance between rep and end of last rep
-  int count;    // number of replacements
-
-  // sanity checks and initialization
-  if (!orig || !rep)
-    return NULL;
-  len_rep = strlen(rep);
-  if (len_rep == 0)
-    return NULL; // empty rep causes infinite loop during count
-  if (!with)
-    with = "";
-  len_with = strlen(with);
-
-  // count the number of replacements needed
-  ins = orig;
-  for (count = 0; tmp = strstr(ins, rep); ++count) {
-    ins = tmp + len_rep;
-  }
-
-  tmp = result = (char*) malloc(strlen(orig) + (len_with - len_rep) * count + 1);
-
-  if (!result)
-    return NULL;
-
-  // first time through the loop, all the variable are set correctly
-  // from here on,
-  //    tmp points to the end of the result string
-  //    ins points to the next occurrence of rep in orig
-  //    orig points to the remainder of orig after "end of rep"
-  while (count--) {
-    ins = strstr(orig, rep);
-    len_front = ins - orig;
-    tmp = strncpy(tmp, orig, len_front) + len_front;
-    tmp = strcpy(tmp, with) + len_with;
-    orig += len_front + len_rep; // move to next "end of rep"
-  }
-  strcpy(tmp, orig);
-  return result;
 }
 
 static doc_sources_t* copy_source_to_doc_src(docgen_t* docgen, source_t* source)
