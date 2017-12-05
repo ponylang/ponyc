ce8532ccb9a1f02be82030b922de2166e2b6e1e4
Gordon Tisher <gordon@balafon.net>
2017-12-05 13:17:00 -0800

    Updates to fix building using Visual Studio 15.5
    - Update Google Test and implement a workaround for deprecated TR1 usage.
    - Turn the `ROUND()` and `G()` macros in the Blake2 code to inline functions to work around a MS compiler bug.

--- lib/blake2/blake2b-ref.c
+++ lib/blake2/blake2b-ref.c
@@ -150,29 +150,32 @@ int blake2b_init_key( blake2b_state *S, size_t outlen, const void *key, size_t k
   return 0;
 }
 
-#define G(r,i,a,b,c,d)                      \
-  do {                                      \
-    a = a + b + m[blake2b_sigma[r][2*i+0]]; \
-    d = rotr64(d ^ a, 32);                  \
-    c = c + d;                              \
-    b = rotr64(b ^ c, 24);                  \
-    a = a + b + m[blake2b_sigma[r][2*i+1]]; \
-    d = rotr64(d ^ a, 16);                  \
-    c = c + d;                              \
-    b = rotr64(b ^ c, 63);                  \
-  } while(0)
+static BLAKE2_INLINE void G(int r, uint64_t i, uint64_t* a, uint64_t* b, uint64_t* c, uint64_t* d, uint64_t* m)
+{
+  do {                                      
+    *a = *a + *b + m[blake2b_sigma[r][2*i+0]]; 
+    *d = rotr64(*d ^ *a, 32);                  
+    *c = *c + *d;                              
+    *b = rotr64(*b ^ *c, 24);                  
+    *a = *a + *b + m[blake2b_sigma[r][2*i+1]]; 
+    *d = rotr64(*d ^ *a, 16);                  
+    *c = *c + *d;                              
+    *b = rotr64(*b ^ *c, 63);                  
+  } while(0);
+}
 
-#define ROUND(r)                    \
-  do {                              \
-    G(r,0,v[ 0],v[ 4],v[ 8],v[12]); \
-    G(r,1,v[ 1],v[ 5],v[ 9],v[13]); \
-    G(r,2,v[ 2],v[ 6],v[10],v[14]); \
-    G(r,3,v[ 3],v[ 7],v[11],v[15]); \
-    G(r,4,v[ 0],v[ 5],v[10],v[15]); \
-    G(r,5,v[ 1],v[ 6],v[11],v[12]); \
-    G(r,6,v[ 2],v[ 7],v[ 8],v[13]); \
-    G(r,7,v[ 3],v[ 4],v[ 9],v[14]); \
-  } while(0)
+static BLAKE2_INLINE void ROUND(int r, uint64_t* m, uint64_t* v) {
+  do {
+    G(r, 0, &v[ 0], &v[ 4], &v[ 8], &v[12], m);
+    G(r, 1, &v[ 1], &v[ 5], &v[ 9], &v[13], m); 
+    G(r, 2, &v[ 2], &v[ 6], &v[10], &v[14], m); 
+    G(r, 3, &v[ 3], &v[ 7], &v[11], &v[15], m); 
+    G(r, 4, &v[ 0], &v[ 5], &v[10], &v[15], m); 
+    G(r, 5, &v[ 1], &v[ 6], &v[11], &v[12], m);
+    G(r, 6, &v[ 2], &v[ 7], &v[ 8], &v[13], m);
+    G(r, 7, &v[ 3], &v[ 4], &v[ 9], &v[14], m);
+  } while(0);
+}
 
 static void blake2b_compress( blake2b_state *S, const uint8_t block[BLAKE2B_BLOCKBYTES] )
 {
@@ -197,18 +200,18 @@ static void blake2b_compress( blake2b_state *S, const uint8_t block[BLAKE2B_BLOC
   v[14] = blake2b_IV[6] ^ S->f[0];
   v[15] = blake2b_IV[7] ^ S->f[1];
 
-  ROUND( 0 );
-  ROUND( 1 );
-  ROUND( 2 );
-  ROUND( 3 );
-  ROUND( 4 );
-  ROUND( 5 );
-  ROUND( 6 );
-  ROUND( 7 );
-  ROUND( 8 );
-  ROUND( 9 );
-  ROUND( 10 );
-  ROUND( 11 );
+  ROUND( 0, m, v );
+  ROUND( 1, m, v );
+  ROUND( 2, m, v );
+  ROUND( 3, m, v );
+  ROUND( 4, m, v );
+  ROUND( 5, m, v );
+  ROUND( 6, m, v );
+  ROUND( 7, m, v );
+  ROUND( 8, m, v );
+  ROUND( 9, m, v );
+  ROUND( 10, m, v );
+  ROUND( 11, m, v );
 
   for( i = 0; i < 8; ++i ) {
     S->h[i] = S->h[i] ^ v[i] ^ v[i + 8];
