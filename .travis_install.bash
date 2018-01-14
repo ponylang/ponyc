98b902e8bc6f70888c63881d7713dadc42bf3469
Sean T Allen <sean@monkeysnatchbanana.com>
2018-01-14 08:51:42 -0500

    Make shellcheck happy
    Because it wasn't and it was erroring out. Bad person who did that. Bad
    bad Sean.

--- .travis_install.bash
+++ .travis_install.bash
@@ -12,10 +12,10 @@ fi
 download_llvm(){
   echo "Downloading and installing the LLVM specified by envvars..."
 
-  git lfs clone -I clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz https://github.com/ponylang/ponyc-llvm-dependencies.git
+  git lfs clone -I "clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz" https://github.com/ponylang/ponyc-llvm-dependencies.git
 
   pushd ponyc-llvm-dependencies
-  tar -xf clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz
+  tar -xf "clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz"
   pushd clang+llvm* && sudo mkdir /tmp/llvm && sudo cp -r ./* /tmp/llvm/
   sudo ln -s "/tmp/llvm/bin/llvm-config" "/usr/local/bin/${LLVM_CONFIG}"
   popd
