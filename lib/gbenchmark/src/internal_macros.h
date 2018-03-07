f17254722be5b0a8b021f8368693d982112683f5
Wink Saville <wink@saville.com>
2018-03-07 12:59:25 -0800

    Revert GBencDragonFly BSD 4.8 Support. (#2579)
    Revert gbenchmark DragonFly BSD 4.8 Support in gbenchmark

--- lib/gbenchmark/src/internal_macros.h
+++ lib/gbenchmark/src/internal_macros.h
@@ -47,7 +47,7 @@
     #endif
   #endif
 #elif defined(__FreeBSD__)
-  #define BENCHMARK_OS_BSD 1
+  #define BENCHMARK_OS_FREEBSD 1
 #elif defined(__NetBSD__)
   #define BENCHMARK_OS_NETBSD 1
 #elif defined(__linux__)
