836b42518fdf6598de532ebce036d41224ce7656
Matthias Wahl <matthiaswahl@m7w3.de>
2018-03-21 22:17:32 +0100

    add a badpony test case for Issue #2602
    to ensure pony properly reports the error and does not assert

--- test/libponyc/badpony.cc
+++ test/libponyc/badpony.cc
@@ -988,3 +988,19 @@ TEST_F(BadPonyTest, IsComparingCreateSugar)
     DO(test_expected_errors(src, "refer", errs));
   }
 }
+
+TEST_F(BadPonyTest, TypeErrorDuringArrayLiteralInference)
+{
+  // From issue 2602
+  const char* src =
+    "class C\n"
+    "trait X\n"
+    "class ExpectX[T: X]\n"
+    "  fun trigger(arg: Iterator[T]) =>\n"
+    "    None\n"
+
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    ExpectX[C].trigger([C; C].values())";
+  TEST_ERRORS_1(src, "type argument is outside its constraint");
+}
