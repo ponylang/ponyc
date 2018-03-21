d3f2c5ca20f2aefa6854469d692b82b6aa930ef2
Rene Nejsum <rene@stranden.com>
2018-03-20 20:35:25 -0400

    Updated to use halfsiphash for 32bit

--- src/libponyrt/ds/fun.c
+++ src/libponyrt/ds/fun.c
@@ -1,8 +1,112 @@
 #include "fun.h"
+#include <stdio.h>
 #include <string.h>
 
 #include <platform.h>
 
+
+#ifdef PLATFORM_IS_ILP32
+/*
+   SipHash reference C implementation
+
+   Copyright (c) 2016 Jean-Philippe Aumasson <jeanphilippe.aumasson@gmail.com>
+
+   To the extent possible under law, the author(s) have dedicated all copyright
+   and related and neighboring rights to this software to the public domain
+   worldwide. This software is distributed without any warranty.
+
+   You should have received a copy of the CC0 Public Domain Dedication along
+   with
+   this software. If not, see
+   <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+/* default: SipHash-2-4 */
+#define cROUNDS 2
+#define dROUNDS 4
+
+static const unsigned char the_key[16] = {
+  0xFE, 0x09, 0xD3, 0x22, 0x6B, 0x9C, 0x10, 0x8A,
+  0xE1, 0x35, 0x72, 0xB5, 0xCC, 0x3F, 0x92, 0x9F
+};
+
+#define ROTL(x, b) (uint32_t)(((x) << (b)) | ((x) >> (32 - (b))))
+
+#define SIPROUND                                                               \
+    do {                                                                       \
+        v0 += v1;                                                              \
+        v1 = ROTL(v1, 5);                                                      \
+        v1 ^= v0;                                                              \
+        v0 = ROTL(v0, 16);                                                     \
+        v2 += v3;                                                              \
+        v3 = ROTL(v3, 8);                                                      \
+        v3 ^= v2;                                                              \
+        v0 += v3;                                                              \
+        v3 = ROTL(v3, 7);                                                      \
+        v3 ^= v0;                                                              \
+        v2 += v1;                                                              \
+        v1 = ROTL(v1, 13);                                                     \
+        v1 ^= v2;                                                              \
+        v2 = ROTL(v2, 16);                                                     \
+    } while (0)
+
+
+size_t halfsiphash(const uint8_t *k, const char *in, const size_t inlen)
+  {
+    uint32_t v0 = 0;
+    uint32_t v1 = 0;
+    uint32_t v2 = 0x6c796765;
+    uint32_t v3 = 0x74656462;
+    uint32_t k0 = *(uint32_t*)(k);
+    uint32_t k1 = *(uint32_t*)(k + 4);
+    uint32_t m;
+    int i;
+    const char* end = in + inlen - (inlen % sizeof(uint32_t));
+    const int left = inlen & 3;
+    uint32_t b = ((uint32_t)inlen) << 24;
+    v3 ^= k1;
+    v2 ^= k0;
+    v1 ^= k1;
+    v0 ^= k0;
+
+    for (; in != end; in += 4)
+      {
+	m = *(uint32_t*)(in);
+        v3 ^= m;
+
+        for (i = 0; i < cROUNDS; ++i)
+            SIPROUND;
+
+        v0 ^= m;
+      }
+
+    switch (left) {
+    case 3:
+        b |= ((uint32_t)in[2]) << 16;
+    case 2:
+        b |= ((uint32_t)in[1]) << 8;
+    case 1:
+        b |= ((uint32_t)in[0]);
+        break;
+    case 0:
+        break;
+    }
+
+    v3 ^= b;
+
+    for (i = 0; i < cROUNDS; ++i)
+        SIPROUND;
+
+    v0 ^= b;
+
+    v2 ^= 0xff;
+
+    for (i = 0; i < dROUNDS; ++i)
+        SIPROUND;
+
+    return v0 ^ v1 ^ v2 ^ v3;
+}
+#else
 static const unsigned char the_key[16] = {
   0xFE, 0x09, 0xD3, 0x22, 0x6B, 0x9C, 0x10, 0x8A,
   0xE1, 0x35, 0x72, 0xB5, 0xCC, 0x3F, 0x92, 0x9F
@@ -64,33 +168,22 @@ static uint64_t siphash24(const unsigned char* key, const char* in, size_t len)
 
   return v0 ^ v1 ^ v2 ^ v3;
 }
-
-#ifdef PLATFORM_IS_ILP32
-static uint32_t pony_hash32(char* str)
-{
-  uint32_t h;
-  char* p;
-
-  h = 0;
-
-  for(p = str; *p != '\0'; p++)
-    {
-      h = 37 * h + *p;
-    }
-
-  return h;
-}
 #endif
 
-PONY_API uint64_t ponyint_hash_block(const void* p, size_t len)
+PONY_API size_t ponyint_hash_block(const void* p, size_t len)
 {
-  return siphash24(the_key, (const char*)p, len);
+#ifdef PLATFORM_IS_ILP32
+  return halfsiphash(the_key, (const char*) p, len);
+#else
+  return siphash24(the_key, (const char*) p, len);
+#endif
 }
+
 
 size_t ponyint_hash_str(const char* str)
 {
 #ifdef PLATFORM_IS_ILP32
-  return pony_hash32((char*) str);
+  return halfsiphash(the_key, str, strlen(str));
 #else
   return siphash24(the_key, str, strlen(str));
 #endif
