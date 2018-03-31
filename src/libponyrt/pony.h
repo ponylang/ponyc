6dd02766e70b4cd680c6ede490d3356b3810be57
Benoit Vey <benoit.vey@etu.upmc.fr>
2018-03-31 16:38:21 +0200

    Do not realloc non-pony_alloc'ed memory in pony_realloc (#2618)
    This change forbids the reallocation of foreign memory in pony_realloc.
    This is because the function can only know the size of the old
    allocation (and thus the amount to copy) if the memory was
    pony_alloc'ed.

--- src/libponyrt/pony.h
+++ src/libponyrt/pony.h
@@ -300,7 +300,8 @@ PONY_API ATTRIBUTE_MALLOC void* pony_alloc_large(pony_ctx_t* ctx, size_t size);
 /** Reallocate memory on the current actor's heap.
  *
  * Take heap memory and expand it. This is a no-op if there's already enough
- * space, otherwise it allocates and copies.
+ * space, otherwise it allocates and copies. The old memory must have been
+ * allocated via pony_alloc(), pony_alloc_small(), or pony_alloc_large().
  */
 PONY_API ATTRIBUTE_MALLOC void* pony_realloc(pony_ctx_t* ctx, void* p, size_t size);
 
