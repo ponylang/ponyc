ponylang/ponyc

commit 9222f08c97f0079f0d5d7f732d246c09969daa4d

Fix code generation crash with variable declarations in nested tuples (#2664)

src/libponyc/codegen/genreference.c
@@ -213,7 +213,7 @@ LLVMValueRef gen_tuple(compile_t* c, ast_t* ast)
     if(value == GEN_NOVALUE)
     {
       ponyint_pool_free_size(buf_size, elements);
-      return GEN_NOTNEEDED;
+      return GEN_NOVALUE;
     }
 
     ast_t* child_type = deferred_reify(reify, ast_type(child), c->opt);

test/libponyc/codegen.cc
@@ -599,3 +599,17 @@ TEST_F(CodegenTest, RecoverCast)
 
   TEST_COMPILE(src);
 }
+
+TEST_F(CodegenTest, VariableDeclNestedTuple)
+{
+  // From issue #2662
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let array = Array[((U8, U8), U8)]\n"
+    "    for ((a, b), c) in array.values() do\n"
+    "      None\n"
+    "    end";
+
+  TEST_COMPILE(src);
+}

test/libponyc/util.cc
@@ -104,8 +104,8 @@ static const char* const _builtin =
   "  var _size: USize = 0\n"
   "  var _alloc: USize = 0\n"
   "  var _ptr: Pointer[A] = Pointer[A]\n"
-  "  new create(len: USize, alloc: USize = 0) => true\n"
-  "  fun ref push(value: A): Array[A]^ => this\n"
+  "  new create(len: USize = 0) => true\n"
+  "  fun ref push(value: A) => true\n"
   "  fun apply(index: USize): this->A ? => error\n"
   "  fun values(): Iterator[A] => object ref\n"
   "    fun ref has_next(): Bool => false\n"

commit efacdd36d667632fea301d1a16b97cbe2a6e9ffc

Fix incorrect tuple handling
I don't know the compiler well and I went off what I knew and believe I
found the root cause for our tuple bugs that started with 9222f08. Which
was in turn fixing https://github.com/ponylang/ponyc/issues/2735.

However, a number of bugs came from that fix because, it didn't fix the
root issue. Which was `gen_localdecl` returning GEN_NOVALUE where
GEN_NOTNEEDED was needed. This is why, prior to 9222f08, `gen_tuple` was
returning GEN_NOTNEEDED when the value it has was GEN_NOVALUE.

Fixes #2760

src/libponyc/codegen/genreference.c
@@ -279,7 +279,7 @@ LLVMValueRef gen_localdecl(compile_t* c, ast_t* ast)
 
   // Put the builder back where it was.
   LLVMPositionBuilderAtEnd(c->builder, this_block);
-  return GEN_NOVALUE;
+  return GEN_NOTNEEDED;
 }
 
 LLVMValueRef gen_localptr(compile_t* c, ast_t* ast)

test/libponyc/codegen.cc
@@ -614,6 +614,20 @@ TEST_F(CodegenTest, VariableDeclNestedTuple)
   TEST_COMPILE(src);
 }
 
+TEST_F(CodegenTest, TupleRemoveUnreachableBlockInLoop)
+{
+  // From issue #2760
+  const char* src =
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    var token = U8(1)\n"
+    "    repeat\n"
+    "      (token, let source) = (U8(1), U8(2))\n"
+    "    until token == 2 end";
+
+  TEST_COMPILE(src);
+}
+
 TEST_F(CodegenTest, TupleRemoveUnreachableBlock)
 {
   // From issue #2735