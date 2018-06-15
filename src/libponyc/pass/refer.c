ponylang/ponyc

commit 9eb1687e4cc16d0467c923c11daeb1f3e01faf9d

Fix repeat loop symbol tracking to allow more valid cases.
Resolves #2784.

src/libponyc/pass/refer.c
@@ -910,7 +910,8 @@ static bool refer_seq(pass_opt_t* opt, ast_t* ast)
   if(ast_checkflag(ast_childlast(ast), AST_FLAG_JUMPS_AWAY))
     ast_setflag(ast, AST_FLAG_JUMPS_AWAY);
 
-  // Propagate consumes forward if this seq is the body of a try expression.
+  // Propagate symbol status forward in some cases where control flow branches
+  // always precede other branches of the same control flow structure.
   if(ast_has_scope(ast))
   {
     ast_t* parent = ast_parent(ast);
@@ -932,6 +933,22 @@ static bool refer_seq(pass_opt_t* opt, ast_t* ast)
           // includes the consumes from the body.
           ast_inheritbranch(then_clause, else_clause);
           ast_consolidate_branches(then_clause, 2);
+        }
+      }
+
+      case TK_REPEAT:
+      {
+        AST_GET_CHILDREN(parent, body, cond, else_clause);
+
+        if(body == ast)
+        {
+          // Push our consumes and our defines to the cond clause.
+          ast_inheritstatus(cond, body);
+        } else if(cond == ast) {
+          // Push our consumes, but not defines, to the else clause. This
+          // includes the consumes from the body.
+          ast_inheritbranch(else_clause, cond);
+          ast_consolidate_branches(else_clause, 2);
         }
       }
 
@@ -1095,7 +1112,7 @@ static bool refer_while(pass_opt_t* opt, ast_t* ast)
 static bool refer_repeat(pass_opt_t* opt, ast_t* ast)
 {
   pony_assert(ast_id(ast) == TK_REPEAT);
-  AST_GET_CHILDREN(ast, cond, body, else_clause);
+  AST_GET_CHILDREN(ast, body, cond, else_clause);
 
   // All consumes have to be in scope when the loop body finishes.
   errorframe_t errorf = NULL;
@@ -1110,17 +1127,29 @@ static bool refer_repeat(pass_opt_t* opt, ast_t* ast)
   // No symbol status is inherited from the loop body. Nothing from outside the
   // loop body can be consumed, and definitions in the body may not occur.
   if(!ast_checkflag(body, AST_FLAG_JUMPS_AWAY))
+  {
     branch_count++;
+    ast_inheritbranch(ast, body);
+  }
 
   if(!ast_checkflag(else_clause, AST_FLAG_JUMPS_AWAY))
   {
-    branch_count++;
-    ast_inheritbranch(ast, else_clause);
-
-    // Use a branch count of two instead of one. This means we will pick up any
-    // consumes, but not any definitions, since definitions may not occur.
-    ast_consolidate_branches(ast, 2);
+    // Only include the else clause in the branch analysis
+    // if the loop has a break statement somewhere in it.
+    // This allows us to treat the entire loop body as being
+    // sure to execute in every case, at least for the purposes
+    // of analyzing variables being defined in its scope.
+    // For the case of errors and return statements that may
+    // exit the loop, they do not affect our analysis here
+    // because they will skip past more than just the loop itself.
+    if(ast_checkflag(body, AST_FLAG_MAY_BREAK))
+    {
+      branch_count++;
+      ast_inheritbranch(ast, else_clause);
+    }
   }
+
+  ast_consolidate_branches(ast, branch_count);
 
   // If all branches jump away with no value, then we do too.
   if(branch_count == 0)
@@ -1253,6 +1282,8 @@ static bool refer_break(pass_opt_t* opt, ast_t* ast)
     ast_error(opt->check.errors, ast, "must be in a loop");
     return false;
   }
+
+  ast_setflag(opt->check.frame->loop_body, AST_FLAG_MAY_BREAK);
 
   errorframe_t errorf = NULL;
   if(!ast_all_consumes_in_scope(opt->check.frame->loop_body, ast, &errorf))

src/libponyc/ast/ast.c
@@ -58,7 +58,7 @@ enum
   AST_ORPHAN = 0x20,
   AST_INHERIT_FLAGS = (AST_FLAG_CAN_ERROR | AST_FLAG_CAN_SEND |
     AST_FLAG_MIGHT_SEND | AST_FLAG_RECURSE_1 | AST_FLAG_RECURSE_2),
-  AST_ALL_FLAGS = 0x7FFFFF
+  AST_ALL_FLAGS = 0xFFFFFF
 };
 
 

src/libponyc/ast/ast.h
@@ -46,6 +46,7 @@ enum
   AST_FLAG_JUMPS_AWAY   = 0x100000, // Jumps away (control flow) without a value.
   AST_FLAG_INCOMPLETE   = 0x200000, // Not all fields are defined.
   AST_FLAG_IMPORT       = 0x400000, // Import the referenced package.
+  AST_FLAG_MAY_BREAK    = 0x800000, // This loop has seen a break statement in it.
 };
 
 DECLARE_LIST(astlist, astlist_t, ast_t);

src/libponyc/ast/parser.c
@@ -815,7 +815,7 @@ DEF(repeat);
   SCOPE();
   RULE("repeat body", seq);
   SKIP(NULL, TK_UNTIL);
-  RULE("condition expression", annotatedrawseq);
+  RULE("condition expression", annotatedseq);
   IF(TK_ELSE, RULE("else clause", annotatedseq));
   TERMINATE("repeat loop", TK_END);
   DONE();

src/libponyc/ast/treecheckdef.h
@@ -323,7 +323,7 @@ RULE(repeat,
   IS_SCOPE
   HAS_TYPE(type)
   CHILD(seq)        // Loop body
-  CHILD(rawseq)        // Condition
+  CHILD(seq)        // Condition
   CHILD(seq, none), // Else body
   TK_REPEAT);
 

test/libponyc/refer.cc
@@ -1,0 +1,217 @@
+#include <gtest/gtest.h>
+#include <platform.h>
+
+#include "util.h"
+
+
+#define TEST_COMPILE(src) DO(test_compile(src, "refer"))
+
+#define TEST_ERRORS_1(src, err1) \
+  { const char* errs[] = {err1, NULL}; \
+    DO(test_expected_errors(src, "refer", errs)); }
+
+#define TEST_ERRORS_2(src, err1, err2) \
+  { const char* errs[] = {err1, err2, NULL}; \
+    DO(test_expected_errors(src, "refer", errs)); }
+
+#define TEST_ERRORS_3(src, err1, err2, err3) \
+  { const char* errs[] = {err1, err2, err3, NULL}; \
+    DO(test_expected_errors(src, "refer", errs)); }
+
+
+class ReferTest : public PassTest
+{};
+
+
+TEST_F(ReferTest, WhileLoopConsumeLet)
+{
+  const char* src =
+    "class iso C\n"
+
+    "primitive P\n"
+      "fun apply(c: C) => None\n"
+
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "let c = C\n"
+        "while true do\n"
+          "P(consume c)\n"
+        "end";
+
+  TEST_ERRORS_1(src,
+    "identifier 'c' is consumed when the loop exits");
+}
+
+TEST_F(ReferTest, WhileLoopConsumeLetInCondition)
+{
+  const char* src =
+    "class iso C\n"
+
+    "primitive P\n"
+      "fun apply(c: C) => None\n"
+
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "let c = C\n"
+        "while P(consume c); true do\n"
+          "None\n"
+        "end";
+
+  TEST_ERRORS_1(src,
+    "can't consume from an outer scope in a loop condition");
+}
+
+TEST_F(ReferTest, WhileLoopDeclareLetAndUseOutside)
+{
+  const char* src =
+    "class val C\n"
+      "new val create() => None\n"
+
+    "primitive P\n"
+      "fun apply(c: C) => None\n"
+
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "while let c1 = C; true do\n"
+          "let c2 = C\n"
+        "else\n"
+          "let c3 = C\n"
+        "end\n"
+
+        "P(c1)\n"
+        "P(c2)\n"
+        "P(c3)";
+
+  TEST_ERRORS_3(src,
+    "can't find declaration of 'c1'",
+    "can't find declaration of 'c2'",
+    "can't find declaration of 'c3'");
+}
+
+TEST_F(ReferTest, RepeatLoopConsumeLet)
+{
+  const char* src =
+    "class iso C\n"
+
+    "primitive P\n"
+      "fun apply(c: C) => None\n"
+
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "let c = C\n"
+        "repeat\n"
+          "P(consume c)\n"
+        "until false end";
+
+  TEST_ERRORS_1(src,
+    "identifier 'c' is consumed when the loop exits");
+}
+
+TEST_F(ReferTest, RepeatLoopConsumeLetInCondition)
+{
+  const char* src =
+    "class iso C\n"
+
+    "primitive P\n"
+      "fun apply(c: C) => None\n"
+
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "let c = C\n"
+        "repeat\n"
+          "None\n"
+        "until\n"
+          "P(consume c); false\n"
+        "end";
+
+  TEST_ERRORS_1(src,
+    "can't consume from an outer scope in a loop condition");
+}
+
+TEST_F(ReferTest, RepeatLoopDeclareLetAndUseOutside)
+{
+  const char* src =
+    "class val C\n"
+      "new val create() => None\n"
+
+    "primitive P\n"
+      "fun apply(c: C) => None\n"
+
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "repeat\n"
+          "let c1 = C\n"
+        "until\n"
+          "let c2 = C; true\n"
+        "else\n"
+          "let c3 = C\n"
+        "end\n"
+
+        "P(c1)\n"
+        "P(c2)\n"
+        "P(c3)";
+
+  TEST_ERRORS_3(src,
+    "can't find declaration of 'c1'",
+    "can't find declaration of 'c2'",
+    "can't find declaration of 'c3'");
+}
+
+TEST_F(ReferTest, RepeatLoopDefineVarAndUseOutside)
+{
+  const char* src =
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "var n: U8\n"
+        "repeat\n"
+          "n = 5\n"
+        "until true end\n"
+        "let n' = n";
+
+  TEST_COMPILE(src);
+}
+
+TEST_F(ReferTest, RepeatLoopDefineVarAfterBreakAndUseOutside)
+{
+  const char* src =
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "var n: U8\n"
+        "repeat\n"
+          "if true then break end\n"
+          "n = 5\n"
+        "until true end\n"
+        "let n' = n";
+
+  TEST_ERRORS_1(src,
+    "can't use an undefined variable in an expression");
+}
+
+TEST_F(ReferTest, RepeatLoopDefineVarAfterNestedBreakAndUseOutside)
+{
+  const char* src =
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "var n: U8\n"
+        "repeat\n"
+          "while true do break end\n"
+          "n = 5\n"
+        "until true end\n"
+        "let n' = n";
+
+  TEST_COMPILE(src);
+}
+
+TEST_F(ReferTest, RepeatLoopDefineVarAndUseInCondition)
+{
+  // From issue #2784
+  const char* src =
+    "actor Main\n"
+      "new create(env: Env) =>\n"
+        "var n: U8\n"
+        "repeat\n"
+          "n = 5\n"
+        "until (n > 2) end";
+
+  TEST_COMPILE(src);
+}

commit 48592177b8f1aff22294829ee00275d3a611586b

Fix missing break in refer_seq
Commit 9eb1687e introduced a bug in the symbol status forward
propagation where breaks are missing between the different
switch cases, causing the TRY case to fall through into the
REPEAT case.

src/libponyc/pass/refer.c
@@ -935,6 +935,7 @@ static bool refer_seq(pass_opt_t* opt, ast_t* ast)
           ast_consolidate_branches(then_clause, 2);
         }
       }
+      break;
 
       case TK_REPEAT:
       {
@@ -951,6 +952,7 @@ static bool refer_seq(pass_opt_t* opt, ast_t* ast)
           ast_consolidate_branches(else_clause, 2);
         }
       }
+      break;
 
       default: {}
     }