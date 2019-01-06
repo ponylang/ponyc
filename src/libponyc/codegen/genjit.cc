e5b279106d152b7fadb7717fec9ed3cfc4d14291
Gordon Tisher <gordon@balafon.net>
2019-01-05 16:12:20 -0800

    Cleaned up JIT for LLVM 7.0.1.

--- src/libponyc/codegen/genjit.cc
+++ src/libponyc/codegen/genjit.cc
@@ -2,36 +2,158 @@
 #include "genexe.h"
 #include "genopt.h"
 
-#include "llvm_config_begin.h"
-
-#include <llvm/IR/Mangler.h>
-#include <llvm/IR/Module.h>
-#include <llvm/ExecutionEngine/Orc/CompileUtils.h>
-#include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
-#include <llvm/ExecutionEngine/Orc/LambdaResolver.h>
 #if PONY_LLVM >= 700
-#  include <llvm/ExecutionEngine/Orc/Core.h>
-#  include <llvm/ExecutionEngine/Orc/NullResolver.h>
-#  include <llvm/ADT/STLExtras.h>
-#endif
-#if PONY_LLVM >= 500
-#  include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
-#else
-#  include <llvm/ExecutionEngine/Orc/ObjectLinkingLayer.h>
-#endif
-#include <llvm/ExecutionEngine/SectionMemoryManager.h>
 
+#include "llvm_config_begin.h"
+#  include <llvm/ExecutionEngine/ExecutionEngine.h>
+#  include <llvm/ExecutionEngine/JITSymbol.h>
+#  include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#  include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#  include <llvm/ExecutionEngine/Orc/Core.h>
+#  include <llvm/ExecutionEngine/Orc/Legacy.h>
+#  include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#  include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#  include <llvm/IR/DataLayout.h>
+#  include <llvm/IR/Mangler.h>
+#  include <llvm/Support/Error.h>
+#  include <llvm/Target/TargetMachine.h>
+#include "llvm_config_end.h"
+
+using namespace llvm;
+using namespace llvm::orc;
+
+class PonyJIT
+{
+  ExecutionSession _es;
+  std::shared_ptr<SymbolResolver> _sr;
+  std::unique_ptr<TargetMachine> _tm;
+  const DataLayout _dl;
+  RTDyldObjectLinkingLayer _obj_layer;
+  IRCompileLayer<decltype(_obj_layer), SimpleCompiler> _compile_layer;
+
+public:
+  Error error;
+
+  PonyJIT() :
+    _es(),
+    _sr(createLegacyLookupResolver(_es,
+      [this](const std::string& name) -> JITSymbol
+      {
+        auto symbol = _compile_layer.findSymbol(name, false);
+        if (symbol) return symbol;
+
+        auto err = symbol.takeError();
+        if (err) return std::move(err);
+
+        auto symaddr = RTDyldMemoryManager::getSymbolAddressInProcess(name);
+        if (symaddr)
+          return JITSymbol(symaddr, JITSymbolFlags::Exported);
+
+        return nullptr;
+      },
+      [this](Error err)
+      {
+        error = std::move(err);
+      })),
+    _tm(EngineBuilder().selectTarget()),
+    _dl(_tm->createDataLayout()),
+    _obj_layer(_es,
+      [this](VModuleKey)
+      {
+        return RTDyldObjectLinkingLayer::Resources
+        {
+          std::make_shared<SectionMemoryManager>(), _sr
+        };
+      }),
+    _compile_layer(_obj_layer, SimpleCompiler(*_tm)),
+    error(Error::success())
+  {
+    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+  }
+
+  VModuleKey addModule(std::unique_ptr<Module> module)
+  {
+    VModuleKey key = _es.allocateVModule();
+    _compile_layer.addModule(key, std::move(module));
+    return key;
+  }
+
+  JITTargetAddress getSymbolAddress(const std::string& name)
+  {
+    std::string mangled;
+    raw_string_ostream mangled_stream(mangled);
+    Mangler::getNameWithPrefix(mangled_stream, name, _dl);
+    JITSymbol symbol = _compile_layer.findSymbol(mangled_stream.str(), true);
+    return symbol.getAddress().get();
+  }
+};
+
+bool gen_jit_and_run(compile_t* c, int* exit_code, jit_symbol_t* symbols,
+  size_t symbol_count)
+{
+  reach_type_t* t_main = reach_type_name(c->reach, "Main");
+  reach_type_t* t_env = reach_type_name(c->reach, "Env");
+
+  if ((t_main == NULL) || (t_env == NULL))
+    return false;
+
+  gen_main(c, t_main, t_env);
+
+  if (!genopt(c, true))
+    return false;
+
+  if (LLVMVerifyModule(c->module, LLVMReturnStatusAction, NULL) != 0)
+    return false;
+
+  PonyJIT jit;
+  jit.addModule(std::unique_ptr<Module>(unwrap(c->module)));
+  c->module = nullptr;
+
+  for (size_t i = 0; i < symbol_count; i++)
+  {
+    void* address = (void*)jit.getSymbolAddress(symbols[i].name);
+    if (address == nullptr)
+      return false;
+
+    memcpy(symbols[i].address, address, symbols[i].size);
+  }
+
+  auto main = reinterpret_cast<int(*)(int, const char**, const char**)>(
+    jit.getSymbolAddress("main")
+  );
+
+  if (main == nullptr)
+    return false;
+
+  const char* argv[] = { "ponyjit", nullptr };
+  const char* envp = nullptr;
+
+  int ec = main(1, argv, &envp);
+
+  if (exit_code != nullptr)
+    *exit_code = ec;
+  return true;
+}
+
+#else
+
+#include "llvm_config_begin.h"
+#  include <llvm/IR/Mangler.h>
+#  include <llvm/IR/Module.h>
+#  include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#  include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#  include <llvm/ExecutionEngine/Orc/LambdaResolver.h>
+#  if PONY_LLVM >= 500
+#    include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#  else
+#    include <llvm/ExecutionEngine/Orc/ObjectLinkingLayer.h>
+#  endif
+#  include <llvm/ExecutionEngine/SectionMemoryManager.h>
 #include "llvm_config_end.h"
 
 namespace orc = llvm::orc;
 
-#if PONY_LLVM >= 700
-using ExecutionSession = orc::ExecutionSession;
-using LinkingLayerType = orc::RTDyldObjectLinkingLayer;
-using CompileLayerType = orc::IRCompileLayer<LinkingLayerType,
-  orc::SimpleCompiler>;
-using ModuleHandleType = orc::VModuleKey;
-#elif PONY_LLVM >= 500
+#if PONY_LLVM >= 500
 using LinkingLayerType = orc::RTDyldObjectLinkingLayer;
 using CompileLayerType = orc::IRCompileLayer<LinkingLayerType,
   orc::SimpleCompiler>;
@@ -81,89 +203,23 @@ static void* find_symbol(llvm::Module& module, CompileLayerType& compile_layer,
 #endif
 }
 
-#if PONY_LLVM >= 700
-// copied from LLVM
-// unittests/ExecutionEngine/Orc/RTDyldObjectLinkingLayerTest.cpp
-class MemoryManagerWrapper : public llvm::SectionMemoryManager
-{
-public:
-  MemoryManagerWrapper(bool &DebugSeen) : DebugSeen(DebugSeen) {}
-
-  uint8_t *allocateDataSection(uintptr_t Size, unsigned Alignment,
-    unsigned SectionID, llvm::StringRef SectionName,
-    bool IsReadOnly) override
-  {
-    if (SectionName == ".debug_str")
-      DebugSeen = true;
-    return llvm::SectionMemoryManager::allocateDataSection(
-        Size, Alignment, SectionID, SectionName, IsReadOnly);
-  }
-
-private:
-  bool &DebugSeen;
-};
-
-#endif
-
 bool gen_jit_and_run(compile_t* c, int* exit_code, jit_symbol_t* symbols,
   size_t symbol_count)
 {
   reach_type_t* t_main = reach_type_name(c->reach, "Main");
   reach_type_t* t_env = reach_type_name(c->reach, "Env");
 
-  if((t_main == NULL) || (t_env == NULL))
+  if ((t_main == NULL) || (t_env == NULL))
     return false;
 
   gen_main(c, t_main, t_env);
 
-  if(!genopt(c, true))
+  if (!genopt(c, true))
     return false;
 
-  if(LLVMVerifyModule(c->module, LLVMReturnStatusAction, NULL) != 0)
+  if (LLVMVerifyModule(c->module, LLVMReturnStatusAction, NULL) != 0)
     return false;
 
-#if PONY_LLVM >= 700
-  bool debug_section_seen = false;
-  auto mem_mgr = std::make_shared<MemoryManagerWrapper>(debug_section_seen);
-  ExecutionSession exec_session(std::make_shared<llvm::orc::SymbolStringPool>());
-
-  std::map<ModuleHandleType, std::shared_ptr<orc::SymbolResolver>> resolvers;
-
-  LinkingLayerType linking_layer(exec_session,
-    [&mem_mgr, &resolvers](ModuleHandleType key)
-    {
-      auto i = resolvers.find(key);
-      assert(i != resolvers.end() && "missing resolver");
-      auto r = std::move(i->second);
-      resolvers.erase(i);
-      return LinkingLayerType::Resources { mem_mgr, std::move(r) };
-    });
-
-  auto machine = reinterpret_cast<llvm::TargetMachine*>(c->machine);
-
-  CompileLayerType compile_layer(linking_layer, orc::SimpleCompiler(*machine));
-
-  // auto local_lookup = [&compile_layer](const std::string & name)
-  // {
-  //   return compile_layer.findSymbol(name, false);
-  // };
-
-  // auto external_lookup = [&mem_mgr](const std::string & name)
-  // {
-  //   return mem_mgr->findSymbol(name);
-  // };
-
-  std::unique_ptr<llvm::Module> module(llvm::unwrap(c->module));
-  ModuleHandleType handle = exec_session.allocateVModule();
-
-  // resolvers[handle] = orc::createLambdaResolver(local_lookup,
-  //   external_lookup);
-
-  // auto error = compile_layer.addModule(handle, std::move(module));
-
-  // if (error)
-  //   return false;
-#else
   // The Orc JIT wants the module in a shared_ptr, but we don't want to transfer
   // ownership to that shared_ptr. Use an empty deleter so that the module
   // doesn't get freed when the shared_ptr is destroyed.
@@ -191,7 +247,6 @@ bool gen_jit_and_run(compile_t* c, int* exit_code, jit_symbol_t* symbols,
       return sym.toRuntimeDyldSymbol();
 
     return llvm::RuntimeDyld::SymbolInfo{nullptr};
-#endif
   };
 
   auto external_lookup = [
@@ -260,3 +315,5 @@ bool gen_jit_and_run(compile_t* c, int* exit_code, jit_symbol_t* symbols,
 
   return true;
 }
+
+#endif
