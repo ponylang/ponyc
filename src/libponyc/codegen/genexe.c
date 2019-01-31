34deee82156b5489fdb45d62866f0b6e40c1b2a5
kiwiMec <kiwi.mec@gmail.com>
2019-01-31 21:00:17 +1300

    Updating minimum OSX version from 10.8 to the current minimum version of 10.12 supported by Apple.

--- src/libponyc/codegen/genexe.c
+++ src/libponyc/codegen/genexe.c
@@ -286,7 +286,7 @@ static bool link_exe(compile_t* c, ast_t* program,
 
   snprintf(ld_cmd, ld_len,
     "%s -execute -no_pie -arch %.*s "
-    "-macosx_version_min 10.8 -o %s %s %s %s -lSystem",
+    "-macosx_version_min 10.12 -o %s %s %s %s -lSystem",
            linker, (int)arch_len, c->opt->triple, file_exe, file_o,
            lib_args, ponyrt
     );
