396ca8637ba5a4f482e1567fd45783ea28311a53
Sean T Allen <sean@monkeysnatchbanana.com>
2019-07-25 15:41:53 -0400

    Dry run of running a small segment of "nightlies" on CircleCI (#3243)
    It's not putting nightlies into the "real" remote repository for them.
    Currently, its using `main-pony/ponyc-nightlies` which is a test area.
    This will also need to be updated once it's run for a few days and we fully sign off.

---
+++ .ci-scripts/x86-64-unknown-linux-gnu-nightly.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+API_KEY=$1
+if [[ ${API_KEY} == "" ]]
+then
+  echo "API_KEY needs to be supplied as first script argument."
+  exit 1
+fi
+
+# Compiler target parameters
+ARCH=x86-64
+PIC=true
+
+# Triple construction
+VENDOR=unknown
+OS=linux-gnu
+TRIPLE=${ARCH}-${VENDOR}-${OS}
+
+# Build parameters
+MAKE_PARALLELISM=4
+BUILD_PREFIX=$(mktemp -d)
+DESTINATION=${BUILD_PREFIX}/lib/pony
+
+# Asset information
+PACKAGE_DIR=$(mktemp -d)
+PACKAGE=ponyc-${TRIPLE}
+
+# Cloudsmith configuration
+VERSION=$(date +%Y%m%d)
+ASSET_OWNER=main-pony
+ASSET_REPO=pony-nightlies
+ASSET_PATH=${ASSET_OWNER}/${ASSET_REPO}
+ASSET_FILE=${PACKAGE_DIR}/${PACKAGE}.tar.gz
+ASSET_SUMMARY="Pony compiler"
+ASSET_DESCRIPTION="https://github.com/ponylang/ponyc"
+
+# Build pony installation
+echo "Building ponyc installation..."
+make install prefix=${BUILD_PREFIX} default_pic=${PIC} arch=${ARCH} \
+  -j${MAKE_PARALLELISM} -f Makefile-lib-llvm symlink=no use=llvm_link_static
+
+# Package it all up
+echo "Creating .tar.gz of ponyc installation..."
+pushd ${DESTINATION} || exit 1
+tar -cvzf ${ASSET_FILE} *
+popd || exit 1
+
+# Ship it off to cloudsmith
+echo "Uploading package to cloudsmith..."
+cloudsmith push raw --version "${VERSION}" --api-key ${API_KEY} \
+  --summary "${ASSET_SUMMARY}" --description "${ASSET_DESCRIPTION}" \
+  ${ASSET_PATH} ${ASSET_FILE}
