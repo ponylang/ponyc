91844306731557ed18d419a25a4fd4d92a8b8795
Maya Rashish <maya@NetBSD.org>
2019-08-03 18:07:52 +0300

    If MAP_ALIGNED_SUPER is not available, don't use it.
    A fallback definition of 0 means that performing
    | MAP_ALIGNED_SUPERPAGES is a no-op.

--- src/libponyrt/mem/alloc.c
+++ src/libponyrt/mem/alloc.c
@@ -36,6 +36,9 @@ void* ponyint_virt_alloc(size_t bytes)
   p = mmap(0, bytes, PROT_READ | PROT_WRITE,
     MAP_PRIVATE | MAP_ANON, -1, 0);
 #elif defined(PLATFORM_IS_BSD)
+#ifndef MAP_ALIGNED_SUPER
+#define MAP_ALIGNED_SUPER 0
+#endif
   p = mmap(0, bytes, PROT_READ | PROT_WRITE,
     MAP_PRIVATE | MAP_ANON | MAP_ALIGNED_SUPER, -1, 0);
 #endif
