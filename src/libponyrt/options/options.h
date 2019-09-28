0137cd06ce9a46c96b915374959a919bacd29b38
Dipin Hora <dipinhora+github@gmail.com>
2019-09-28 10:25:22 -0400

    Add `--ponyhelp` option to compiled program (#3312)
    * Add `--ponyhelp` option to compiled program
    
    Prior to this commit, the only way to find out what pony runtime
    specific command line options a compiled pony program supported
    was by running `ponyc --help` and looking at the usage info for
    the compiler.
    
    This commit adds a new `--ponyhelp` option to all compiled pony
    programs that will output the usage information for pony runtime
    related command line options.
    
    * Add changelog entry

--- src/libponyrt/options/options.h
+++ src/libponyrt/options/options.h
@@ -7,6 +7,40 @@
 #define OPT_ARG_OPTIONAL 1 << 1
 #define OPT_ARG_NONE     1 << 2
 #define OPT_ARGS_FINISH {NULL, 0, UINT32_MAX, UINT32_MAX}
+#define PONYRT_HELP \
+  "Runtime options for Pony programs (not for use with ponyc):\n" \
+  "  --ponythreads    Use N scheduler threads. Defaults to the number of\n" \
+  "                   cores (not hyperthreads) available.\n" \
+  "                   This can't be larger than the number of cores available.\n" \
+  "  --ponyminthreads Minimum number of active scheduler threads allowed.\n" \
+  "                   Defaults to 0, meaning that all scheduler threads are\n" \
+  "                   allowed to be suspended when no work is available.\n" \
+  "                   This can't be larger than --ponythreads if provided,\n" \
+  "                   or the physical cores available\n" \
+  "  --ponynoscale    Don't scale down the scheduler threads.\n" \
+  "                   See --ponythreads on how to specify the number of threads\n" \
+  "                   explicitly. Can't be used with --ponyminthreads.\n" \
+  "  --ponysuspendthreshold\n" \
+  "                   Amount of idle time before a scheduler thread suspends\n" \
+  "                   itself to minimize resource consumption (max 1000 ms,\n" \
+  "                   min 1 ms).\n" \
+  "                   Defaults to 1 ms.\n" \
+  "  --ponycdinterval Run cycle detection every N ms (max 1000 ms, min 10 ms).\n" \
+  "                   Defaults to 100 ms.\n" \
+  "  --ponygcinitial  Defer garbage collection until an actor is using at\n" \
+  "                   least 2^N bytes. Defaults to 2^14.\n" \
+  "  --ponygcfactor   After GC, an actor will next be GC'd at a heap memory\n" \
+  "                   usage N times its current value. This is a floating\n" \
+  "                   point value. Defaults to 2.0.\n" \
+  "  --ponynoyield    Do not yield the CPU when no work is available.\n" \
+  "  --ponynoblock    Do not send block messages to the cycle detector.\n" \
+  "  --ponypin        Pin scheduler threads to CPU cores. The ASIO thread\n" \
+  "                   can also be pinned if `--ponypinasio` is set.\n" \
+  "  --ponypinasio    Pin the ASIO thread to a CPU the way scheduler\n" \
+  "                   threads are pinned to CPUs. Requires `--ponypin` to\n" \
+  "                   be set to have any effect.\n" \
+  "  --ponyversion    Print the version of the compiler and exit.\n" \
+  "  --ponyhelp       Print the runtime usage options and exit.\n"
 
 typedef struct opt_arg_t
 {
