4deba934ad1d8779fb74b415a170e01c78dc18aa
Sean T. Allen <sean@seantallen.com>
2019-10-08 16:39:28 -0400

    Add musl libc nightly builder
    [skip ci]

---
+++ .ci-dockerfiles/x86-64-unknown-linux-musl-nightly/build-and-push.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to DockerHub when you run this ***
+#
+
+TODAY=$(date +%Y%m%d)
+DOCKERFILE_DIR="$(dirname "$0")"
+
+docker build -t "ponylang/ponyc-ci-x86-64-unknown-linux-musl-nightly:${TODAY}" \
+  "${DOCKERFILE_DIR}"
+docker push "ponylang/ponyc-ci-x86-64-unknown-linux-musl-nightly:${TODAY}"
