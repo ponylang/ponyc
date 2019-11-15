cfafa3ca913fcde746d55a747ea0999964909d0e
Sean T. Allen <sean@seantallen.com>
2019-11-15 09:35:20 -0500

    Move release image building to being trigger based
    We don't need it to be trigger based as we aren't building via Ponyup
    yet, but we plan to be before the next release. If we aren't using
    Ponyup to populate the images before the next release, this is still
    fine and doesn't do any harm.

--- .ci-scripts/build-alpine-docker-images-on-release.bash
+++ .ci-scripts/build-alpine-docker-images-on-release.bash
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-#
 # *** You should already be logged in to DockerHub when you run this ***
 #
+# Builds docker release images with two tags:
+#
+# - release-alpine
+# - X.Y.Z-alpine for example 0.32.1-alpine
+#
+# Tools required in the environment that runs this:
+#
+# - bash
+# - docker
 
 set -o errexit
-set -o nounset
 
-# Gather expected arguments.
-if [ $# -lt 1 ]
-then
-  echo "Tag is required"
+# Verify ENV is set up correctly
+# We validate all that need to be set in case, in an absolute emergency,
+# we need to run this by hand. Otherwise the GitHub actions environment should
+# provide all of these if properly configured
+if [[ -z "${VERSION}" ]]; then
+  echo -e "\e[31mThe version number needs to be set in VERSION."
+  echo -e "\e[31mThe tag should be in the following form:"
+  echo -e "\e[31m    X.Y.Z"
+  echo -e "\e[31mExiting.\e[0m"
   exit 1
 fi
 
-# We aren't validating TAG is in our x.y.z format but we could.
-# For now, TAG validating is left up to the configuration in
-# .circleci/config.yml.
-TAG=$1
+if [[ -z "${GITHUB_REPOSITORY}" ]]; then
+  echo -e "\e[31mName of this repository needs to be set in GITHUB_REPOSITORY."
+  echo -e "\e[31mShould be in the form OWNER/REPO, for example:"
+  echo -e "\e[31m     ponylang/ponyup"
+  echo -e "\e[31mThis will be used as the docker image name."
+  echo -e "\e[31mExiting.\e[0m"
+  exit 1
+fi
 
-# Build and push :TAG tag e.g. ponyc:0.32.1-alpine.
-DOCKER_TAG=ponylang/ponyc:"${TAG}"-alpine
-docker build --pull --file=.dockerhub/alpine/Dockerfile -t "${DOCKER_TAG}" .
+# no unset variables allowed from here on out
+# allow above so we can display nice error messages for expected unset variables
+set -o nounset
+
+# Build and push :VERSION tag e.g. ponylang/ponyup:0.32.1-alpine
+DOCKER_TAG=${GITHUB_REPOSITORY}:"${VERSION}-alpine"
+docker build --pull --file .dockerhub/alpine/Dockerfile -t "${DOCKER_TAG}" .
 docker push "${DOCKER_TAG}"
 
-# Build and push "release" tag e.g. ponyc:release-alpine
-DOCKER_TAG=ponylang/ponyc:release-alpine
-docker build --pull --file=.dockerhub/alpine/Dockerfile -t "${DOCKER_TAG}" .
+# Build and push "release" tag e.g. ponylang/ponyup:release-alpine
+DOCKER_TAG=${GITHUB_REPOSITORY}:release-alpine
+docker build --pull --file .dockerhub/alpine/Dockerfile -t "${DOCKER_TAG}" .
 docker push "${DOCKER_TAG}"
