30a8b2c69a3fa6f4880d5092e3846b683d5c8a25
Sean T. Allen <sean@seantallen.com>
2019-11-14 21:27:07 -0500

    Trigger nightly jobs via API

--- .ci-scripts/trigger-nightly-builds.bash
+++ .ci-scripts/trigger-nightly-builds.bash
@@ -14,25 +14,27 @@ set -o errexit
 # We validate all that need to be set in case, in an absolute emergency,
 # we need to run this by hand. Otherwise the GitHub actions environment should
 # provide all of these if properly configured
-#if [[ -z "${CIRRUS_ACCESS_TOKEN}" ]]; then
-#  echo -e "\e[31mA cirrus access token needs to be set in CIRRUS_ACCESS_TOKEN."
-#  echo -e "Exiting.\e[0m"
-#  exit 1
-#fi
+if [[ -z "${CIRRUS_ACCESS_TOKEN}" ]]; then
+  echo -e "\e[31mA cirrus access token needs to be set in CIRRUS_ACCESS_TOKEN."
+  echo -e "Exiting.\e[0m"
+  exit 1
+fi
 
 # no unset variables allowed from here on out
 # allow above so we can display nice error messages for expected unset variables
 set -o nounset
 
+TODAY=$(date +%Y%m%d)
+
 curl -X POST https://api.cirrus-ci.com/graphql \
-  -H 'authorization: Bearer 3uf30rrv4ef0rtfm18glu4lqsl9n9k2nskmaa8e' \
+  -H 'authorization: Bearer $CIRRUS_ACCESS_TOKEN' \
   -d '{
     "query": "mutation($input: RepositoryCreateBuildInput!) { createBuild(input: $input) { build { id, repositoryId, status }, clientMutationId } }",
     "variables": {
       "input": {
         "repositoryId": 5677320443527168,
-        "clientMutationId": "lib/llvm: macOS",
-        "branch": "nightly-to-cirrus"
+        "clientMutationId": "${TODAY}",
+        "branch": "master"
       }
     }
   }'
