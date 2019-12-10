df8779d3aa1747a619570f55a5c157567d385779
Matthias Wahl <matthiaswahl@m7w3.de>
2019-12-10 11:03:34 +0100

    fix ponyint_win_process_wait declaration

--- src/libponyrt/lang/process.h
+++ src/libponyrt/lang/process.h
@@ -14,7 +14,7 @@ PONY_API size_t ponyint_win_process_create(char* appname, char* cmdline,
     char* environ, char* wdir, uint32_t stdin_fd, uint32_t stdout_fd,
     uint32_t stderr_fd, uint32_t* error_code, char** error_msg);
 
-PONY_API int32_t ponyint_win_process_wait(size_t hProcess);
+PONY_API int32_t ponyint_win_process_wait(size_t hProcess, int32_t* exit_code_ptr);
 
 PONY_API int32_t ponyint_win_process_kill(size_t hProcess);
 
