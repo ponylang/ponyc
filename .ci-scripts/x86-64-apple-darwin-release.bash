8fc895185479b27600c36886f5a9ea8f683c6e0a
Sean T. Allen <sean@seantallen.com>
2019-12-23 10:31:31 -0500

    Add macOS prebuilt releases via Cloudsmith

---
+++ .ci-scripts/x86-64-apple-darwin-release.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+API_KEY=$1
+if [[ ${API_KEY} == "" ]]
+then
+  echo "API_KEY needs to be supplied as first script argument."
+  exit 1
+fi
+
+TODAY=$(date +%Y%m%d)
+
+# Compiler target parameters
+ARCH=x86-64
+PIC=true
+
+# Triple construction
+VENDOR=apple
+OS=darwin
+TRIPLE=${ARCH}-${VENDOR}-${OS}
+
+# Build parameters
+MAKE_PARALLELISM=8
+BUILD_PREFIX=$(mktemp -d)
+DESTINATION=${BUILD_PREFIX}/lib/pony
+
+# Asset information
+PACKAGE_DIR=$(mktemp -d)
+PACKAGE=ponyc-${TRIPLE}
+
+# Cloudsmith configuration
+CLOUDSMITH_VERSION=${TODAY}
+ASSET_OWNER=ponylang
+ASSET_REPO=releases
+ASSET_PATH=${ASSET_OWNER}/${ASSET_REPO}
+ASSET_FILE=${PACKAGE_DIR}/${PACKAGE}.tar.gz
+ASSET_SUMMARY="Pony compiler"
+ASSET_DESCRIPTION="https://github.com/ponylang/ponyc"
+
+# Build pony installation
+echo "Building ponyc installation..."
+make install prefix=${BUILD_PREFIX} default_pic=${PIC} arch=${ARCH} \
+  -j${MAKE_PARALLELISM} -f Makefile-lib-llvm symlink=no
+
+# Package it all up
+echo "Creating .tar.gz of ponyc installation..."
+pushd ${DESTINATION} || exit 1
+tar -cvzf ${ASSET_FILE} *
+popd || exit 1
+
+# Ship it off to cloudsmith
+echo "Uploading package to cloudsmith..."
+cloudsmith push raw --version "${CLOUDSMITH_VERSION}" --api-key ${API_KEY} \
+  --summary "${ASSET_SUMMARY}" --description "${ASSET_DESCRIPTION}" \
+  ${ASSET_PATH} ${ASSET_FILE}
