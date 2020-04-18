71bd8b0da2b2313adfd71cb7b4f780e01b2cd398
Sean T. Allen <sean@seantallen.com>
2020-04-18 08:20:28 -0400

    Add nightly FreeBSD 12.1 builds

---
+++ .ci-scripts/x86-64-unknown-freebsd12.1-nightly.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+API_KEY=$1
+if [[ ${API_KEY} == "" ]]
+then
+  echo "API_KEY needs to be supplied as first script argument."
+  exit 1
+fi
+
+TODAY=$(date +%Y%m%d)
+
+# Compiler target parameters
+ARCH=x86-64
+PIC=true
+
+# Triple construction
+VENDOR=unkown
+OS=freebsd12.1
+TRIPLE=${ARCH}-${VENDOR}-${OS}
+
+# Build parameters
+MAKE_PARALLELISM=8
+BUILD_PREFIX=$(mktemp -d)
+DESTINATION=${BUILD_PREFIX}/lib/pony
+PONY_VERSION="nightly-${TODAY}"
+
+# Asset information
+PACKAGE_DIR=$(mktemp -d)
+PACKAGE=ponyc-${TRIPLE}
+
+# Cloudsmith configuration
+CLOUDSMITH_VERSION=${TODAY}
+ASSET_OWNER=ponylang
+ASSET_REPO=nightlies
+ASSET_PATH=${ASSET_OWNER}/${ASSET_REPO}
+ASSET_FILE=${PACKAGE_DIR}/${PACKAGE}.tar.gz
+ASSET_SUMMARY="Pony compiler"
+ASSET_DESCRIPTION="https://github.com/ponylang/ponyc"
+
+# Build pony installation
+echo "Building ponyc installation..."
+gmake configure arch=${ARCH} build_flags=-j${MAKE_PARALLELISM} \
+  version="${PONY_VERSION}"
+gmake build arch=${ARCH} build_flags=-j${MAKE_PARALLELISM} \
+  version="${PONY_VERSION}"
+gmake install prefix=${BUILD_PREFIX} symlink=no arch=${ARCH} \
+  build_flags=-j${MAKE_PARALLELISM} version="${PONY_VERSION}"
+
+# Package it all up
+echo "Creating .tar.gz of ponyc installation..."
+pushd ${DESTINATION} || exit 1
+tar -cvzf ${ASSET_FILE} *
+popd || exit 1
+
+# Ship it off to cloudsmith
+echo "Uploading package to cloudsmith..."
+cloudsmith push raw --version "${CLOUDSMITH_VERSION}" --api-key ${API_KEY} \
+  --summary "${ASSET_SUMMARY}" --description "${ASSET_DESCRIPTION}" \
+  ${ASSET_PATH} ${ASSET_FILE}
