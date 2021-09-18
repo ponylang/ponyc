773083ba26fcd8611929f6fc6c72bf19fd859358
Sean T. Allen <sean@seantallen.com>
2021-09-17 22:46:37 -0400

    Fix for 3840

--- src/libponyrt/gc/objectmap.h
+++ src/libponyrt/gc/objectmap.h
@@ -12,6 +12,7 @@ typedef struct object_t
   size_t rc;
   uint32_t mark;
   bool immutable;
+  pony_type_t* type;
 } object_t;
 
 DECLARE_HASHMAP(ponyint_objectmap, objectmap_t, object_t);
@@ -19,7 +20,7 @@ DECLARE_HASHMAP(ponyint_objectmap, objectmap_t, object_t);
 object_t* ponyint_objectmap_getobject(objectmap_t* map, void* address, size_t* index);
 
 object_t* ponyint_objectmap_getorput(objectmap_t* map, void* address,
-  uint32_t mark);
+  pony_type_t* type, uint32_t mark);
 
 void ponyint_objectmap_sweep(objectmap_t* map);
 
