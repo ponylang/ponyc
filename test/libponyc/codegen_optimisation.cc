48de3b017c987ebd394e4263d457b6f66da573ff
Sean T. Allen <sean@seantallen.com>
2021-09-19 16:58:29 -0400

    Move optimisation jit test

--- test/libponyc/codegen_optimisation.cc
+++ test/libponyc/codegen_optimisation.cc
@@ -9,38 +9,6 @@
 class CodegenOptimisationTest : public PassTest
 {};
 
-
-TEST_F(CodegenOptimisationTest, MergeSendMessageReordering)
-{
-  const char* src =
-    "use @pony_exitcode[None](code: I32)\n"
-    "actor Main\n"
-    "  var state: U8 = 0\n"
-    "  new create(env: Env) =>\n"
-    "    msg1(env)\n"
-    "    msg2(env)\n"
-    "    msg3(env)\n"
-
-    "  be msg1(env: Env) =>\n"
-    "    if state == 0 then state = 1 end\n"
-
-    "  be msg2(env: Env) =>\n"
-    "    if state == 1 then state = 2 end\n"
-
-    "  be msg3(env: Env) =>\n"
-    "    if state == 2 then\n"
-    "      @pony_exitcode(I32(1))\n"
-    "    end";
-
-  opt.release = true;
-
-  TEST_COMPILE(src);
-
-  int exit_code = 0;
-  ASSERT_TRUE(run_program(&exit_code));
-  ASSERT_EQ(exit_code, 1);
-}
-
 TEST_F(CodegenOptimisationTest, MergeSendCrossMessaging)
 {
   // Test that we don't produce invalid LLVM IR that would cause a crash.
