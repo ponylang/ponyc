13661ae627a8840a01c06cd58d047bd7e5752825
Sean T. Allen <sean@seantallen.com>
2021-11-02 19:50:13 -0400

    comment out for darwin build code

--- test/libponyc-run/small-finalisers/additional.c
+++ test/libponyc-run/small-finalisers/additional.c
@@ -16,8 +16,8 @@ EXPORT_SYMBOL extern void codegentest_small_finalisers_increment_num_objects()
   pony_exitcode((int)num_objects);
 }
 
-EXPORT_SYMBOL extern void Main_runtime_override_defaults_oo(void* opt)
-{
-  (void)opt;
-  return;
-}
+// EXPORT_SYMBOL extern void Main_runtime_override_defaults_oo(void* opt)
+// {
+//   (void)opt;
+//   return;
+// }
