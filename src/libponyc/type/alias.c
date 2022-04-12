202f784a35d37c8d11123b77660b69a911da0184
Jason Carr <jcarr250@gmail.com>
2022-04-11 22:51:42 -0400

    Handle empty union

--- src/libponyc/type/alias.c
+++ src/libponyc/type/alias.c
@@ -355,7 +355,11 @@ ast_t* consume_type(ast_t* type, token_id cap, bool keep_double_ephemeral)
         child = ast_sibling(child);
       }
 
-      if (ast_sibling(ast_child(r_type)) == NULL)
+      if (ast_child(r_type) == NULL)
+      {
+        return NULL;
+      }
+      else if (ast_sibling(ast_child(r_type)) == NULL)
       {
         ast_replace(&r_type, ast_child(r_type));
       }
