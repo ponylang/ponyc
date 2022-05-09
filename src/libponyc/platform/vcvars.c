19e86b1dd9cba1113b61972845d202219c7c90ed
Sean T. Allen <sean@seantallen.com>
2022-05-09 11:05:55 -0400

    Always use msvcrt.lib

--- src/libponyc/platform/vcvars.c
+++ src/libponyc/platform/vcvars.c
@@ -225,9 +225,9 @@ static bool find_kernel32(vcvars_t* vcvars, errors_t* errors)
   vcvars->ucrt[0] = '\0';
   strcpy(vcvars->default_libs,
     "kernel32.lib "
-#if !defined(DEBUG)
+//#if !defined(DEBUG)
     "msvcrt.lib "
-#endif
+//#endif
     "Ws2_32.lib advapi32.lib vcruntime.lib "
     "legacy_stdio_definitions.lib dbghelp.lib");
 
