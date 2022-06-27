5bd83b1a963b66ce48bf948ede78e7e1d1553881
stefandd <stefan.mannsfeld@gmail.com>
2022-06-27 17:57:07 +0200

    Tweaked test to match reported PASS on apple arm64

--- test/libponyc-run/ffi-tuple-parameters/additional.c
+++ test/libponyc-run/ffi-tuple-parameters/additional.c
@@ -5,9 +5,9 @@
 struct Vector {
   uint64_t x;
   uint64_t y;
-  uint64_t z;
+  //uint64_t z;
 };
 
 struct Vector build_vector(uint64_t init) {
-  return (struct Vector) {.x=init, .y=init, .z=init};
+  return (struct Vector) {.x=init, .y=init}; //, .z=init};
 }
