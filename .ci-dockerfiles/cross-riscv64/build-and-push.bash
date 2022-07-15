56cc20791db9c84066aa7b9b864d0953650796cf
Dipin Hora <dipinhora+github@gmail.com>
2022-07-14 23:33:04 -0400

    Add CI stuff

---
+++ .ci-dockerfiles/cross-riscv64/build-and-push.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to DockerHub when you run this ***
+#
+
+NAME="ponylang/ponyc-ci-cross-riscv64"
+TODAY=$(date +%Y%m%d)
+DOCKERFILE_DIR="$(dirname "$0")"
+
+docker build --pull -t "${NAME}:${TODAY}" "${DOCKERFILE_DIR}"
+docker push "${NAME}:${TODAY}"
