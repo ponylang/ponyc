f68f3649eca1a62a4b7891e286238bbc93e295a8
Sean T. Allen <sean@seantallen.com>
2022-10-26 03:45:39 +0000

    Test

--- benchmark/libponyrt/ds/hash.cc
+++ benchmark/libponyrt/ds/hash.cc
@@ -46,7 +46,7 @@ struct hash_elem_t
 
 void HashMapBench::SetUp(const ::benchmark::State& st)
 {
-  if (st.thread_index == 0) {
+  if (st.thread_index() == 0) {
     // range(0) == initial size of hashmap
     testmap_init(&_map, static_cast<size_t>(st.range(0)));
     // range(1) == # of items to insert
