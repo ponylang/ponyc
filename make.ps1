25ec1214f605495d41a49e970758bf09b149cf92
Sean T. Allen <sean@seantallen.com>
2022-11-04 20:10:52 +0000

    With flags

--- make.ps1
+++ make.ps1
@@ -413,8 +413,7 @@ switch ($Command.ToLower())
         else
         {
             & $outDir\ponyc.exe --bin-name=ubench --output=$outDir examples\message-ubench
-            & $outDir\ubench.exe
-            #--pingers=320 --initial-pings=5 --report-count=80 --report-interval=300 --ponynoscale
+            & $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale
             $err = $LastExitCode
         }
         if ($err -ne 0) { throw "Stress test failed: exit code $err" }
