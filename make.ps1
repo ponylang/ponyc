5fc591b1340e548ee80f72d37a7df94b40d2ba66
Sean T. Allen <sean@seantallen.com>
2022-11-04 22:28:05 +0000

    Add all jobs

--- make.ps1
+++ make.ps1
@@ -401,21 +401,45 @@ switch ($Command.ToLower())
     }
     "stress-test-release"
     {
-        if ($Uselldb -eq "yes")
-        {
-            $lldbcmd = 'C:\msys64\mingw64\bin\lldb.exe'
-            $lldbargs = @('--batch', '--one-line', 'run', '--one-line-on-crash', '"frame variable"', '--one-line-on-crash', '"register read"', '--one-line-on-crash', '"bt all"', '--one-line-on-crash', '"quit 1"', '--')
+        $lldbcmd = 'C:\msys64\mingw64\bin\lldb.exe'
+        $lldbargs = @('--batch', '--one-line', 'run', '--one-line-on-crash', '"frame variable"', '--one-line-on-crash', '"register read"', '--one-line-on-crash', '"bt all"', '--one-line-on-crash', '"quit 1"', '--')
 
-            & $outDir\ponyc.exe --bin-name=ubench --output=$outDir examples\message-ubench
-            & $lldbcmd $lldbargs $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale
-            $err = $LastExitCode
-        }
-        else
-        {
-            & $outDir\ponyc.exe --bin-name=ubench --output=$outDir examples\message-ubench
-            & $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale
-            $err = $LastExitCode
-        }
+        & $outDir\ponyc.exe --bin-name=ubench --output=$outDir examples\message-ubench
+        & $lldbcmd $lldbargs $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale --ponynoblock
+        $err = $LastExitCode
+        if ($err -ne 0) { throw "Stress test failed: exit code $err" }
+        break
+    }
+    "stress-test-with-cd-release"
+    {
+        $lldbcmd = 'C:\msys64\mingw64\bin\lldb.exe'
+        $lldbargs = @('--batch', '--one-line', 'run', '--one-line-on-crash', '"frame variable"', '--one-line-on-crash', '"register read"', '--one-line-on-crash', '"bt all"', '--one-line-on-crash', '"quit 1"', '--')
+
+        & $outDir\ponyc.exe --bin-name=ubench --output=$outDir examples\message-ubench
+        & $lldbcmd $lldbargs $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale
+        $err = $LastExitCode
+        if ($err -ne 0) { throw "Stress test failed: exit code $err" }
+        break
+    }
+    "stress-test-debug"
+    {
+        $lldbcmd = 'C:\msys64\mingw64\bin\lldb.exe'
+        $lldbargs = @('--batch', '--one-line', 'run', '--one-line-on-crash', '"frame variable"', '--one-line-on-crash', '"register read"', '--one-line-on-crash', '"bt all"', '--one-line-on-crash', '"quit 1"', '--')
+
+        & $outDir\ponyc.exe --debug --bin-name=ubench --output=$outDir examples\message-ubench
+        & $lldbcmd $lldbargs $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale --ponynoblock
+        $err = $LastExitCode
+        if ($err -ne 0) { throw "Stress test failed: exit code $err" }
+        break
+    }
+    "stress-test-with-cd-debug"
+    {
+        $lldbcmd = 'C:\msys64\mingw64\bin\lldb.exe'
+        $lldbargs = @('--batch', '--one-line', 'run', '--one-line-on-crash', '"frame variable"', '--one-line-on-crash', '"register read"', '--one-line-on-crash', '"bt all"', '--one-line-on-crash', '"quit 1"', '--')
+
+        & $outDir\ponyc.exe --debug --bin-name=ubench --output=$outDir examples\message-ubench
+        & $lldbcmd $lldbargs $outDir\ubench.exe --pingers 320 --initial-pings 5 --report-count 80 --report-interval 300 --ponynoscale
+        $err = $LastExitCode
         if ($err -ne 0) { throw "Stress test failed: exit code $err" }
         break
     }
