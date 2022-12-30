ponylang/ponyc

commit c7746df99e78be369681057b7057dff66b97e96d

Fix soundness bug introduced in Pony 0.51.2 (#4283)
This commit includes a fix for the soundness bug introduced in 0.51.2 and  adds a set of simple "obviously-correct" tests of soundness / some completeness.

The bug was introduced because the change to safe_to_move in https://github.com/ponylang/ponyc/pull/4124/commits/c5113e74cd96bf0fb528a301b9e45534c3a63494 was correct from an isolation standpoint, but not from the standpoint of checking mutability, which is what the operator code was using it for. I.e. the change was valid only for the case of auto-recovering a constructor, and not mutating an existing object.

Now, mutability is checked before checking safety. This allows a better error message for the user in addition to fixing the bug.

Fixes #4282

src/libponyc/type/safeto.c
@@ -192,3 +192,59 @@ bool safe_to_autorecover(ast_t* receiver_type, ast_t* type, direction direction)
   pony_assert(0);
   return false;
 }
+
+bool safe_to_mutate(ast_t* ast)
+{
+  switch (ast_id(ast))
+  {
+    case TK_VAR:
+    case TK_LET:
+    case TK_VARREF:
+    case TK_DONTCARE:
+    case TK_DONTCAREREF:
+      return true;
+
+    case TK_TUPLEELEMREF:
+      return true; // this isn't actually allowed, but it will be caught later.
+
+    case TK_FVARREF:
+    case TK_FLETREF:
+    case TK_EMBEDREF:
+    {
+      // If the ast is x.f, we need the type of x, which will be a nominal
+      // type or an arrow type, since we were able to lookup a field on it.
+      AST_GET_CHILDREN(ast, left, right);
+      ast_t* l_type = ast_type(left);
+
+      token_id l_cap = cap_single(l_type);
+      return cap_mutable(l_cap);
+    }
+
+    case TK_TUPLE:
+    {
+      ast_t* child = ast_child(ast);
+
+      while(child != NULL)
+      {
+        if(!safe_to_mutate(child))
+          return false;
+
+        child = ast_sibling(child);
+      }
+      return true;
+    }
+
+    case TK_SEQ:
+    {
+      // Occurs when there is a tuple on the left. Each child of the tuple will
+      // be a sequence, but only sequences with a single writeable child are
+      // valid. Other types won't appear here.
+      return safe_to_mutate(ast_child(ast));
+    }
+
+    default: {}
+  }
+
+  pony_assert(0);
+  return false;
+}

test/libponyc/cap_safety.cc
@@ -1,0 +1,183 @@
+#include <gtest/gtest.h>
+#include "util.h"
+
+#define TEST_COMPILE(src) DO(test_compile(src, "expr"))
+#define TEST_ERROR(src) DO(test_error(src, "expr"))
+
+//
+// A list of test cases for fundamental cap safety requirements.
+// Each test here should be as simple as possible.
+//
+class CapSafetyTest: public PassTest
+{};
+
+TEST_F(CapSafetyTest, NoWriteToVal)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  var y: Y = Y\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X val = recover X end\n"
+    "    x.y = Y\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, NoWriteToBox)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  var y: Y = Y\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X box = X\n"
+    "    x.y = Y\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, WriteToRef)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  var y: Y = Y\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X ref = X\n"
+    "    x.y = Y\n";
+
+  TEST_COMPILE(src);
+}
+
+TEST_F(CapSafetyTest, NoCallRefOnVal)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  fun ref mutateme() => None\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X val = recover X end\n"
+    "    x.mutateme()\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, NoCallRefOnBox)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  fun ref mutateme() => None\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X box = X\n"
+    "    x.mutateme()\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, CallRefOnRef)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  fun ref mutateme() => None\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X ref = X\n"
+    "    x.mutateme()\n";
+
+  TEST_COMPILE(src);
+}
+
+TEST_F(CapSafetyTest, CallValOnVal)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  fun val shareme() => None\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X val = recover X end\n"
+    "    x.shareme()\n";
+
+  TEST_COMPILE(src);
+}
+
+TEST_F(CapSafetyTest, NoCallValOnBox)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  fun val shareme() => None\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X box = X\n"
+    "    x.shareme()\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, NoCallValOnRef)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  fun val shareme() => None\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X ref = X\n"
+    "    x.shareme()\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, NoWriteRefToIso)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  var y: Y = Y\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X iso = recover X end\n"
+    "    let y: Y ref = Y\n"
+    "    x.y = y\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, NoWriteBoxToIso)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  var y: Y box = Y\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X iso = recover X end\n"
+    "    let y: Y box = Y\n"
+    "    x.y = y\n";
+
+  TEST_ERROR(src);
+}
+
+TEST_F(CapSafetyTest, WriteValToIso)
+{
+  const char* src =
+    "class Y\n"
+    "class X\n"
+    "  var y: Y val = Y\n"
+    "actor Main\n"
+    "  new create(env: Env) =>\n"
+    "    let x: X iso = recover X end\n"
+    "    let y: Y val = Y\n"
+    "    x.y = y\n";
+
+  TEST_COMPILE(src);
+}

src/libponyc/expr/operator.c
@@ -443,6 +443,15 @@ bool expr_assign(pass_opt_t* opt, ast_t* ast)
     return false;
   }
 
+  bool ok_mutable = safe_to_mutate(left);
+  if(!ok_mutable)
+  {
+    ast_error(opt->check.errors, ast,
+      "left side is immutable");
+    ast_free_unattached(wl_type);
+    return false;
+  }
+
   bool ok_safe = safe_to_move(left, r_type, WRITE);
 
   if(!ok_safe)

src/libponyc/type/cap.c
@@ -999,6 +999,31 @@ bool cap_sendable(token_id cap)
   return false;
 }
 
+bool cap_mutable(token_id cap)
+{
+  switch(cap)
+  {
+    case TK_VAL:
+    case TK_BOX:
+    case TK_TAG:
+    case TK_CAP_SEND:
+    case TK_CAP_SHARE:
+    case TK_CAP_READ:
+    case TK_CAP_ALIAS:
+    case TK_CAP_ANY:
+      return false;
+
+    case TK_ISO:
+    case TK_TRN:
+    case TK_REF:
+      return true;
+
+    default:
+      pony_assert(0);
+      return false;
+  }
+}
+
 bool cap_immutable_or_opaque(token_id cap)
 {
   switch(cap)

src/libponyc/type/cap.h
@@ -96,6 +96,8 @@ bool cap_sendable(token_id cap);
 
 bool cap_immutable_or_opaque(token_id cap);
 
+bool cap_mutable(token_id cap);
+
 /**
  * For a temporary capability (iso/trn), returns
  * the capability with no isolation constraints

src/libponyc/type/safeto.h
@@ -8,6 +8,7 @@
 PONY_EXTERN_C_BEGIN
 
 bool safe_to_move(ast_t* ast, ast_t* type, direction direction);
+bool safe_to_mutate(ast_t* ast);
 
 bool safe_to_autorecover(ast_t* receiver, ast_t* type, direction direction);
 

test/libponyc/CMakeLists.txt
@@ -9,6 +9,7 @@ add_executable(libponyc.tests
     bare.cc
     buildflagset.cc
     cap.cc
+    cap_safety.cc
     chain.cc
     codegen.cc
     codegen_ffi.cc

commit 03819552eb65fa05675e23939add27ac579ce433

Fix compiler crash introduced in #4283
Fixes a missed case in #4283 (arrow types). Instead of displaying
an error message, the user who was attempting to do an unsafe
mutation of a val was greeted by a compiler crash.

Fixes #4287

src/libponyc/type/safeto.c
@@ -216,6 +216,10 @@ bool safe_to_mutate(ast_t* ast)
       AST_GET_CHILDREN(ast, left, right);
       ast_t* l_type = ast_type(left);
 
+      // Any viewpoint adapted type will not be safe to write to.
+      if(ast_id(l_type) != TK_NOMINAL)
+        return false;
+
       token_id l_cap = cap_single(l_type);
       return cap_mutable(l_cap);
     }

test/libponyc/cap_safety.cc
@@ -181,3 +181,16 @@ TEST_F(CapSafetyTest, WriteValToIso)
 
   TEST_COMPILE(src);
 }
+
+TEST_F(CapSafetyTest, NoWriteArrow)
+{
+  const char* src =
+    "actor Main"
+    "  var f: U64 = 1"
+    "  new create(env: Env) =>"
+    "    _start()"
+    "  fun _start() =>"
+    "    f = 2";
+
+  TEST_ERROR(src);
+}