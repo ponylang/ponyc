f764ee7b4daa5addfdb04ef85fafcbc24ce9611c
Sean T. Allen <sean@seantallen.com>
2023-08-30 21:04:02 +0000

    Move private CI images from DockerHub to GitHub Container Registry
    Old images will remain at DockerHub, all new ones, starting with the ones
    I generated for this commit will be on GitHub Container Registry.

--- .ci-dockerfiles/x86-64-unknown-linux-musl-builder/build-and-push.bash
+++ .ci-dockerfiles/x86-64-unknown-linux-musl-builder/build-and-push.bash
@@ -4,12 +4,12 @@ set -o errexit
 set -o nounset
 
 #
-# *** You should already be logged in to DockerHub when you run this ***
+# *** You should already be logged in to GHCR when you run this ***
 #
 
 TODAY=$(date +%Y%m%d)
 DOCKERFILE_DIR="$(dirname "$0")"
 
-docker build --pull -t "ponylang/ponyc-ci-x86-64-unknown-linux-musl-builder:${TODAY}" \
+docker build --pull -t "ghcr.io/ponylang/ponyc-ci-x86-64-unknown-linux-musl-builder:${TODAY}" \
   "${DOCKERFILE_DIR}"
-docker push "ponylang/ponyc-ci-x86-64-unknown-linux-musl-builder:${TODAY}"
+docker push "ghcr.io/ponylang/ponyc-ci-x86-64-unknown-linux-musl-builder:${TODAY}"
