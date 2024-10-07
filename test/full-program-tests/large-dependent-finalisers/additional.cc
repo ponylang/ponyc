59ba3064303bd8f6dd12aac47220931f3bfc8834
Dipin Hora <dhora@movableink.com>
2024-10-07 10:04:36 -0400

    Add tests for small/large dependent finalisers use-after-free access

---
+++ test/full-program-tests/large-dependent-finalisers/additional.cc
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <pony.h>
+
+#ifdef _MSC_VER
+#  define EXPORT_SYMBOL __declspec(dllexport)
+#else
+#  define EXPORT_SYMBOL
+#endif
+
+extern "C"
+{
+
+static uint32_t num_objects = 3;
+
+EXPORT_SYMBOL extern void codegentest_large_dependent_finalisers_decrement_num_objects()
+{
+  num_objects--;
+  pony_exitcode((int)num_objects);
+}
+
+#ifndef _MSC_VER
+void Main_runtime_override_defaults_oo(void* opt)
+{
+  (void)opt;
+  return;
+}
+#endif
+
+}
