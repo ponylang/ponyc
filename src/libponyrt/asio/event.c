ponylang/ponyc

commit e9827a2563aee70a765274ce5fedd17e4bcb836d

Fix ASIO thread scheduler index (#4646)
Prior to this commit, the ASIO thread index was hardcoded via
constant in multiple places. Sometimes this was correct but other
times it wasn't (because the function was being called by an actor
instead of the ASIO theead).

This commit changes things so that the ASIO thread index can be
retrieved the same way as any other scheduler thread index and
replace all the hardcoded constant references with a call to the
pony_scheduler_index() function so that the scheduler index is
always the correct one.

src/libponyrt/asio/event.c
@@ -56,7 +56,7 @@ PONY_API void pony_asio_event_destroy(asio_event_t* ev)
     // tell scheduler threads that asio has no noisy actors
     // if the old_count was 1
     if (old_count == 1)
-      ponyint_sched_unnoisy_asio(PONY_UNKNOWN_SCHEDULER_INDEX);
+      ponyint_sched_unnoisy_asio(pony_scheduler_index());
 
     ev->noisy = false;
   }
@@ -134,7 +134,7 @@ PONY_API void pony_asio_event_send(asio_event_t* ev, uint32_t flags,
 #ifdef PLATFORM_IS_WINDOWS
   // On Windows, this can be called from an IOCP callback thread, which may
   // not have a pony_ctx() associated with it yet.
-  pony_register_thread();
+  ponyint_register_asio_thread();
 #endif
 
   asio_msg_t* m = (asio_msg_t*)pony_alloc_msg(POOL_INDEX(sizeof(asio_msg_t)),
@@ -149,5 +149,5 @@ PONY_API void pony_asio_event_send(asio_event_t* ev, uint32_t flags,
   ponyint_sendv_inject(ev->owner, &m->msg);
 
   // maybe wake up a scheduler thread if they've all fallen asleep
-  ponyint_sched_maybe_wakeup_if_all_asleep(PONY_UNKNOWN_SCHEDULER_INDEX);
+  ponyint_sched_maybe_wakeup_if_all_asleep(pony_scheduler_index());
 }

src/libponyrt/asio/asio.h
@@ -7,12 +7,16 @@
 
 #if defined(PLATFORM_IS_LINUX)
 #  define ASIO_USE_EPOLL
+#  define PONY_ASIO_SCHEDULER_INDEX PONY_EPOLL_SCHEDULER_INDEX
 #elif defined(PLATFORM_IS_MACOSX) || defined(PLATFORM_IS_BSD)
 #  define ASIO_USE_KQUEUE
+#  define PONY_ASIO_SCHEDULER_INDEX PONY_KQUEUE_SCHEDULER_INDEX
 #elif defined(PLATFORM_IS_WINDOWS)
 #  define ASIO_USE_IOCP
+#  define PONY_ASIO_SCHEDULER_INDEX PONY_IOCP_SCHEDULER_INDEX
 #elif defined(PLATFORM_IS_EMSCRIPTEN)
 #  define ASIO_USE_EMSCRIPTEN
+#  define PONY_ASIO_SCHEDULER_INDEX PONY_EPOLL_SCHEDULER_INDEX
 #else
 #  error PLATFORM NOT SUPPORTED!
 #endif

src/libponyrt/asio/emscripten.c
@@ -73,7 +73,7 @@ static void handle_queue(asio_backend_t* b)
 
   while((msg = (asio_msg_t*)ponyint_thread_messageq_pop(&b->q
 #ifdef USE_DYNAMIC_TRACE
-    , SPECIAL_THREADID_EPOLL
+    , pony_scheduler_index()
 #endif
     )) != NULL)
   {
@@ -93,7 +93,7 @@ static void handle_queue(asio_backend_t* b)
 DECLARE_THREAD_FN(ponyint_asio_backend_dispatch)
 {
   ponyint_cpu_affinity(ponyint_asio_get_cpu());
-  pony_register_thread();
+  ponyint_register_asio_thread();
   asio_backend_t* b = arg;
   pony_assert(b != NULL);
 

src/libponyrt/asio/epoll.c
@@ -45,7 +45,7 @@ static void send_request(asio_event_t* ev, int req)
   msg->flags = req;
   ponyint_thread_messageq_push(&b->q, (pony_msg_t*)msg, (pony_msg_t*)msg
 #ifdef USE_DYNAMIC_TRACE
-    , SPECIAL_THREADID_EPOLL, SPECIAL_THREADID_EPOLL
+    , pony_scheduler_index(), pony_scheduler_index()
 #endif
     );
 
@@ -85,7 +85,7 @@ static void handle_queue(asio_backend_t* b)
 
   while((msg = (asio_msg_t*)ponyint_thread_messageq_pop(&b->q
 #ifdef USE_DYNAMIC_TRACE
-    , SPECIAL_THREADID_EPOLL
+    , pony_scheduler_index()
 #endif
     )) != NULL)
   {
@@ -206,7 +206,7 @@ PONY_API void pony_asio_event_resubscribe_read(asio_event_t* ev)
 DECLARE_THREAD_FN(ponyint_asio_backend_dispatch)
 {
   ponyint_cpu_affinity(ponyint_asio_get_cpu());
-  pony_register_thread();
+  ponyint_register_asio_thread();
   asio_backend_t* b = arg;
   pony_assert(b != NULL);
 
@@ -367,7 +367,7 @@ PONY_API void pony_asio_event_subscribe(asio_event_t* ev)
     // tell scheduler threads that asio has at least one noisy actor
     // if the old_count was 0
     if (old_count == 0)
-      ponyint_sched_noisy_asio(SPECIAL_THREADID_EPOLL);
+      ponyint_sched_noisy_asio(pony_scheduler_index());
   }
 
   struct epoll_event ep;

src/libponyrt/asio/iocp.c
@@ -55,7 +55,7 @@ static void send_request(asio_event_t* ev, int req)
 
   ponyint_thread_messageq_push(&b->q, (pony_msg_t*)msg, (pony_msg_t*)msg
 #ifdef USE_DYNAMIC_TRACE
-    , SPECIAL_THREADID_IOCP, SPECIAL_THREADID_IOCP
+    , pony_scheduler_index(), pony_scheduler_index()
 #endif
     );
 
@@ -113,7 +113,7 @@ void ponyint_asio_backend_final(asio_backend_t* b)
 DECLARE_THREAD_FN(ponyint_asio_backend_dispatch)
 {
   ponyint_cpu_affinity(ponyint_asio_get_cpu());
-  pony_register_thread();
+  ponyint_register_asio_thread();
   asio_backend_t* b = (asio_backend_t*)arg;
   pony_assert(b != NULL);
   asio_event_t* stdin_event = NULL;
@@ -147,7 +147,7 @@ DECLARE_THREAD_FN(ponyint_asio_backend_dispatch)
         while((msg = (asio_msg_t*)ponyint_thread_messageq_pop(
           &b->q
 #ifdef USE_DYNAMIC_TRACE
-          , SPECIAL_THREADID_IOCP
+          , pony_scheduler_index()
 #endif
           )) != NULL)
         {
@@ -287,7 +287,7 @@ PONY_API void pony_asio_event_subscribe(asio_event_t* ev)
     // tell scheduler threads that asio has at least one noisy actor
     // if the old_count was 0
     if (old_count == 0)
-      ponyint_sched_noisy_asio(SPECIAL_THREADID_IOCP);
+      ponyint_sched_noisy_asio(pony_scheduler_index());
   }
 
   if((ev->flags & ASIO_TIMER) != 0)

src/libponyrt/asio/kqueue.c
@@ -87,7 +87,7 @@ static void handle_queue(asio_backend_t* b)
   while((msg = (asio_msg_t*)ponyint_thread_messageq_pop(
     &b->q
 #ifdef USE_DYNAMIC_TRACE
-    , SPECIAL_THREADID_KQUEUE
+    , pony_scheduler_index()
 #endif
     )) != NULL)
   {
@@ -164,7 +164,7 @@ PONY_API void pony_asio_event_resubscribe_write(asio_event_t* ev)
 DECLARE_THREAD_FN(ponyint_asio_backend_dispatch)
 {
   ponyint_cpu_affinity(ponyint_asio_get_cpu());
-  pony_register_thread();
+  ponyint_register_asio_thread();
   asio_backend_t* b = arg;
   pony_assert(b != NULL);
 
@@ -297,7 +297,7 @@ PONY_API void pony_asio_event_subscribe(asio_event_t* ev)
     // tell scheduler threads that asio has at least one noisy actor
     // if the old_count was 0
     if (old_count == 0)
-      ponyint_sched_noisy_asio(SPECIAL_THREADID_KQUEUE);
+      ponyint_sched_noisy_asio(pony_scheduler_index());
   }
 
   struct kevent event[4];
@@ -455,7 +455,7 @@ PONY_API void pony_asio_event_unsubscribe(asio_event_t* ev)
   msg->flags = ASIO_DISPOSABLE;
   ponyint_thread_messageq_push(&b->q, (pony_msg_t*)msg, (pony_msg_t*)msg
 #ifdef USE_DYNAMIC_TRACE
-    , SPECIAL_THREADID_KQUEUE, SPECIAL_THREADID_KQUEUE
+    , pony_scheduler_index(), pony_scheduler_index()
 #endif
     );
 

src/libponyrt/sched/scheduler.c
@@ -1883,6 +1883,7 @@ PONY_API void pony_register_thread()
   memset(this_scheduler, 0, sizeof(scheduler_t));
   this_scheduler->tid = ponyint_thread_self();
   this_scheduler->index = PONY_UNKNOWN_SCHEDULER_INDEX;
+  this_scheduler->ctx.scheduler = this_scheduler;
 }
 
 PONY_API void pony_unregister_thread()
@@ -1900,6 +1901,12 @@ PONY_API pony_ctx_t* pony_ctx()
 {
   pony_assert(this_scheduler != NULL);
   return &this_scheduler->ctx;
+}
+
+void ponyint_register_asio_thread()
+{
+  pony_register_thread();
+  this_scheduler->index = PONY_ASIO_SCHEDULER_INDEX;
 }
 
 // Tell all scheduler threads that asio is noisy

src/libponyrt/sched/scheduler.h
@@ -15,9 +15,9 @@ typedef struct scheduler_t scheduler_t;
 #include <platform.h>
 #include "mutemap.h"
 
-#define SPECIAL_THREADID_KQUEUE   -10
-#define SPECIAL_THREADID_IOCP     -11
-#define SPECIAL_THREADID_EPOLL    -12
+#define PONY_KQUEUE_SCHEDULER_INDEX   -10
+#define PONY_IOCP_SCHEDULER_INDEX     -11
+#define PONY_EPOLL_SCHEDULER_INDEX    -12
 #define PONY_UNKNOWN_SCHEDULER_INDEX -1
 
 // the `-999` constant is the same value that is hardcoded in `actor_pinning.pony`
@@ -140,6 +140,8 @@ PONY_API uint32_t pony_active_schedulers();
 PONY_API int32_t pony_scheduler_index();
 
 PONY_API schedulerstats_t* pony_scheduler_stats();
+
+void ponyint_register_asio_thread();
 
 /** Mark asio as being noisy
  */

commit 849251ffc63006ed97115a9820945a6dff8f332f

Fix windows ASIO clobbering scheduler index bug introduced in #4646
In #4646, we started using a dedicated scheduler index for the ASIO
thread. This was generally a good thing but on windows due to how
IOCP works, we could accidentally overwrite the scheduler index for
a normal scheduler if it happened to be the one receiving the ASIO
callback.

This commit fixes the bug to ensure that we will not overwrite the
scheduler index during ASIO callbacks on windows.

src/libponyrt/asio/event.c
@@ -134,7 +134,10 @@ PONY_API void pony_asio_event_send(asio_event_t* ev, uint32_t flags,
 #ifdef PLATFORM_IS_WINDOWS
   // On Windows, this can be called from an IOCP callback thread, which may
   // not have a pony_ctx() associated with it yet.
-  ponyint_register_asio_thread();
+  // Don't call `ponyint_register_asio_thread` because that would overwrite the
+  // scheduler index if this is run on a normal scheduler thread and that would
+  // be not good.
+  pony_register_thread();
 #endif
 
   asio_msg_t* m = (asio_msg_t*)pony_alloc_msg(POOL_INDEX(sizeof(asio_msg_t)),