b59a57a896fe6742b2bb070227deb59b6a855296
Sean T. Allen <sean@seantallen.com>
2025-05-10 22:59:23 -0400

    Add arm64 Alpine 3.21 builder

---
+++ .ci-dockerfiles/arm64-unknown-linux-alpine3.21-builder/build-and-push.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to GHCR when you run this ***
+#
+
+NAME="ghcr.io/ponylang/ponyc-ci-arm64-unknown-linux-alpine3.21-builder"
+TODAY=$(date +%Y%m%d)
+DOCKERFILE_DIR="$(dirname "$0")"
+BUILDER="arm64-builder-$(date +%s)"
+
+docker buildx create --use --name "${BUILDER}"
+docker buildx build --platform linux/arm64 --pull -t "${NAME}:${TODAY}" --output "type=image,push=true" "${DOCKERFILE_DIR}"
+docker buildx stop "${BUILDER}"
