527b5b37c9651c328b9553af900a5186a2b990f5
Sean T. Allen <sean@seantallen.com>
2025-05-10 23:26:52 -0400

    Rename aarch64 Ubuntu 24.04 to arm64 Ubuntu 24.04
    We are standardizing on arm64 as it is more generic.

--- .ci-dockerfiles/arm64-unknown-linux-ubuntu24.04-builder/build-and-push.bash
+++ .ci-dockerfiles/arm64-unknown-linux-ubuntu24.04-builder/build-and-push.bash
@@ -7,10 +7,10 @@ set -o nounset
 # *** You should already be logged in to GHCR when you run this ***
 #
 
-NAME="ghcr.io/ponylang/ponyc-ci-aarch64-unknown-linux-ubuntu24.04-builder"
+NAME="ghcr.io/ponylang/ponyc-ci-arm64-unknown-linux-ubuntu24.04-builder"
 TODAY=$(date +%Y%m%d)
 DOCKERFILE_DIR="$(dirname "$0")"
-BUILDER="aarch64-builder-$(date +%s)"
+BUILDER="arm64-builder-$(date +%s)"
 
 docker buildx create --use --name "${BUILDER}"
 docker buildx build --platform linux/arm64 --pull -t "${NAME}:${TODAY}" --output "type=image,push=true" "${DOCKERFILE_DIR}"
