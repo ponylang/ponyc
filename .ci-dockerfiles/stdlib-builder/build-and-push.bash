22ed6e20a0f92d00e07d17c7093383afca09927a
Sean T. Allen <sean@seantallen.com>
2025-12-04 12:13:25 -0500

    Stop using mkdocs-material insiders builds
    Everything is now public. See:
    https://squidfunk.github.io/mkdocs-material/blog/2025/11/11/insiders-now-free-for-everyone/

--- .ci-dockerfiles/stdlib-builder/build-and-push.bash
+++ .ci-dockerfiles/stdlib-builder/build-and-push.bash
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 set -o errexit
-
-if [[ -z "${MATERIAL_INSIDERS_ACCESS}" ]]; then
-  echo -e "\e[31mMaterial Insiders Access API key needs to be set in MATERIAL_INSIDERS_ACCESS."
-  echo -e "Exiting.\e[0m"
-  exit 1
-fi
-
-MIA="${MATERIAL_INSIDERS_ACCESS}"
-
 set -o nounset
 
 #
@@ -23,7 +14,6 @@ DOCKERFILE_DIR="$(dirname "$0")"
 FROM_TAG=release
 TAG_AS=release
 docker build --pull --build-arg FROM_TAG="${FROM_TAG}" \
-  --build-arg MATERIAL_INSIDERS_ACCESS="${MIA}" \
   -t ghcr.io/ponylang/ponyc-ci-stdlib-builder:"${TAG_AS}" "${DOCKERFILE_DIR}"
 docker push ghcr.io/ponylang/ponyc-ci-stdlib-builder:"${TAG_AS}"
 
@@ -31,6 +21,5 @@ docker push ghcr.io/ponylang/ponyc-ci-stdlib-builder:"${TAG_AS}"
 FROM_TAG=nightly
 TAG_AS=nightly
 docker build --pull --build-arg FROM_TAG="${FROM_TAG}" \
-  --build-arg MATERIAL_INSIDERS_ACCESS="${MIA}" \
   -t ghcr.io/ponylang/ponyc-ci-stdlib-builder:"${TAG_AS}" "${DOCKERFILE_DIR}"
 docker push ghcr.io/ponylang/ponyc-ci-stdlib-builder:"${TAG_AS}"
