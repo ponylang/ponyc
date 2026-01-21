2969c9ee4aa83c608d8138c575225f9ec8f01a8b
Sean T. Allen <sean@seantallen.com>
2026-01-20 22:46:25 -0500

    Fix Windows packaging
    The powershell to remove all executables except ponyc.exe and
    pony-lsp.exe was incorrect. It resulted in only ponyc.exe being left in
    the bin directory when packaging.

--- make.ps1
+++ make.ps1
@@ -592,7 +592,7 @@ switch ($Command.ToLower())
         Write-Output "Creating $buildDir\..\$package"
 
         # Remove unneeded files; we do it this way because Compress-Archive cannot add a single file to anything other than the root directory
-        Get-ChildItem -File -Path "$Prefix\bin\*" -Exclude ponyc.exe,pony-lsp.exe | Remove-Item
+        # Get-ChildItem -File -Path "$Prefix\bin\*" | Where-Object { $_.Name -notin 'ponyc.exe','pony-lsp.exe' } | Remove-Item
         Compress-Archive -Path "$Prefix\bin", "$Prefix\lib", "$Prefix\packages", "$Prefix\examples" -DestinationPath "$buildDir\..\$package" -Force
         break
     }
@@ -602,7 +602,7 @@ switch ($Command.ToLower())
         Write-Output "Creating $buildDir\..\$package"
 
         # Remove unneeded files; we do it this way because Compress-Archive cannot add a single file to anything other than the root directory
-        Get-ChildItem -File -Path "$Prefix\bin\*" -Exclude ponyc.exe,pony-lsp.exe | Remove-Item
+        Get-ChildItem -File -Path "$Prefix\bin\*" Where-Object { $_.Name -notin 'ponyc.exe','pony-lsp.exe' } | Remove-Item
         Compress-Archive -Path "$Prefix\bin", "$Prefix\lib", "$Prefix\packages", "$Prefix\examples" -DestinationPath "$buildDir\..\$package" -Force
         break
     }
